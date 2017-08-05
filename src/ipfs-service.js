4463b7e044d66226342c4edb3f68c34d2d6e1d21
Josh Fraser <joshfraz@gmail.com>
2017-08-04 18:31:24 -0700

    post JSON blog to IPFS network

--- src/ipfs-service.js
+++ src/ipfs-service.js
@@ -1,3 +1,5 @@
+var ipfsAPI = require('ipfs-api')
+
 class IpfsService {
 
   static instance;
@@ -7,16 +9,39 @@ class IpfsService {
       return IpfsService.instance;
     }
 
+    this.ipfs = ipfsAPI('gateway.0rigin.org', '5001', {protocol: 'http'})
+    this.ipfs.swarm.peers(function(err, response) { 
+      if (err) {
+        console.log("Can't connect to the IPFS Gateway.")
+        console.error(err);
+      } else {
+        console.log("IPFS - connected to " + response.length + " peers");
+      }
+    });
+
     IpfsService.instance = this;
   }
 
-  submitListing(listing) {
+  submitListing(formListing) {
+
     console.log("Submit data to IPFS and return an ipfs object here");
-    console.log(listing);
+    console.log(formListing.formData)
 
     // Stub promise
     const promise = new Promise((resolve, reject) => {
-      const ipfsListing = "QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG";
+
+      const file = {
+        path: 'listing.json',
+        content: JSON.stringify(formListing.formData)
+      }
+
+      this.ipfs.files.add([file], (err, res) => {
+        const file = res[0]
+        console.log(file.hash)
+        console.log("http://gateway.0rigin.org/ipfs/"+file.hash)
+      })
+
+      let ipfsListing = file.hash
 
       if (ipfsListing) {
         resolve(ipfsListing);
