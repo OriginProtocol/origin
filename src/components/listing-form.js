15ec579863355a54d9cc0edb3b1fa39c0e8f0d80
Josh Fraser <joshfraz@gmail.com>
2017-08-17 15:41:24 -0700

    more plausible looking keybase signature

--- src/components/listing-form.js
+++ src/components/listing-form.js
@@ -11,7 +11,19 @@ class ListingForm extends Component {
     const jsonBlob = {
       'schema':'http://localhost:3000/schemas/' + selectedSchemaType.type + '.json', 
       'data':formListing.formData, 
-      'signature':'QmbviCEZgU1ArYtEHW9JRntdLQd1RcJ4hLuNJ3PCxkk12A'
+      'signed_by':'https://keybase.io/joshfraser',
+      'signature':'-----BEGIN PGP SIGNATURE-----\n\
+Version: Keybase OpenPGP v2.0.73\n\
+Comment: https://keybase.io/crypto\n\
+\n\
+wsBcBAABCgAGBQJZlhmAAAoJEKTjGE37cmbxy38IALSQxXAE4wVc8d4rP0v8TaBE\n\
+MolxVoyev2MXUz0wdclXS2mmKMSVObiFOqjrCxqBTvzQRYbquuSQUTzO4t/C1WPp\n\
+AEodUf7KSBH7fGnuYVixIRvrvtF2MMGlFm/U1MpY1CtY5G+UYhzdoLWvOGf5b1yw\n\
+BiTAwczR7KqtFOYYdmNuIIqsUvLlV6fQjCihItIgc2521iZYxNUBSBjhINEtCUvV\n\
+L6tE1lR1dMcKOa7JMTqQsbGloiD5t2IsEdzxbzgWlheTjcqoN6id+QzPC1DK9mjX\n\
+b7Qf9nchgZZhJdOBSoSRqf47nxdUx1bqY1DIR+hOyF+p6j2nYVMcDD5Z3uB/tns=\n\
+=A9r6\n\
+-----END PGP SIGNATURE-----'
     }
     onSubmitListing(jsonBlob)
   }
