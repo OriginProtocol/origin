3e0ce12b61554a5dc0c61b9371a7768ad071edf7
Stan James <stan@wanderingstan.com>
2017-10-07 17:04:18 -0700

    Better commends on conversion funcs

--- src/services/contract-service.js
+++ src/services/contract-service.js
@@ -16,20 +16,26 @@ class ContractService {
     this.listingContract = this.contract(ListingContract)
   }
 
-  // Strip leading 2 bytes from 34 byte IPFS hash
+  // Return bytes32 hex string from base58 encoded ipfs hash,
+  // stripping leading 2 bytes from 34 byte IPFS hash
+  // Assume IPFS defaults: function:0x12=sha2, size:0x20=256 bits
+  // E.g. "QmNSUYVKDSvPUnRLKmuxk9diJ6yS96r1TrAXzjTiBcCLAL" -->
+  // "0x017dfd85d4f6cb4dcd715a88101f7b1f06cd1e009b2327a0809d01eb9c91f231"
   getBytes32FromIpfsHash(ipfsListing) {
-    // Assume IPFS defaults: function:0x12=sha2, size:0x20=256 bits
     return "0x"+bs58.decode(ipfsListing).slice(2).toString('hex')
   }
 
+  // Return base58 encoded ipfs hash from bytes32 hex string,
+  // E.g. "0x017dfd85d4f6cb4dcd715a88101f7b1f06cd1e009b2327a0809d01eb9c91f231"
+  // --> "QmNSUYVKDSvPUnRLKmuxk9diJ6yS96r1TrAXzjTiBcCLAL"
   getIpfsHashFromBytes32(bytes32Hex) {
     // Add our default ipfs values for first 2 bytes:
     // function:0x12=sha2, size:0x20=256 bits
     // and cut off leading "0x"
-    const testHashHex = "12" + "20" + bytes32Hex.slice(2)
-    const testHashBytes = Buffer.from(testHashHex, 'hex');
-    const testHashStr = bs58.encode(testHashBytes)
-    return testHashStr
+    const hashHex = "12" + "20" + bytes32Hex.slice(2)
+    const hashBytes = Buffer.from(hashHex, 'hex');
+    const hashStr = bs58.encode(hashBytes)
+    return hashStr
   }
 
   getListingForAddress(address) {
