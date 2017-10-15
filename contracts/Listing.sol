f51d94bd3788b697528f2759f9a9b80b72ea6654
Stan James <stan@wanderingstan.com>
2017-10-14 20:54:33 -0600

    Added buy function to contract, with modifiers

--- contracts/Listing.sol
+++ contracts/Listing.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.11;
 
 /// @title Listing
 /// @dev Used to keep marketplace of listings for buyers and sellers
@@ -13,8 +13,9 @@ contract Listing {
   // Event denoting that a given address has updated its array of listings
   // Currently, this means creating or deleting listings
   // In the future, we will have separate events for specific actions
-  event UpdateListings(address from);
-
+  event UpdateListings(address _from);
+  event NewListing(uint _index);
+  event ListingPurchased(uint _index, uint _unitsToBuy, uint _value);
 
   /*
    * Storage
@@ -39,6 +40,26 @@ contract Listing {
     bytes32 ipfsHash;
     uint price;
     uint unitsAvailable;
+  }
+
+
+  /*
+   * Modifiers
+   */
+
+  modifier isValidListingIndex(uint _index) {
+    require (_index < listings.length);
+    _;
+  }
+
+  modifier hasUnitsAvailable(uint _index, uint _unitsToBuy) {
+    require (_unitsToBuy <= listings[_index].unitsAvailable);
+    _;
+  }
+
+  modifier hasValueToPurchase(uint _index, uint _unitsToBuy) {
+    require (this.balance >= (listings[_index].price * _unitsToBuy));
+    _;
   }
 
 
@@ -94,6 +115,28 @@ contract Listing {
     listings.push(listingStruct(msg.sender, _ipfsHash, _price, _unitsAvailable));
     UpdateListings(msg.sender);
     return listings.length;
+  }
+
+
+  /// @dev buyListing(): Buy a listing
+  /// @param _index Index of listing to buy
+  /// @param _unitsToBuy Number of units to buy
+  function buyListing(uint _index, uint _unitsToBuy)
+    public
+    payable
+    isValidListingIndex(_index)
+    hasUnitsAvailable(_index, _unitsToBuy)
+    hasValueToPurchase(_index, _unitsToBuy)
+  {
+    // Count units as sold
+    listings[_index].unitsAvailable -= _unitsToBuy;
+
+    // Send funds to lister
+    // TODO: In future there will likely be some sort of escrow
+    listings[_index].lister.transfer(this.balance);
+
+    NewListing(_index);
+    ListingPurchased(_index, _unitsToBuy, this.balance);
   }
 
 }
