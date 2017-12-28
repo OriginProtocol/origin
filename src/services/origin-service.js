752a2bf16e13d648cc0909790346672be9925cab
Stan James <stan@wanderingstan.com>
2017-12-28 12:06:14 -0700

    Removed Josh's keybase from ipfs json.

--- src/services/origin-service.js
+++ src/services/origin-service.js
@@ -11,19 +11,6 @@ class OriginService {
       const jsonBlob = {
         'schema': `http://localhost:3000/schemas/${selectedSchemaType.type}.json`,
         'data': formListing.formData,
-        'signed_by': 'https://keybase.io/joshfraser',
-        'signature': `-----BEGIN PGP SIGNATURE-----
-  Version: Keybase OpenPGP v2.0.73
-  Comment: https://keybase.io/crypto
-
-  wsBcBAABCgAGBQJZlhmAAAoJEKTjGE37cmbxy38IALSQxXAE4wVc8d4rP0v8TaBE
-  MolxVoyev2MXUz0wdclXS2mmKMSVObiFOqjrCxqBTvzQRYbquuSQUTzO4t/C1WPp
-  AEodUf7KSBH7fGnuYVixIRvrvtF2MMGlFm/U1MpY1CtY5G+UYhzdoLWvOGf5b1yw
-  BiTAwczR7KqtFOYYdmNuIIqsUvLlV6fQjCihItIgc2521iZYxNUBSBjhINEtCUvV
-  L6tE1lR1dMcKOa7JMTqQsbGloiD5t2IsEdzxbzgWlheTjcqoN6id+QzPC1DK9mjX
-  b7Qf9nchgZZhJdOBSoSRqf47nxdUx1bqY1DIR+hOyF+p6j2nYVMcDD5Z3uB/tns=
-  =A9r6
-  -----END PGP SIGNATURE-----`
       }
 
     	// Submit to IPFS
@@ -39,7 +26,7 @@ class OriginService {
   	    contractService.submitListing(ipfsHash, formListing.formData.price, units)
   	    .then((transactionReceipt) => {
           // Success!
-  	    	console.log(`On ETH blockchain with transactionReceipt: ${transactionReceipt.tx}`)
+  	    	console.log(`Submitted to ETH blockchain with transactionReceipt.tx: ${transactionReceipt.tx}`)
           resolve(transactionReceipt.tx)
   	    })
   	    .catch((error) => {
