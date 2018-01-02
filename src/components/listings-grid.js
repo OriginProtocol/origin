26abf2b0f93a220ca236718eb1063e8dad109805
Stan James <stan@wanderingstan.com>
2018-01-01 18:09:04 -0700

    Added basic pagination with `react-js-pagination` Css from bootstrap not working. Need to figure that out.

--- src/components/listings-grid.js
+++ src/components/listings-grid.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import contractService from '../services/contract-service'
+import Pagination from 'react-js-pagination';
 
 import ListingCard from './listing-card'
 
@@ -8,7 +9,13 @@ class ListingsGrid extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {listingIds: []}
+    this.state = {
+      listingIds: [],
+      listingsPerPage: 10,
+      activePage: 1
+    }
+
+    this.handlePageChange = this.handlePageChange.bind(this)
   }
 
   componentWillMount() {
@@ -24,15 +31,27 @@ class ListingsGrid extends Component {
 
   }
 
+  handlePageChange(pageNumber) {
+    console.log(`active page is ${pageNumber}`);
+    this.setState({activePage: pageNumber});
+  }
+
   render() {
     return (
       <div className="listings-grid">
         <h1>{this.state.listingIds.length} Listings</h1>
         <div className="row">
-          {this.state.listingIds.reverse().map(listingId => (
+          {this.state.listingIds.reverse().slice((this.state.activePage-1)*this.state.listingsPerPage, (this.state.activePage)*this.state.listingsPerPage).map(listingId => (
             <ListingCard listingId={listingId} key={listingId}/>
           ))}
         </div>
+        <Pagination
+          activePage={this.state.activePage}
+          itemsCountPerPage={this.state.listingsPerPage}
+          totalItemsCount={this.state.listingIds.length}
+          pageRangeDisplayed={5}
+          onChange={this.handlePageChange}
+        />
       </div>
     )
   }
