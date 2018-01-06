9a5a2c8764bc5692c750b24554bbf07bedadacd2
Stan James <stan@wanderingstan.com>
2018-01-06 12:51:59 -0700

    Added error handling for being on wrong network.

--- src/components/hide-list.js
+++
@@ -1,7 +0,0 @@
-
-// Which listings to hide on a given networkId
-const hideList = {
-	4: [0, 1, 2, 3, 4, 5, 8]
-}
-
-export default hideList
--- src/components/listings-grid.js
+++ src/components/listings-grid.js
@@ -4,8 +4,6 @@ import Pagination from 'react-js-pagination'
 
 import ListingCard from './listing-card'
 
-import hideList from './hide-list'
-
 class ListingsGrid extends Component {
 
   constructor(props) {
@@ -20,16 +18,38 @@ class ListingsGrid extends Component {
   }
 
   componentWillMount() {
-    contractService.getAllListingIds()
-    .then((ids) => {
-      const showIds = ids.filter((i)=>hideList[4].indexOf(i) < 0)
-      this.setState({ listingIds: showIds })
-      console.log(`Listing Ids:`)
-      console.log(this.state.listingIds)
+
+    const hideListPromise = fetch('https://raw.githubusercontent.com/OriginProtocol/demo-dapp/master/public/schemas/announcements.json')
+      .then((response) => response.json())
+    const allListingsPromise = contractService.getAllListingIds()
+    .catch((error) => {
+      if (error.message.indexOf("(network/artifact mismatch)") > 0) {
+        alert("The Origin Contract was not found on this network.\nYou may need to change networks, or deploy the contract.")
+      }
+    })
+
+    Promise.all([hideListPromise, allListingsPromise])
+    .then((hideList, ids) => {
+      console.log("all!")
+      console.log(hideList, ids);
+      // const showIds = ids.filter((i)=>hideList[4].indexOf(i) < 0)
+      // this.setState({ listingIds: showIds })
+      // console.log(`Listing Ids:`)
+      // console.log(this.state.listingIds)
     })
     .catch((error) => {
-      console.error(`Error fetching listing ids`)
+      alert(error.message)
     })
+
+
+    // .then((ids) => {
+    //   const showIds = ids.filter((i)=>hideList[4].indexOf(i) < 0)
+    //   this.setState({ listingIds: showIds })
+    //   console.log(`Listing Ids:`)
+    //   console.log(this.state.listingIds)
+    //   return ids
+    // })
+
 
   }
 
