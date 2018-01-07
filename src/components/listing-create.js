ff2ae7a4ab58d43da710df7eef3c6125afadac4e
Stan James <stan@wanderingstan.com>
2018-01-07 10:20:37 -0700

    Fixed warning of "for in", and change copied code style to match ours

--- src/components/listing-create.js
+++ src/components/listing-create.js
@@ -62,29 +62,32 @@ class ListingCreate extends Component {
   }
 
   onDetailsEntered(formListing) {
+    // Helper function to approximate size of object in bytes
     function roughSizeOfObject( object ) {
-        var objectList = [];
-        var stack = [ object ];
-        var bytes = 0;
-        while ( stack.length ) {
-            var value = stack.pop();
-            if ( typeof value === 'boolean' ) {
-                bytes += 4;
-            } else if ( typeof value === 'string' ) {
-                bytes += value.length * 2;
-            } else if ( typeof value === 'number' ) {
-                bytes += 8;
-            }
-            else if (typeof value === 'object'
-              && objectList.indexOf( value ) === -1)
-            {
-                objectList.push( value );
-                for( var i in value ) {
-                    stack.push( value[ i ] );
-                }
-            }
+      var objectList = [];
+      var stack = [object];
+      var bytes = 0;
+      while (stack.length) {
+        var value = stack.pop();
+        if (typeof value === 'boolean') {
+          bytes += 4;
+        } else if (typeof value === 'string') {
+          bytes += value.length * 2;
+        } else if (typeof value === 'number') {
+          bytes += 8;
         }
-        return bytes;
+        else if (typeof value === 'object'
+          && objectList.indexOf(value) === -1)
+        {
+          objectList.push(value);
+          for(var i in value) {
+            if (value.hasOwnProperty(i)) {
+              stack.push(value[i]);
+            }
+          }
+        }
+      }
+      return bytes;
     }
     if (roughSizeOfObject(formListing.formData) > this.MAX_UPLOAD_BYTES) {
       alert("Your listing is too large. Consider using fewer or smaller photos.")
