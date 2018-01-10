09be80e9efcb8a115b830efff66d0cb1a63c43eb
Stan James <stan@wanderingstan.com>
2018-01-09 19:25:51 -0800

    Open links in new window

--- src/components/listing-create.js
+++ src/components/listing-create.js
@@ -167,7 +167,7 @@ class ListingCreate extends Component {
             <div className="row flex-sm-row-reverse">
                <div className="col-md-5 offset-md-2">
                   <div className="info-box">
-                    <div><h2>How it works</h2>Origin uses a Mozilla project called <a href="http://json-schema.org/" target="_blank">JSONSchema</a> to validate your listing according to standard rules. This standardization is key to allowing unaffiliated entities to read and write to the same data layer.<br/><br/>Be sure to give your listing an appropriate title and description that will inform others as to what you’re offering.<br/><br/><a href={`/schemas/${this.state.selectedSchemaType}.json`} target="_blank">View the <code>{this.state.selectedSchema.name}</code> schema</a></div>
+                    <div><h2>How it works</h2>Origin uses a Mozilla project called <a target="_blank" href="http://json-schema.org/" target="_blank">JSONSchema</a> to validate your listing according to standard rules. This standardization is key to allowing unaffiliated entities to read and write to the same data layer.<br/><br/>Be sure to give your listing an appropriate title and description that will inform others as to what you’re offering.<br/><br/><a href={`/schemas/${this.state.selectedSchemaType}.json`} target="_blank">View the <code>{this.state.selectedSchema.name}</code> schema</a></div>
                     <div className="info-box-image"><img className="d-none d-md-block" src="/images/features-graphic.svg" role="presentation" /></div>
                   </div>
                 </div>
@@ -223,7 +223,7 @@ class ListingCreate extends Component {
             <div className="row flex-sm-row-reverse">
               <div className="col-md-5">
                 <div className="info-box">
-                  <div><h2>What happens next?</h2>When you hit submit, a JSON object representing your listing will be published to <a href="https://ipfs.io">IPFS</a> and the content hash will be published to a listing smart contract running on the Ethereum network.<br/><br/>Please review your listing before submitting. Your listing will appear to others just as it looks on the window to the left.</div>
+                  <div><h2>What happens next?</h2>When you hit submit, a JSON object representing your listing will be published to <a target="_blank" href="https://ipfs.io">IPFS</a> and the content hash will be published to a listing smart contract running on the Ethereum network.<br/><br/>Please review your listing before submitting. Your listing will appear to others just as it looks on the window to the left.</div>
                 </div>
               </div>
               <div className="col-md-7">
