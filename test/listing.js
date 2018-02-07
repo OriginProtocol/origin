80e0b6dd0998fc05aa6fc0eedbea868cf6842b34
BFunk <bret.e.funk@gmail.com>
2018-02-07 18:39:47 -0500

    five working tests and cleanup

--- test/listing.js
+++ test/listing.js
@@ -2,13 +2,16 @@ const Listing = artifacts.require('./Listing.sol');
 
 contract('Listing', ([owner]) => {
   let listing
+  let accounts
 
   beforeEach('setup contract for each test', async () => {
     listing = await Listing.new(owner)
+    accounts = await web3.eth.accounts
   })
 
   it('has an owner', async () => {
     assert.equal(await listing.owner(), owner)
+    assert.equal(await listing.owner(), accounts[0])
   })
 
   it('can return a listing', async () => {
@@ -34,6 +37,12 @@ contract('Listing', ([owner]) => {
   })
 
   it('can create a listing', async () => {
+
+    await listing.listingsLength()
+      .then((listings) => {
+        assert.equal(listings.toNumber(), 5);
+      })
+
     await listing.create(
       "0x4f32f7a7d40b4d65a917926cbfd8fd521483e7472bcc4d024179735622447dc9", 0.6, 1
     )
@@ -43,8 +52,25 @@ contract('Listing', ([owner]) => {
       })
   })
 
+  it('can buy a listing', async () => {
+    //create listing of ten units
+    await listing.create(
+      "0x4f32f7a7d40b4d65a917926cbfd8fd521483e7472bcc4d024179735622447dc9", 0.1, 10
+    )
 
-  //it('can buy a listing', async function() {
+    //verify there are ten units
+    await listing.getListing.call(5)
+      .then((listing)  => {
+        assert.equal(listing[4].toNumber(), 10)
+        })
 
-  //})
+    //buy five units
+    await listing.buyListing(5, 5)
+
+    //check new listing total
+    await listing.getListing.call(5)
+      .then((listing)  => {
+        assert.equal(listing[4].toNumber(), 5)
+        })
+  })
 })
