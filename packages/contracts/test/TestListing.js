32429775ceedfee5c04cf9f217856d9063b64d3d
Tyler Yasaka <hey@tyleryasaka.me>
2018-02-28 08:51:54 -0700

    Fill in tests for creating and buying listings

--- packages/contracts/test/TestListing.js
+++ packages/contracts/test/TestListing.js
@@ -1,5 +1,8 @@
 const contractDefinition = artifacts.require('./Listing.sol');
 
+const initialListingsLength = 5
+const ipfsHash = '0x6b14cac30356789cd0c39fec0acc2176c3573abdb799f3b17ccc6972ab4d39ba'
+
 // Used to assert error cases
 const isEVMError = function(err) {
   let str = err.toString();
@@ -28,10 +31,22 @@ contract('Listing', accounts => {
   xit('should be able to remove a listing', async function() {
   });
 
-  xit('should be able to create a listing', async function() {
+  it('should be able to create a listing', async function() {
+    await instance.create(ipfsHash, 2, 5, {from: accounts[0]})
+    let listingCount = await instance.listingsLength()
+    assert.equal(listingCount, initialListingsLength + 1, 'listings count has incremented')
+    let [index, lister, hash, price, unitsAvailable] = await instance.getListing(initialListingsLength)
+    assert.equal(lister, accounts[0], 'lister is correct')
+    assert.equal(hash, ipfsHash, 'ipfsHash is correct')
+    assert.equal(price, 2, 'price is correct')
+    assert.equal(unitsAvailable, 5, 'unitsAvailable is correct')
   });
 
-  xit('should be able to buy a listing', async function() {
+  it('should be able to buy a listing', async function() {
+    await instance.create(ipfsHash, 2, 5, {from: accounts[0]})
+    await instance.buyListing(initialListingsLength, 3, { from: accounts[1], value: 6 })
+    let [index, lister, hash, price, unitsAvailable] = await instance.getListing(initialListingsLength)
+    assert.equal(unitsAvailable, 2, 'units available has decreased')
   });
 
 });
