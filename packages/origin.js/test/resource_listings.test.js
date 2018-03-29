b820830603af543ecf9b30eaf5355d78f2577ebb
Daniel Von Fange <daniel@leancoder.com>
2018-03-29 09:51:50 -0400

    Pretty resource_listings.test.js

--- packages/origin.js/test/resource_listings.test.js
+++ packages/origin.js/test/resource_listings.test.js
@@ -1,34 +1,37 @@
-import { expect } from 'chai'
-import Origin from '../src/index.js'
+import { expect } from "chai";
+import Origin from "../src/index.js";
 
 describe("Listing Resource", () => {
-    var origin
-    var testListingIds
+  var origin;
+  var testListingIds;
 
-    before( async () => {
-        origin = Origin // Doing this little dance because eventualy Origin won't be a singleton, but something we instantiate
-        testListingIds = await origin.contractService.getAllListingIds()
-    })
+  before(async () => {
+    origin = Origin; // Doing this little dance because eventualy Origin won't be a singleton, but something we instantiate
+    testListingIds = await origin.contractService.getAllListingIds();
+  });
 
-    it("should get all listing ids", async () => {
-        console.log("b")
-        const ids = await origin.resources.listings.allIds()
-        console.log("a")
-        expect(ids.length).to.equal(5)
-    })
-    
-    it("should get a listing", async () => {
-        const listing = await origin.resources.listings.get(testListingIds[0])
-        expect(listing.name).to.equal("Zinc House")
-        expect(listing.index).to.equal(testListingIds[0])
-    })
+  it("should get all listing ids", async () => {
+    console.log("b");
+    const ids = await origin.resources.listings.allIds();
+    console.log("a");
+    expect(ids.length).to.equal(5);
+  });
 
-    it("should buy a listing", async () => {
-        const listing = await origin.resources.listings.get(testListingIds[0])
-        const transaction = await origin.resources.listings.buy(listing.address, 1, listing.price * 1)
-        //Todo: Currently this test will fail here with a timeout
-        //  because we need to somehow get web3 approve this transaction
-        // Todo: wait for transaction, then check that purchase was created.
-    }).timeout(5000)
+  it("should get a listing", async () => {
+    const listing = await origin.resources.listings.get(testListingIds[0]);
+    expect(listing.name).to.equal("Zinc House");
+    expect(listing.index).to.equal(testListingIds[0]);
+  });
 
-})
+  it("should buy a listing", async () => {
+    const listing = await origin.resources.listings.get(testListingIds[0]);
+    const transaction = await origin.resources.listings.buy(
+      listing.address,
+      1,
+      listing.price * 1
+    );
+    //Todo: Currently this test will fail here with a timeout
+    //  because we need to somehow get web3 approve this transaction
+    // Todo: wait for transaction, then check that purchase was created.
+  }).timeout(5000);
+});
