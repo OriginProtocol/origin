1c4d40bb58f9944ecf9fce699119ab97c6be8cec
Micah Alcorn <mail@micahalcorn.com>
2018-04-03 15:46:37 -0700

    Separate listing card and purchase card

--- src/components/my-listings.js
+++ src/components/my-listings.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import TransactionCard from './transaction-card'
+import MyListingCard from './my-listing-card'
 import data from '../data'
 
 class MyListings extends Component {
@@ -15,12 +15,10 @@ class MyListings extends Component {
       const arr = data.listings
 
       switch(filter) {
-        case 'sold':
-          return arr.filter(l => l.soldAt)
-        case 'fulfilled':
-          return arr.filter(l => l.fulfilledAt)
-        case 'received':
-          return arr.filter(l => l.receivedAt)
+        case 'active':
+          return arr.filter(l => l.active)
+        case 'inactive':
+          return arr.filter(l => !l.active)
         default:
           return arr
       }
@@ -38,14 +36,13 @@ class MyListings extends Component {
             <div className="col-12 col-md-3">
               <div className="filters list-group flex-row flex-md-column">
                 <a className={`list-group-item list-group-item-action${filter === 'all' ? ' active' : ''}`} onClick={() => this.setState({ filter: 'all' })}>All</a>
-                <a className={`list-group-item list-group-item-action${filter === 'sold' ? ' active' : ''}`} onClick={() => this.setState({ filter: 'sold' })}>Sold</a>
-                <a className={`list-group-item list-group-item-action${filter === 'fulfilled' ? ' active' : ''}`} onClick={() => this.setState({ filter: 'fulfilled' })}>Fulfilled</a>
-                <a className={`list-group-item list-group-item-action${filter === 'received' ? ' active' : ''}`} onClick={() => this.setState({ filter: 'received' })}>Received</a>
+                <a className={`list-group-item list-group-item-action${filter === 'active' ? ' active' : ''}`} onClick={() => this.setState({ filter: 'active' })}>Active</a>
+                <a className={`list-group-item list-group-item-action${filter === 'inactive' ? ' active' : ''}`} onClick={() => this.setState({ filter: 'inactive' })}>Inactive</a>
               </div>
             </div>
             <div className="col-12 col-md-9">
               <div className="my-listings-list">
-                {listings.map(l => <TransactionCard key={`my-listing-${l._id}`} listing={l} perspective="seller" />)}
+                {listings.map(l => <MyListingCard key={`my-listing-${l._id}`} listing={l} />)}
               </div>
             </div>
           </div>
