3d4e053b1f535e516bb9e49fad4f6a9e384ccf99
Tyler Yasaka <hey@tyleryasaka.me>
2018-04-04 19:01:59 -0700

    Refactor origin service to be an uninstantiated class
    This allows the user of the class to instatiate it, and will allow the
    origin service to receive configuration options upon instantiation.

--- packages/origin.js/src/index.js
+++ packages/origin.js/src/index.js
@@ -1,34 +1,29 @@
-// const contractService = require('./contract-service')
-// const ipfsService = require('./ipfs-service')
-// const originService = require('./origin-service')
-
 import ContractService from "./contract-service"
 import IpfsService from "./ipfs-service"
 import OriginService from "./origin-service"
 import UserRegistryService from "./user-registry-service"
 
-const contractService = new ContractService()
-const ipfsService = new IpfsService()
-const originService = new OriginService({ contractService, ipfsService })
-const userRegistryService = new UserRegistryService()
-
-var origin = {
-  contractService: contractService,
-  ipfsService: ipfsService,
-  originService: originService,
-  userRegistryService: userRegistryService
-}
-
 var resources = {
   listings: require("./resources/listings")
 }
 
-// Give each resource access to the origin services.
-// By having a single origin, its configuration can be changed
-// and all contracts will follow it
-for (var resourceName in resources) {
-  resources[resourceName].origin = origin
-  origin[resourceName] = resources[resourceName]
+class Origin {
+  constructor() {
+    // Give each resource access to the origin services.
+    // By having a single origin, its configuration can be changed
+    // and all contracts will follow it
+    for (let resourceName in resources) {
+      resources[resourceName].origin = this
+      this[resourceName] = resources[resourceName]
+    }
+    this.contractService = new ContractService()
+    this.ipfsService = new IpfsService()
+    this.originService = new OriginService({
+      contractService: this.contractService,
+      ipfsService: this.ipfsService
+    })
+    this.userRegistryService = new UserRegistryService()
+  }
 }
 
-module.exports = origin
+module.exports = Origin
