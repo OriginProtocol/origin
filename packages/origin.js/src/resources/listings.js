4b4f23e23fa58a16b06e8094acd9bd690f35fd0c
Tyler Yasaka <hey@tyleryasaka.me>
2018-04-05 00:25:02 -0700

    Move code from submitListing method directly into the listing resource

--- packages/origin.js/src/resources/listings.js
+++ packages/origin.js/src/resources/listings.js
@@ -49,10 +49,42 @@ class Listings {
     if (data.name == undefined) {
       throw "You must include a name"
     }
-    return this.origin.originService.submitListing(
-      { formData: data },
-      schemaType
-    )
+
+    let formListing = { formData: data }
+
+    // TODO: Why can't we take schematype from the formListing object?
+    const jsonBlob = {
+      'schema': `http://localhost:3000/schemas/${schemaType}.json`,
+      'data': formListing.formData,
+    }
+
+    let ipfsHash
+    try {
+      // Submit to IPFS
+      ipfsHash = await this.ipfsService.submitFile(jsonBlob)
+    } catch (error) {
+      throw new Error(`IPFS Failure: ${error}`)
+    }
+
+    console.log(`IPFS file created with hash: ${ipfsHash} for data:`)
+    console.log(jsonBlob)
+
+    // Submit to ETH contract
+    const units = 1 // TODO: Allow users to set number of units in form
+    let transactionReceipt
+    try {
+      transactionReceipt = await this.contractService.submitListing(
+        ipfsHash,
+        formListing.formData.price,
+        units)
+    } catch (error) {
+      console.error(error)
+      throw new Error(`ETH Failure: ${error}`)
+    }
+
+    // Success!
+    console.log(`Submitted to ETH blockchain with transactionReceipt.tx: ${transactionReceipt.tx}`)
+    return transactionReceipt
   }
 
   async buy(listingAddress, unitsToBuy, ethToPay) {
