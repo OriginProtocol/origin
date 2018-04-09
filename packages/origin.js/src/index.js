0ac69623f1934643f2e1dde55768eee9625f41bd
Stan James <stan@wanderingstan.com>
2018-04-09 08:48:38 -0700

    Clarifying comment on constructors

--- packages/origin.js/src/index.js
+++ packages/origin.js/src/index.js
@@ -11,12 +11,14 @@ class Origin {
     this.contractService = new ContractService()
     this.ipfsService = new IpfsService()
 
-    // TODO: This service is deprecated. Remove once the demo dapp no longer depends on it.
+    // TODO: This service is deprecated. Remove once the demo dapp no longer
+    // depends on it.
     this.userRegistryService = new UserRegistryService()
 
     // Instantiate each resource and give it access to contracts and IPFS
     for (let resourceName in resources) {
       let Resource = resources[resourceName]
+      // A `Resource` constructor always takes a contractService and ipfsService
       this[resourceName] = new Resource({
         contractService: this.contractService,
         ipfsService: this.ipfsService
