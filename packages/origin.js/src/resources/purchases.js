84ff758a797977e3b4c00b8bc93c28b3b8d0fed0
Daniel Von Fange <daniel@leancoder.com>
2018-04-11 09:23:04 -0400

    I realized I let this file be using four spaces.

--- packages/origin.js/src/resources/purchases.js
+++ packages/origin.js/src/resources/purchases.js
@@ -1,64 +1,56 @@
 class Purchases {
-    constructor({ contractService, ipfsService }) {
-      this.contractService = contractService
-      this.ipfsService = ipfsService
-      
-      this._STAGES_TO_NUMBER = {
-        awaiting_payment: 0,
-        buyer_pending: 1,
-        seller_pending: 2,
-        in_dispute: 3,
-        review_period: 4,
-        complete: 5
-      }
-      this._NUMBERS_TO_STAGE = {}
+  constructor({ contractService, ipfsService }) {
+    this.contractService = contractService
+    this.ipfsService = ipfsService
 
-      this.STAGES = {}
-      Object.entries(this._STAGES_TO_NUMBER).map(([k, v]) => {
-        this.STAGES[k.toUpperCase()] = k
-        this._NUMBERS_TO_STAGE[v] = k
-      })
+    this._STAGES_TO_NUMBER = {
+      awaiting_payment: 0,
+      buyer_pending: 1,
+      seller_pending: 2,
+      in_dispute: 3,
+      review_period: 4,
+      complete: 5
     }
+    this._NUMBERS_TO_STAGE = {}
 
-    async contractFn(address, functionName, args=[], value=0){
-        const purchaseContract = this.contractService.purchaseContract
-        const purchase = await purchaseContract.at(address)
-        const account = await this.contractService.currentAccount()
-        args.push({ from: account, value: value })
-        return await purchase[functionName].apply(purchase, args)
-    }
+    this.STAGES = {}
+    Object.entries(this._STAGES_TO_NUMBER).map(([k, v]) => {
+      this.STAGES[k.toUpperCase()] = k
+      this._NUMBERS_TO_STAGE[v] = k
+    })
+  }
 
-    async get(address){
-        const contractData = await this.contractFn(
-            address,
-            'data'
-        )
-        return {
-            address: address,
-            stage: this._NUMBERS_TO_STAGE[contractData[0]],
-            listingAddress: contractData[1],
-            buyerAddress: contractData[2],
-            created: contractData[3],
-            buyerTimout: contractData[4]
-        }
-    }
+  async contractFn(address, functionName, args = [], value = 0) {
+    const purchaseContract = this.contractService.purchaseContract
+    const purchase = await purchaseContract.at(address)
+    const account = await this.contractService.currentAccount()
+    args.push({ from: account, value: value })
+    return await purchase[functionName].apply(purchase, args)
+  }
 
-    async pay(address, amountWei){
-        return await this.contractFn(
-            address,
-            'pay',
-            [],
-            amountWei
-        )
+  async get(address) {
+    const contractData = await this.contractFn(address, "data")
+    return {
+      address: address,
+      stage: this._NUMBERS_TO_STAGE[contractData[0]],
+      listingAddress: contractData[1],
+      buyerAddress: contractData[2],
+      created: contractData[3],
+      buyerTimout: contractData[4]
     }
+  }
 
-    async buyerConfirmReceipt(address){
-        return await this.contractFn( address, 'buyerConfirmReceipt')
-    }
+  async pay(address, amountWei) {
+    return await this.contractFn(address, "pay", [], amountWei)
+  }
 
-    async sellerGetPayout(address){
-        return await this.contractFn( address, 'sellerGetPayout')
-    }
-}  
+  async buyerConfirmReceipt(address) {
+    return await this.contractFn(address, "buyerConfirmReceipt")
+  }
+
+  async sellerGetPayout(address) {
+    return await this.contractFn(address, "sellerGetPayout")
+  }
+}
 
 module.exports = Purchases
