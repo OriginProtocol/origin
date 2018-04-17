a5f62fe54fc9a1a72c2fbeedac945d26aee7cf26
Tyler Yasaka <contact@tyleryasaka.me>
2018-04-17 03:36:15 -0700

    Pass IPFS config options to origin.js from environment

--- src/services/origin.js
+++ src/services/origin.js
@@ -1,3 +1,10 @@
 import Origin from '@originprotocol/origin'
 
-export default new Origin()
+let config = {}
+
+config.ipfsDomain = process.env.IPFS_DOMAIN ? process.env.IPFS_DOMAIN : null
+config.ipfsApiPort = process.env.IPFS_API_PORT ? process.env.IPFS_API_PORT : null
+config.ipfsGatewayPort = process.env.IPFS_GATEWAY_PORT ? process.env.IPFS_GATEWAY_PORT : null
+config.ipfsGatewayProtocol = process.env.IPFS_GATEWAY_PROTOCOL ? process.env.IPFS_GATEWAY_PROTOCOL : null
+
+export default new Origin(config)
