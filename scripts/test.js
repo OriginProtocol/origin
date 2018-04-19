26094ef689dd55f4e2986cb6337f1553a2d1fff5
Tyler Yasaka <contact@tyleryasaka.me>
2018-04-19 01:14:41 -0700

    Run local IPFS during tests

--- scripts/test.js
+++ scripts/test.js
@@ -1,16 +1,18 @@
-const { spawn } = require('child_process')
 const startGanache = require('./start-ganache')
 const deployContracts = require('./deploy-contracts')
 const testContracts = require('./test-contracts')
+const startIpfs = require('./start-ipfs')
 const testJavascript = require('./test-javascript')
 
-async function start() {
+const start = async () => {
   try {
     await startGanache()
-    await testContracts()
     await deployContracts()
+    await testContracts()
+    let daemon = await startIpfs()
     await testJavascript()
     console.log('All tests successful. :)')
+    daemon.kill() // otherwise this process will keep running in the background
     process.exit()
   }
   catch(error) {
