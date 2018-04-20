ebeb25cee35735ae88681e6db55b7996fba1f35c
Daniel Von Fange <daniel@leancoder.com>
2018-04-20 10:48:43 -0400

    Default dev contracts were broken and using infinitesimally small prices.
    Fixed by entering prices as eth and converting wie.

--- contracts/migrations/3_create_sample_listings.js
+++ contracts/migrations/3_create_sample_listings.js
@@ -13,7 +13,7 @@ module.exports = function(deployer, network) {
       deployed = deployedContract
       return deployed.create(
         "0x4f32f7a7d40b4d65a917926cbfd8fd521483e7472bcc4d024179735622447dc9",
-        3.999,
+        web3.toWei(3, "ether"),
         1,
         { from: web3.eth.accounts[0], gas: 4476768 }
       )
@@ -21,7 +21,7 @@ module.exports = function(deployer, network) {
     .then(() => {
       return deployed.create(
         "0xa183d4eb3552e730c2dd3df91384426eb88879869b890ad12698320d8b88cb48",
-        0.600,
+        web3.toWei(0.6, "ether"),
         1,
         { from: web3.eth.accounts[0], gas: 4476768 }
       )
@@ -29,7 +29,7 @@ module.exports = function(deployer, network) {
     .then(() => {
       return deployed.create(
         "0xab92c0500ba26fa6f5244f8ba54746e15dd455a7c99a67f0e8f8868c8fab4a1a",
-        8.500,
+        web3.toWei(8.5, "ether"),
         1,
         { from: web3.eth.accounts[0], gas: 4476768 }
       )
@@ -37,7 +37,7 @@ module.exports = function(deployer, network) {
     .then(() => {
       return deployed.create(
         "0x6b14cac30356789cd0c39fec0acc2176c3573abdb799f3b17ccc6972ab4d39ba",
-        1.500,
+        web3.toWei(1.5, "ether"),
         1,
         { from: web3.eth.accounts[0], gas: 4476768 }
       )
@@ -45,7 +45,7 @@ module.exports = function(deployer, network) {
     .then(() => {
       return deployed.create(
         "0xff5957ff4035d28dcee79e65aa4124a4de4dcc8cb028faca54c883a5497d8917",
-        0.300,
+        web3.toWei(0.3, "ether"),
         25,
         { from: web3.eth.accounts[0], gas: 4476768 }
       )
