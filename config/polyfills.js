2ad1b39b9512bbc847c9a1e89048273fc75ac68c
Anthony Bull <anthony@bluxomelabs.com>
2018-04-24 12:51:36 -0700

    cleanup

--- config/polyfills.js
+++ config/polyfills.js
@@ -13,14 +13,12 @@ require('whatwg-fetch');
 // It will use the native implementation if it's present and isn't buggy.
 Object.assign = require('object-assign');
 
-const { jsdom } = require('jsdom');
-global.document = jsdom('');
-global.window = document.defaultView;
-
 // Without the following stubs, when running component tests, we get...
 //  Err : "Cannot read property 'currentProvider' of undefined"
 //  Loc : node_modules/origin/dist/index.js:345
 //  Ref : http://airbnb.io/enzyme/docs/guides/jsdom.html : "jsdom ~<v10"
+
 import sinon from 'sinon';
+const { jsdom } = require('jsdom');
 global.window.web3 = sinon.stub();
 global.window.web3.currentProvider = sinon.stub();
