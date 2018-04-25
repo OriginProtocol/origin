d942fcee354af72752d1a33994fb8c10021d2f10
Tyler Yasaka <contact@tyleryasaka.me>
2018-04-25 00:08:34 -0700

    Remove obsolete service views

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from app import app_config
 from config import settings
 from util import patches
 
-from views import service_views
 from views import web_views
 
 # Silence pyflakes
 assert patches
-assert service_views
 assert web_views
 
 app_config.init_prod_app(app)
