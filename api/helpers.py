a19da9a07bdeec272954bd1095aa8e029ae88baf
Tyler Yasaka <contact@tyleryasaka.me>
2018-04-25 00:07:42 -0700

    Switch from apilib to flask-restful and marshmallow

---
+++ api/helpers.py
@@ -0,0 +1,43 @@
+from marshmallow import Schema, fields, ValidationError
+from logic import service_utils
+
+class StandardRequest(Schema):
+    pass
+
+class StandardResponse(Schema):
+    errors = fields.List(
+        fields.Dict(
+            code=fields.Str(),
+            path=fields.Str(),
+            message=fields.Str()
+        )
+    )
+
+def handle_request(data, handler, request_schema, response_schema):
+    try:
+        req = request_schema().load(data)
+    # Handle validation errors
+    except ValidationError as validation_err:
+        errors = []
+        for attr, msg in validation_err.messages.items():
+            errors.append({
+                'code': 'INVALID_REQUEST',
+                'path': attr,
+                'message': ' '.join(msg)
+            })
+        resp = {
+            'errors': errors
+        }
+        return response_schema().dump(resp), 400
+    try:
+        resp = handler(req) or {}
+        return response_schema().dump(resp), 200
+    # Handle custom errors we have explicitly thrown from our services
+    except service_utils.ServiceError as service_err:
+        resp = {
+            'errors': [{
+                'code': service_err.args[0]['code'],
+                'message': service_err.args[0]['message']
+            }]
+        }
+        return response_schema().dump(resp), 422
---
+++ api/routes.py
@@ -0,0 +1,9 @@
+from api.modules import verification
+
+def add_resources(api, resources, namespace):
+    for path, resource in resources.items():
+        api.add_resource(resource, namespace + path)
+
+def init_routes(api):
+    # add routes for new modules here
+    add_resources(api, verification.resources, '/api/verification/')
