5262de5e878ea367c26e57b1546b0cd5f4796d7b
Nick Poulden <nick@poulden.com>
2018-04-26 20:58:37 -0600

    Remove IPFS dependency

--- scripts/test.js
+++ scripts/test.js
@@ -6,23 +6,18 @@ const testJavascript = require('./helpers/test-javascript')
 const testFormat = require('./helpers/test-format')
 
 const start = async () => {
-  let daemon
   try {
     await startGanache()
     await deployContracts()
     await testContracts()
-    daemon = await startIpfs()
+    await startIpfs()
     await testJavascript()
     await testFormat()
     console.log('Tests passed. :)')
-    daemon.kill()
     process.exit()
   }
   catch(error) {
     console.error(`Tests failed.\n\n${error}`)
-    if (daemon) {
-      daemon.kill()
-    }
     process.exit(1)
   }
 }
