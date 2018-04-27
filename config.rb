8e64720daa3dbb942e17f6cd0450ba2fb7a31c56
Micah Alcorn <mail@micahalcorn.com>
2018-04-27 16:32:28 -0500

    Activate unique heading renderer

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # Unique header generation
-require './lib/unique_head.rb'
+require './lib/nesting_unique_head.rb'
 
 # Markdown
 set :markdown_engine, :redcarpet
@@ -11,7 +11,7 @@ set :markdown,
     tables: true,
     with_toc_data: true,
     no_intra_emphasis: true,
-    renderer: UniqueHeadCounter
+    renderer: NestingUniqueHeadCounter
 
 # Assets
 set :css_dir, 'stylesheets'
