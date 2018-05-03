d1a4359ab1a45f97e6a91998ed888ef3b3cb8d9b
Tyler Yasaka <contact@tyleryasaka.me>
2018-05-03 02:34:04 -0700

    Add a second sample issuer during migrations

--- contracts/migrations/4_add_sample_issuers.js
+++ contracts/migrations/4_add_sample_issuers.js
@@ -1,8 +1,8 @@
 var OriginIdentity = artifacts.require("./OriginIdentity.sol");
 var Web3 = require("web3")
 
-const issuer = "0x99C03fBb0C995ff1160133A8bd210D0E77bCD101"
-const issuerHashed = Web3.utils.soliditySha3(issuer)
+const issuer_1 = "0x99C03fBb0C995ff1160133A8bd210D0E77bCD101"
+const issuer_2 = "0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf"
 const keyPurpose = 3
 const keyType = 1
 
@@ -17,8 +17,15 @@ async function add_sample_issuer(network) {
 
   let originIdentity = await OriginIdentity.deployed()
 
+  await originIdentity.addKey(
+    Web3.utils.soliditySha3(issuer_1),
+    keyPurpose,
+    keyType,
+    { from: defaultAccount, gas: 4000000 }
+  )
+
   return await originIdentity.addKey(
-    issuerHashed,
+    Web3.utils.soliditySha3(issuer_2),
     keyPurpose,
     keyType,
     { from: defaultAccount, gas: 4000000 }
