e6e546b99a9f85c13df501142d6867e646b3c24b
Tyler Yasaka <contact@tyleryasaka.me>
2018-05-09 12:17:31 -0700

    Update default attestation server url
    The app at this domain will automatically deploy changes to the `develop` branch of the bridge server.

--- src/services/origin.js
+++ src/services/origin.js
@@ -7,7 +7,7 @@ config.ipfsApiPort = process.env.IPFS_API_PORT || null
 config.ipfsGatewayPort = process.env.IPFS_GATEWAY_PORT || null
 config.ipfsGatewayProtocol = process.env.IPFS_GATEWAY_PROTOCOL || null
 config.attestationServerUrl = process.env.ATTESTATION_SERVER_URL
-  || 'http://bridge-server-test.herokuapp.com/api/attestations'
+  || 'https://dev.bridge.originprotocol.com/api/attestations'
 
 const origin = new Origin(config)
 window.web3 = origin.contractService.web3
