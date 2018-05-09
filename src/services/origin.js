4b54cad038e3e7de579d0a59625caa068d8b85d0
Tyler Yasaka <contact@tyleryasaka.me>
2018-05-09 12:09:47 -0700

    Use `or` instead of ternary operator
    This is equivalent and easier to read

--- src/services/origin.js
+++ src/services/origin.js
@@ -2,16 +2,10 @@ import Origin from 'origin'
 
 let config = {}
 
-config.ipfsDomain = process.env.IPFS_DOMAIN ? process.env.IPFS_DOMAIN : null
-config.ipfsApiPort = process.env.IPFS_API_PORT
-  ? process.env.IPFS_API_PORT
-  : null
-config.ipfsGatewayPort = process.env.IPFS_GATEWAY_PORT
-  ? process.env.IPFS_GATEWAY_PORT
-  : null
-config.ipfsGatewayProtocol = process.env.IPFS_GATEWAY_PROTOCOL
-  ? process.env.IPFS_GATEWAY_PROTOCOL
-  : null
+config.ipfsDomain = process.env.IPFS_DOMAIN || null
+config.ipfsApiPort = process.env.IPFS_API_PORT || null
+config.ipfsGatewayPort = process.env.IPFS_GATEWAY_PORT || null
+config.ipfsGatewayProtocol = process.env.IPFS_GATEWAY_PROTOCOL || null
 config.attestationServerUrl = process.env.ATTESTATION_SERVER_URL
   || 'http://bridge-server-test.herokuapp.com/api/attestations'
 
