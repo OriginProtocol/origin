fb7513ea60b669b39350b5dab853d4a40714ffec
Micah Alcorn <mail@micahalcorn.com>
2018-05-10 14:13:21 -0500

    Wire up user to review

--- src/data.js
+++ src/data.js
@@ -56,20 +56,14 @@ export default {
       _id: '1foo2',
       content: "These chicks are HOT! I mean, ever since I got my baby chicken in the mail it's been non-stop phone calls from all my friends and neighbors and family. Everyone wants a piece of me now. I used to be quite the anti-social but, honestly, I think these baby chickens are just the boost my social life needed. Lovin' life!",
       createdAt: new Date(),
-      reviewer: {
-        name: 'Stan J.',
-        address: '0x12Be343B94f860124dC4fEe278FDCBD38C102D88',
-      },
+      reviewerAddress: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
       score: Math.floor(Math.random() * 6),
     },
     {
       _id: '3bar4',
       content: "I must admit I've always had a thing for baby chickens. I think it goes back to my childhood. Growing up all I ever really had access to were regular baby chickens so you can imagine when I saw this listing I totally flipped! I mean, how cute are these little guys?? I would highly recommend you purchase one for yourself and see what all the fuss is about.",
       createdAt: new Date(),
-      reviewer: {
-        name: 'Matt L.',
-        address: '0x34Be343B94f860124dC4fEe278FDCBD38C102D88',
-      },
+      reviewerAddress: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
       score: Math.floor(Math.random() * 6),
     }
   ],
