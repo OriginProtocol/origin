b6efd9d18e07e62c3e1863bc0bbab21358992584
Tyler Yasaka <contact@tyleryasaka.me>
2018-05-11 13:26:46 -0700

    Refactor env vars to just have bridge server domain + protocol

--- src/services/origin.js
+++ src/services/origin.js
@@ -1,11 +1,16 @@
 import Origin from 'origin'
 
-let config = {
+const bridgeServerProtocol = process.env.BRIDGE_SERVER_PROTOCOL
+const bridgeServerDomain = process.env.BRIDGE_SERVER_DOMAIN
+const attestationServerUrl =
+`${bridgeServerProtocol}://${bridgeServerDomain}/api/attestations`
+
+const config = {
   ipfsDomain: process.env.IPFS_DOMAIN,
   ipfsApiPort: process.env.IPFS_API_PORT,
   ipfsGatewayPort: process.env.IPFS_GATEWAY_PORT,
   ipfsGatewayProtocol: process.env.IPFS_GATEWAY_PROTOCOL,
-  attestationServerUrl: process.env.ATTESTATION_SERVER_URL
+  attestationServerUrl
 }
 
 try {
