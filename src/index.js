fbd6273165ba50f4eae4c3d73d95fc0f0979bed9
John <jordajm@gmail.com>
2018-05-28 00:47:36 -0600

    Issue #63 - More setup work for react-intl

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom'
 import App from './components/app'
 import { Provider } from 'react-redux'
 import Store from './Store'
-import IntlProvider from 'react-intl'
+import { IntlProvider } from 'react-intl'
 import addLocales from './utils/addLocales'
-import translations from './../messages/data.json'
+import translations from './../locales/data.json'
 
 // Add locale data to react-intl
 addLocales()
+
+//
+// TODO - move these language/messages variables to redux so we can allow user to change it via dropdown menu
+//
 
 // Detect user's preferred settings
 const language = (navigator.languages && navigator.languages[0]) ||
@@ -20,6 +24,24 @@ const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
 
 // Try full locale, try locale without region code, fallback to 'en'
 const messages = translations[languageWithoutRegionCode] || translations[language] || translations.en;
+
+// If browser doesn't support Intl (i.e. Safari), then we manually import
+// the intl polyfill and locale data.
+if (!window.Intl) {
+  require.ensure([
+    'intl',
+    'intl/locale-data/jsonp/en.js',
+    'intl/locale-data/jsonp/es.js',
+    'intl/locale-data/jsonp/fr.js',
+    'intl/locale-data/jsonp/it.js',
+  ], (require) => {
+    require('intl')
+    require('intl/locale-data/jsonp/en.js')
+    require('intl/locale-data/jsonp/es.js')
+    require('intl/locale-data/jsonp/fr.js')
+    require('intl/locale-data/jsonp/it.js')
+  })
+}
 
 ReactDOM.render(
   <Provider store={Store}>
---
+++ src/utils/doTranslations.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const glob = require('glob');
+const mkdirp = require('mkdirp');
+
+const filePattern = './messages/**/*.json';
+const outputLanguageDataDir = './locales/';
+
+// Aggregates the default messages that were extracted from the example app's
+// React components via the React Intl Babel plugin. An error will be thrown if
+// there are messages in different components that use the same `id`. The result
+// is a flat collection of `id: message` pairs for the app's default locale.
+const defaultMessages = glob.sync(filePattern)
+  .map((filename) => fs.readFileSync(filename, 'utf8'))
+  .map((file) => JSON.parse(file))
+  .reduce((collection, descriptors) => {
+    descriptors.forEach(({ id, defaultMessage }) => {
+      if (collection.hasOwnProperty(id)) {
+        throw new Error(`Duplicate message id: ${id}`);
+      }
+      collection[id] = defaultMessage;
+    });
+
+    return collection;
+  }, {});
+
+mkdirp.sync(outputLanguageDataDir);
+
+fs.writeFileSync(outputLanguageDataDir + 'data.json', `{ "en": ${JSON.stringify(defaultMessages, null, 2)} }`);
