5621baa47b95e7e2b9744324351c494292d1b003
Yu Pan <yupan@originprotocol.com>
2018-06-08 11:32:44 -0700

    Added files for support of installing web3 on react native

---
+++ global.js
@@ -0,0 +1,15 @@
+// Inject node globals into React Native global scope.
+global.Buffer = require('buffer').Buffer;
+global.process = require('process');
+
+if (typeof btoa === 'undefined') {
+  global.btoa = function (str) {
+    return new Buffer(str, 'binary').toString('base64');
+  };
+}
+
+if (typeof atob === 'undefined') {
+  global.atob = function (b64Encoded) {
+    return new Buffer(b64Encoded, 'base64').toString('binary');
+  };
+}
---
+++ rn-cli.config.js
@@ -0,0 +1,5 @@
+const extraNodeModules = require('node-libs-browser');
+
+module.exports = {
+  extraNodeModules,
+};
