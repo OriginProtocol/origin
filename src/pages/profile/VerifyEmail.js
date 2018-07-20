bb5ffc8024ef577d2629edc559776b11db9158cd
Domen Grabec <grabec@gmail.com>
2018-07-20 09:13:49 +0200

    add general and form errors for VerifyEmail attestation

--- src/pages/profile/VerifyEmail.js
+++ src/pages/profile/VerifyEmail.js
@@ -8,7 +8,13 @@ import origin from '../../services/origin'
 class VerifyEmail extends Component {
   constructor() {
     super()
-    this.state = { mode: 'email', email: '', code: '' }
+    this.state = { 
+      mode: 'email',
+      email: '',
+      code: '' ,
+      formErrors: {},
+      generalErrors: []
+    }
   }
 
   render() {
@@ -22,25 +28,9 @@ class VerifyEmail extends Component {
         <div className="image-container d-flex align-items-center">
           <img src="images/email-icon-dark.svg" role="presentation" />
         </div>
-        <form
-          onSubmit={async e => {
-            e.preventDefault()
-            if (this.state.mode === 'email') {
-              origin.attestations.emailGenerateCode({
-                email: this.state.email
-              })
-              this.setState({ mode: 'code' })
-            } else if (this.state.mode === 'code') {
-              let emailAttestation = await origin.attestations.emailVerify({
-                email: this.state.email,
-                code: this.state.code,
-                wallet: this.props.wallet
-              })
-              this.props.onSuccess(emailAttestation)
-            }
-          }}
-        >
+        <form onSubmit={ event => this.onSubmit(event)}>
           <h2>Verify Your Email Address</h2>
+          <div className="general-error">{this.state.generalErrors.length > 0 ? this.state.generalErrors.join(' ') : ''}</div>
           {this.state.mode === 'email'
             ? this.renderEmailForm()
             : this.renderCodeForm()}
@@ -69,7 +59,45 @@ class VerifyEmail extends Component {
     )
   }
 
+  async onSubmit(event) {
+    event.preventDefault()
+    this.clearErrors()
+
+    try{
+      if (this.state.mode === 'email') {
+        await origin.attestations.emailGenerateCode({
+          email: this.state.email
+        })
+
+        this.setState({ mode: 'code' })
+      } else if (this.state.mode === 'code') {
+        let emailAttestation = await origin.attestations.emailVerify({
+          email: this.state.email,
+          code: this.state.code,
+          wallet: this.props.wallet
+        })
+        this.props.onSuccess(emailAttestation)
+      }
+    } catch (exception) {
+      const errorsJson = JSON.parse(exception).errors
+        
+      if (Array.isArray(errorsJson)) // Service exceptions
+        this.setState({ generalErrors: errorsJson })
+      else // Form exception
+        this.setState({ formErrors: errorsJson })
+    }
+
+  }
+
+  clearErrors() {
+    // clear errors
+    this.setState({formErrors: {}})
+    this.setState({generalErrors:[]})
+  }
+
   renderEmailForm() {
+    const emailErrors = this.state.formErrors.email
+
     return (
       <div className="form-group">
         <label htmlFor="email">
@@ -77,21 +105,24 @@ class VerifyEmail extends Component {
           <sup>ID</sup>
           {' will send you a verification code'}
         </label>
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          name="email"
-          value={this.state.email}
-          onChange={e =>
-            this.setState({ email: e.currentTarget.value })
-          }
-          placeholder={ <FormattedMessage
-                          id={ 'VerifyEmail.validEmail' }
-                          defaultMessage={ 'Valid email address' }
-                        /> }
-          required
-        />
+        <div className={`form-control-wrap ${emailErrors ? 'error' : ''}`}>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={e =>
+                this.setState({ email: e.currentTarget.value })
+              }
+              placeholder={ <FormattedMessage
+                              id={ 'VerifyEmail.validEmail' }
+                              defaultMessage={ 'Valid email address' }
+                            /> }
+              required
+            />
+            <div className="error_message">{emailErrors ? emailErrors.join(' ') : ''}</div>
+          </div>
         <div className="explanation">
           <FormattedMessage
             id={ 'VerifyEmail.emailNotPublished' }
