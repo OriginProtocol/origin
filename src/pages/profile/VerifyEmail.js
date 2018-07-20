2549af215123d4ac96858823fe53734f31fac139
Domen Grabec <grabec@gmail.com>
2018-07-20 11:02:44 +0200

    on cancel restart verification flow
    (cherry picked from commit c4c156e0176841937c51583a8ab7ec24b5d3f8ac)

--- src/pages/profile/VerifyEmail.js
+++ src/pages/profile/VerifyEmail.js
@@ -57,7 +57,7 @@ class VerifyEmail extends Component {
             <a
               href="#"
               data-modal="email"
-              onClick={this.props.handleToggle}
+              onClick={event => this.onCancel(event)}
             >
               <FormattedMessage
                 id={ 'VerifyEmail.cancel' }
@@ -68,6 +68,13 @@ class VerifyEmail extends Component {
         </form>
       </Modal>
     )
+  }
+
+  onCancel(event) {
+    event.preventDefault()
+    this.clearErrors()
+    this.setState({ mode: 'email' })
+    this.props.handleToggle(event)
   }
 
   async onSubmit(event) {
