c378cfa1fb98ae672b405fb032b038f4911f0dcb
tomlinton <tlinton@dockyard.nz>
2018-08-02 10:20:54 +1200

    Use a single script with an argument for stack selection

--- install.sh
+++ install.sh
@@ -2,29 +2,30 @@
 
 set -e
 
-VERBOSE=0
+QUIET=0
 DRY=0
 
-function print_help() {
-  echo "Sets up origin-develop."
+function usage() {
+  echo "Installs Origin Protocol development environments."
   echo
   echo "Usage:"
-  echo "  $0 [-h] [-v] [-d]"
+  echo "  $0 -e [env] [-h] [-v] [-d]"
   echo
   echo "Options:"
-  echo -e "  -h \t For usage help."
-  echo -e "  -v \t Verbose mode - show output of all commands."
+  echo -e "  -e \t The environment to install, origin or origin-website."
+  echo -e "  -h \t Show this help."
+  echo -e "  -q \t Quiet mode - hide output of all commands."
   echo -e "  -d \t Dry run mode - show the commands that would be executed."
   echo
 }
 
-function print_hi() {
+function print_origin_start() {
   echo
   echo -e "\033[94m Setting up your Origin Protocol development environment. This will take a few minutes... \033[0m"
   echo
 }
 
-function print_bye() {
+function print_origin_finish() {
   echo
   echo -e "\033[97m All set! Get building... \033[0m"
   echo
@@ -37,31 +38,102 @@ function print_bye() {
   echo
 }
 
+function print_website_start() {
+  echo
+  echo -e "\033[94m Setting up your Origin Protocol website development environment. This will take a few minutes... \033[0m"
+  echo
+}
+
+function print_website_finish() {
+  echo
+  echo -e "\033[97m All set! Get building... \033[0m"
+  echo
+  echo -e " Origin-website is running at \033[94mhttp://localhost:5000\033[0m"
+  echo -e " For more help \033[97mdocker-compose -h\033[0m"
+  echo -e " To follow the logs run \033[97mdocker-compose logs -f\033[0m"
+  echo
+}
+
 function run_step() {
   printf " \033[97m%-24s\033[0m" "$1"
   shift
 
-  [ $VERBOSE -eq 0 ] && out="> /dev/null 2>&1"
+  [ $QUIET -eq 1 ] && out="> /dev/null 2>&1"
   [ $DRY -eq 1 ] && dry="echo"
+
+  if [ $QUIET -eq 0 ] ; then
+    echo -e ""
+  fi
 
   eval "$dry $@ $out"
   wait
 
-  if [ $VERBOSE -eq 0 ] ; then
+  if [ $QUIET -eq 1 ] ; then
     echo -e "\033[97m ✓\033[0m"
   fi
 }
 
-while getopts ":vdh" opt; do
+function install_origin_environment() {
+	print_origin_start
+
+	run_step "Cloning origin-js" &&
+	test -d origin-js || git clone https://github.com/OriginProtocol/origin-js.git --branch develop &&
+
+	run_step "Cloning origin-bridge" &&
+	test -d origin-bridge || git clone https://github.com/OriginProtocol/origin-bridge.git --branch develop &&
+
+	run_step "Cloning origin-dapp" &&
+	test -d origin-dapp || git clone https://github.com/OriginProtocol/origin-dapp.git --branch develop
+
+	run_step "Building containers" \
+	docker-compose build
+
+	run_step "Bringing up stack" \
+	docker-compose up -d &&
+	# Wait for origin-js to bring up tests server to ensure contracts are compiled
+	# This can't be checked against localhost because Docker exposes the ports
+	# before the service is running
+	docker-compose exec origin-bridge wait-for.sh -t 0 -q origin-js:8081
+
+	run_step "Configuring database" \
+	docker-compose exec origin-bridge flask db upgrade
+
+	print_origin_finish
+}
+
+
+function install_website_environment() {
+	print_website_start
+
+	run_step "Cloning origin-website" &&
+		test -d origin-website || git clone https://github.com/OriginProtocol/origin-website.git --branch develop
+
+	run_step "Building containers" \
+	  docker-compose -f docker-compose-web.yml build --no-cache
+
+	run_step "Bringing up stack" \
+	  docker-compose -f docker-compose-web.yml up -d
+
+	run_step "Configuring database" \
+	  docker-compose -f docker-compose-web.yml exec origin-website flask db upgrade
+
+	print_website_finish
+}
+
+while getopts "e:vdh" opt; do
   case $opt in
-    v)
-      VERBOSE=1
+    e)
+      ENV=$OPTARG
+      echo $ENV
+      ;;
+    q)
+      QUIET=1
       ;;
     d)
       DRY=1
       ;;
     h)
-      print_help
+      usage
       exit 0
       ;;
     \?)
@@ -75,25 +147,10 @@ while getopts ":vdh" opt; do
   esac
 done
 
-print_hi
-
-run_step "Cloning repositories" &&
-	test -d origin-js || git clone https://github.com/OriginProtocol/origin-js.git --branch develop &&
-	test -d origin-bridge || git clone https://github.com/OriginProtocol/origin-bridge.git --branch develop &&
-	test -d origin-dapp || git clone https://github.com/OriginProtocol/origin-dapp.git --branch develop
-
-run_step "Building containers" \
-  docker-compose build
-
-run_step "Bringing up stack" \
-  docker-compose up -d &&
-  # Wait for origin-js to bring up tests server to ensure contracts are compiled
-  # This can't be checked against localhost because Docker exposes the ports
-  # before the service is running
-  docker-compose exec origin-bridge wait-for.sh -t 0 -q origin-js:8081
-
-
-run_step "Configuring database" \
-  docker-compose exec origin-bridge flask db upgrade
-
-print_bye
+if [ "$ENV" = "origin" ]; then
+ 	install_origin_environment
+elif [ "$ENV" = "origin-website" ]; then
+	install_website_environment
+else
+	usage
+fi
