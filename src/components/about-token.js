3ff40bd44de6c1c0ba47bc63988840d8e43a0f43
Mitch Kessler <ekemekm@gmail.com>
2018-08-14 12:48:01 -0700

    add page content

---
+++ src/components/about-token.js
@@ -0,0 +1,59 @@
+import React from 'react'
+// import { FormattedMessage } from 'react-intl'
+
+const AboutToken = () => {
+  return (
+    <div className="about-token-wrapper">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="">About Origin Tokens</h1>
+            <p className="lead">What are Origin Tokens used for?</p>
+            <h3 className="">Incentives</h3>
+            <p className="">
+  	        Vivamus faucibus tortor ultrices arcu 
+            consectetur convallis. Fusce eget fringilla 
+            velit, vitae dignissim ipsum. Curabitur hendrerit, 
+            massa sit amet molestie tincidunt, lacus purus aliquam urna, 
+            at venenatis nibh dui sit amet nunc. Nunc tortor leo, 
+            pretium id convallis et, sollicitudin in ex.</p>
+            <h3 className="">Governance</h3>
+            <p className="">
+            Vivamus faucibus tortor ultrices arcu 
+            consectetur convallis. Fusce eget fringilla 
+            velit, vitae dignissim ipsum. Curabitur hendrerit, 
+            massa sit amet molestie tincidunt, lacus purus aliquam urna, 
+            at venenatis nibh dui sit amet nunc. Nunc tortor leo, 
+            pretium id convallis et, sollicitudin in ex.</p>
+            <p className="lead">Where can I buy Origin Tokens?</p>
+            <h3 className="">List of Approved Exchanges</h3>
+            <p className="">
+            The following are the only approved exchanges
+             where you can buy Origin Tokens. Never attempt 
+             to buy anywhere else.</p>
+             <p className=""><a href="#" className="">Exchange Name ></a></p>
+             <p className=""><a href="#" className="">Exchange Name ></a></p>
+             <p className=""><a href="#" className="">Exchange Name ></a></p>
+             <p className="lead">Can I earn Origin Tokens?</p>
+            <h3 className="">Earning Origin Tokens</h3>
+            <p className="">
+            You can earn Origin Tokens by following 
+            any of the steps below:</p>
+            <ul>
+              <li>Do this to earn tokens</li>
+              <li>Do this to earn tokens</li>
+              <li>Do this to earn tokens</li>
+            </ul>
+          </div>
+          <div className="col-md-6">
+            <br/>
+            <br/>
+            <div className="video-placeholder"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutToken
