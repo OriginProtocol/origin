a1a8dc6b66e2363955a262206a7a8cb8fa63dbc7
Mitch Kessler <ekemekm@gmail.com>
2018-08-14 13:36:34 -0700

    add styling and translation support

--- src/components/about-token.js
+++ src/components/about-token.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { FormattedMessage } from 'react-intl'
+import { FormattedMessage } from 'react-intl'
 
 const AboutToken = () => {
   return (
@@ -7,47 +7,122 @@ const AboutToken = () => {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <h1 className="">About Origin Tokens</h1>
-            <p className="lead">What are Origin Tokens used for?</p>
-            <h3 className="">Incentives</h3>
-            <p className="">
-  	        Vivamus faucibus tortor ultrices arcu 
-            consectetur convallis. Fusce eget fringilla 
-            velit, vitae dignissim ipsum. Curabitur hendrerit, 
-            massa sit amet molestie tincidunt, lacus purus aliquam urna, 
-            at venenatis nibh dui sit amet nunc. Nunc tortor leo, 
-            pretium id convallis et, sollicitudin in ex.</p>
-            <h3 className="">Governance</h3>
-            <p className="">
-            Vivamus faucibus tortor ultrices arcu 
-            consectetur convallis. Fusce eget fringilla 
-            velit, vitae dignissim ipsum. Curabitur hendrerit, 
-            massa sit amet molestie tincidunt, lacus purus aliquam urna, 
-            at venenatis nibh dui sit amet nunc. Nunc tortor leo, 
-            pretium id convallis et, sollicitudin in ex.</p>
-            <p className="lead">Where can I buy Origin Tokens?</p>
-            <h3 className="">List of Approved Exchanges</h3>
-            <p className="">
-            The following are the only approved exchanges
-             where you can buy Origin Tokens. Never attempt 
-             to buy anywhere else.</p>
-             <p className=""><a href="#" className="">Exchange Name ></a></p>
-             <p className=""><a href="#" className="">Exchange Name ></a></p>
-             <p className=""><a href="#" className="">Exchange Name ></a></p>
-             <p className="lead">Can I earn Origin Tokens?</p>
-            <h3 className="">Earning Origin Tokens</h3>
-            <p className="">
-            You can earn Origin Tokens by following 
-            any of the steps below:</p>
+            <h1>
+              <FormattedMessage
+                id={'about-token.about'}
+                defaultMessage={'About Origin Tokens'}
+              />
+            </h1>
+            <h3 className="lead">
+              <FormattedMessage
+                id={'about-token.use'}
+                defaultMessage={'What are Origin Tokens used for?'}
+              />
+            </h3>
+            <h6><strong>
+              <FormattedMessage
+                id={'about-token.incentives'}
+                defaultMessage={'Incentives'}
+              /></strong>
+            </h6>
+            <p>
+              <FormattedMessage
+                id={'about-token.incentive-text'}
+                defaultMessage={'Vivamus faucibus tortor ultrices arcu consectetur convallis. Fusce eget fringilla velit, vitae dignissim ipsum. Curabitur hendrerit, massa sit amet molestie tincidunt, lacus purus aliquam urna, at venenatis nibh dui sit amet nunc. Nunc tortor leo, pretium id convallis et, sollicitudin in ex.'}
+              />
+  	        </p>
+            <h6><strong>
+              <FormattedMessage
+                id={'about-token.governance'}
+                defaultMessage={'Governance'}
+              /></strong>
+            </h6>
+            <p>
+              <FormattedMessage
+                id={'about-token.governance-text'}
+                defaultMessage={'Vivamus faucibus tortor ultrices arcu consectetur convallis. Fusce eget fringilla velit, vitae dignissim ipsum. Curabitur hendrerit, massa sit amet molestie tincidunt, lacus purus aliquam urna, at venenatis nibh dui sit amet nunc. Nunc tortor leo, pretium id convallis et, sollicitudin in ex.'}
+              />
+            </p>
+            <br />
+            <h3 className="lead">
+              <FormattedMessage
+                id={'about-token.buying-tokens'}
+                defaultMessage={'Where can I buy Origin Tokens?'}
+              />
+            </h3>
+            <h6><strong>
+              <FormattedMessage
+                id={'about-token.exchanges'}
+                defaultMessage={'List of Approved Exchanges'}
+              /></strong>
+            </h6>
+            <p>
+              <FormattedMessage
+                id={'about-token.exchanges-text'}
+                defaultMessage={'The following are the only approved exchanges where you can buy Origin Tokens. Never attempt to buy anywhere else.'}
+              />
+            </p>
+            <p><a href="#">
+              <FormattedMessage
+                id={'about-token.exchange-1'}
+                defaultMessage={'Exchange Name >'}
+              /></a>
+            </p>
+            <p><a href="#">
+              <FormattedMessage
+                id={'about-token.exchange-2'}
+                defaultMessage={'Exchange Name >'}
+              /></a>
+            </p>
+            <p><a href="#">
+              <FormattedMessage
+                id={'about-token.exchange-3'}
+                defaultMessage={'Exchange Name >'}
+              /></a>
+            </p>
+            <br />
+            <h3 className="lead">
+              <FormattedMessage
+                id={'about-token.earn'}
+                defaultMessage={'Can I earn Origin Tokens?'}
+              />
+            </h3>
+            <h6><strong>
+              <FormattedMessage
+                id={'about-token.earning'}
+                defaultMessage={'Earning Origin Tokens'}
+              /></strong>
+            </h6>
+            <p>
+              <FormattedMessage
+                id={'about-token.earning-text'}
+                defaultMessage={'You can earn Origin Tokens by following any of the steps below:'}
+              />
+            </p>
             <ul>
-              <li>Do this to earn tokens</li>
-              <li>Do this to earn tokens</li>
-              <li>Do this to earn tokens</li>
+              <li>
+                <FormattedMessage
+                  id={'about-token.earn-step-1'}
+                  defaultMessage={'Do this to earn tokens'}
+                />
+              </li>
+              <li>
+                <FormattedMessage
+                  id={'about-token.earn-step-2'}
+                  defaultMessage={'Do this to earn tokens'}
+                />
+              </li>
+              <li>
+                <FormattedMessage
+                  id={'about-token.earn-step-3'}
+                  defaultMessage={'Do this to earn tokens'}
+                />
+              </li>
             </ul>
           </div>
           <div className="col-md-6">
-            <br/>
-            <br/>
+            <br />
+            <br />
             <div className="video-placeholder"></div>
           </div>
         </div>
