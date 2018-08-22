c0dd5f6a5fce5c8bc6f1dfa5e1f6cf3b05f1d73d
natasha08 <natasha08@me.com>
2018-08-22 12:57:52 -0600

    display overview content in onboarding modal

--- src/components/onboarding-modal.js
+++ src/components/onboarding-modal.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
 import $ from 'jquery'
 
+const leftPanelInfo = [
+  { title: 'Overview', description: 'How to start selling on the Origin DApp' },
+  { title: 'Connect Wallet', description: 'Connect your wallet to start selling' },
+  { title: 'Get Origin Tokens', description: 'Connect your wallet to start selling' }
+]
+
 class OnboardingModal extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { currentPosition: 0 }
+  }
+
   componentDidMount() {
     this.$el = $(this.el)
 
@@ -23,7 +34,15 @@ class OnboardingModal extends Component {
     this.$el.modal('hide')
   }
 
+  changePosition(currentPosition) {
+    this.setState({ currentPosition });
+  }
+
   render() {
+    const { currentPosition } = this.state;
+
+    const selected = (p) => p === currentPosition ? 'selected' : '';
+
     return (
       <div
         ref={el => (this.el = el)}
@@ -36,24 +55,31 @@ class OnboardingModal extends Component {
           <div className="modal-content d-flex">
             <div className="row">
               <div className="flex-column col-4 text-left left-panel">
-                <div className="content">
-                  <div className="oval rounded-circle"> </div>
-                  <span>Overview</span>
-                  <p className="text-muted">How to start selling on the Origin DApp</p>
-                </div>
-                <div className="content">
-                  <div className="oval rounded-circle"> </div>
-                  <span>Connect Wallet</span>
-                  <p className="text-muted">Connect your wallet to start selling</p>
-                </div>
-                <div className="content">
-                  <div className="oval rounded-circle"> </div>
-                  <span>Get Origin Tokens</span>
-                  <p className="text-muted">Connect your wallet to start selling</p>
-                </div>
+                { leftPanelInfo.map(({title, description}, i) => (
+                  <div key={title}
+                    className={`content ${selected(i)}`}
+                    onClick={() => this.changePosition(i)}
+                  >
+                    <div className="oval rounded-circle"> </div>
+                    <span>{title}</span>
+                    <p className="text-muted">{description}</p>
+                  </div>
+                ))}
               </div>
-              <div style={{backgroundColor: 'white'}} className="flex-column col-8 right-panel">
-
+              <div className="flex-column col-8 right-panel">
+              <img src="/images/eth-tokens.svg" alt="eth-tokens" />
+                <div>
+                  <h3>Selling on the Origin DApp</h3>
+                  <p>
+                    In order to sell on the Origin DApp, you will need to connect
+                    a wallet in order to accept payment in ETH
+                  </p>
+                  <p>
+                    Payment for goods and services on the Origin DApp are always
+                    made in ETH
+                  </p>
+                  <button className='btn'>Connect a Wallet</button>
+                </div>
               </div>
             </div>
           </div>
