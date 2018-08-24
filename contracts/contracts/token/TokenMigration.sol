bdb1ab6f3cb8bd6e2b770c3ca6c81afb95d2862c
Cuong Do <cuongdo@noreply.github.com>
2018-08-23 21:49:09 -0700

    Remove demo code and add comments

--- contracts/contracts/token/TokenMigration.sol
+++ contracts/contracts/token/TokenMigration.sol
@@ -4,6 +4,12 @@ import "../../../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.
 import "../../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "./OriginToken.sol";
 
+/**
+ * @title Migrates balances from one token contract to another
+ * @dev Migrates all balances from one token contract to another. Both contracts
+ * must be pausable (to prevent changes during migration), and the target
+ * contract must support minting tokens.
+ */
 contract TokenMigration is Ownable {
   OriginToken public fromToken;
   OriginToken public toToken;
@@ -18,18 +24,23 @@ contract TokenMigration is Ownable {
     _;
   }
 
+  // @dev Public constructor
   constructor(OriginToken _fromToken, OriginToken _toToken) public {
     owner = msg.sender;
     fromToken = _fromToken;
     toToken = _toToken;
   }
 
+  // @dev Migrates a set of accounts, which should be limited in size so that
+  // the transaction is under the gas limit.
   function migrateAccounts(address[] _holders) public onlyOwner notFinished {
     for (uint i = 0; i < _holders.length; i++) {
         migrateAccount(_holders[i]);
     }
   }
   
+  // @dev Migrates the balance for a single address by minting the same number
+  // of new tokens the address had with the old token.
   function migrateAccount(address _holder) public onlyOwner notFinished {
     require(!migrated[_holder], "holder already migrated");
     uint256 balance = fromToken.balanceOf(_holder);
@@ -40,7 +51,7 @@ contract TokenMigration is Ownable {
     }
   }
   
-  // @dev finishes migration and transfers token ownership to new owner
+  // @dev Finishes migration and transfers token ownership to new owner.
   function finish(address _newTokenOwner) public onlyOwner notFinished {
     require(fromToken.totalSupply() == toToken.totalSupply(),
       "total token supplies do not match");
@@ -53,58 +64,3 @@ contract TokenMigration is Ownable {
   
   // TODO: revisit whether we want to migrate approvals
 }
-
-// DELETE ME BEFORE MERGE -->
-//
-// NOTE: you'll need a gas limit of ~7,000,000 to deploy this contract in Remix
-contract NewOriginToken is OriginToken {
-  constructor(uint256 _initialSupply) public OriginToken(_initialSupply) { }
-    
-  function answerToLife() public pure returns (uint256) {
-    return 42;
-  }
-}
-
-contract TestTokenMigrationDeleteMeBeforeMerge {
-  OriginToken public token;
-  address constant other = address(1);
-  uint256 constant initialSupply = 100;
-  uint256 constant transferAmount = 2;
-  
-  constructor() public {
-    token = new OriginToken(100);
-    require(token.transfer(other, transferAmount));
-  }
-  
-  function migrate() public {
-    OriginToken oldToken = token;
-    token = new NewOriginToken(0);
-    
-    // pause both contracts
-    oldToken.pause();
-    token.pause();
-    
-    // transfer token ownership to migration contract, because migrations need
-    // to mint new tokens
-    TokenMigration migration = new TokenMigration(oldToken, token);
-    token.transferOwnership(migration);
-
-    // migrate balances (NOT APPROVALS, which we may need to revisit)
-    address[] memory holders = new address[](2);
-    holders[0] = address(this);
-    holders[1] = other;
-    migration.migrateAccounts(holders);
-
-    // finish migration, which transfers the ownership of the token contract
-    // back to this test contract
-    migration.finish(address(this));
-    token.unpause();
-    
-    // verify
-    require(token.totalSupply() == initialSupply, "total supply wrong");
-    require(token.balanceOf(address(this)) == initialSupply - transferAmount,
-      "owner balance wrong");
-    require(token.balanceOf(other) == transferAmount, "other balance wrong");
-  }
-}
-// <-- DELETE ME BEFORE MERGE
