da5c76efcea3722500c0082d654dc18bc74dd183
natasha08 <natasha08@me.com>
2018-08-26 12:07:46 -0600

    WIP - complete/incomplete left panel status

--- src/components/onboardingModal/left-panel.js
+++ src/components/onboardingModal/left-panel.js
@@ -6,11 +6,13 @@ export default function({ steps, firstIncompleteStep }) {
 
     return matchingStep ? 'selected' : ''
   }
+  const completed = (complete=false) => complete ? 'complete' : 'incomplete'
+
   return (
     <div className="flex-column col-4 text-left left-panel">
-      { steps && steps.map(({name, description}, i) => (
+      { steps && steps.map(({name, description, complete}, i) => (
         <div key={name} className={`content ${selected(name)}`}>
-          <div className="oval rounded-circle"> </div>
+          <div className={`${completed(complete)} rounded-circle`}></div>
           <span>{name}</span>
           <p className="text-muted">{description}</p>
         </div>
