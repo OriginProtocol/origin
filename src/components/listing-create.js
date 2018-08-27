77d0247e9033ba7ef474e33313e4294fddf3e4a9
John <jordajm@gmail.com>
2018-08-27 16:28:32 -0600

    removing usused var

--- src/components/listing-create.js
+++ src/components/listing-create.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { FormattedMessage, defineMessages, injectIntl } from 'react-intl'
