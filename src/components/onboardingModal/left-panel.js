70cede9b78021649234d3322f7c4f5a9a6eeb01f
natasha08 <natasha08@me.com>
2018-08-29 08:30:05 -0600

    add checkmark when user progresses

--- src/components/onboardingModal/left-panel.js
+++ src/components/onboardingModal/left-panel.js
@@ -12,7 +12,7 @@ export default function({ steps, firstIncompleteStep }) {
     <div className="d-flex flex-column col-4 pr-1 pt-1 text-left left-panel">
       { steps && steps.map(({name, description, complete}, i) => (
         <div key={name} className={`content d-flex ${selected(name)}`}>
-          <div className={`${completed(complete)} rounded-circle col-2`}></div>
+          <div className={`${completed(complete)} rounded-circle col`}></div>
           <div className="panel-text d-flex flex-column">
             <span>{name}</span>
             <p>{description}</p>
