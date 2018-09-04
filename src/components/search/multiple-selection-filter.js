37df0027babd817f8805df092934682a475cfaf1
Domen Grabec <grabec@gmail.com>
2018-09-04 17:08:28 +0200

    provide multiple selection filter render solution when there are a lot of elements

--- src/components/search/multiple-selection-filter.js
+++ src/components/search/multiple-selection-filter.js
@@ -5,10 +5,27 @@ import schemaMessages from '../../schemaMessages/index'
 
 class MultipleSelectionFilter extends Component {
   render() {
+    let containerClass = 'd-flex flex-column'
+    let itemClass = 'form-check'
+
+    /* Render items into 1 column when under 9 elements,
+     * into 2 columns when between 10 and 19 elements,
+     * and into 3 columns when 20 or more.
+     */
+    if (this.props.multipleSelectionValues.length > 19) {
+      containerClass = 'd-flex flex-wrap three-column-container'
+      itemClass = 'form-check limit-checkbox-three-columns'
+    } else if (this.props.multipleSelectionValues.length > 9) {
+      containerClass = 'd-flex flex-wrap two-column-container'
+      itemClass = 'form-check limit-checkbox-two-columns'
+    }
+
+    const renderElementsIn2Rows = this.props.multipleSelectionValues.length > 9
+
     return (
-      <div className="d-flex flex-column flex-wrap" key={this.props.title}>
+      <div className={containerClass} key={this.props.title}>
       {this.props.multipleSelectionValues.map(multipleSelectionValue =>
-        <div className="form-check" key={multipleSelectionValue}>
+        <div className={itemClass} key={multipleSelectionValue}>
           <input type="checkbox" className="form-check-input" id={multipleSelectionValue}/>
           <label htmlFor={multipleSelectionValue}>
             {
