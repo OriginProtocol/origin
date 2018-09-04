caefa5f5e30e1646ed17fed53b2370c2d9578274
John <jordajm@gmail.com>
2018-09-04 00:16:13 -0600

    Wallet ETH price with five decimal palces

--- src/components/wallet.js
+++ src/components/wallet.js
@@ -111,7 +111,15 @@ class Wallet extends Component {
               <div className="d-flex align-items-start">
                 <img src="images/eth-icon.svg" role="presentation" />
                 <div className="amounts">
-                  <div className="eth">{balance || 0}<span className="symbol">ETH</span></div>
+                  <div className="eth">
+                    {
+                      `${Number(balance).toLocaleString(undefined, {
+                        minimumFractionDigits: 5,
+                        maximumFractionDigits: 5
+                      })}`
+                      || 0
+                    }&nbsp;
+                    <span className="symbol">ETH</span></div>
                   <div className="usd">{this.state.usdBalance} USD</div>
                 </div>
                 {withMenus &&
