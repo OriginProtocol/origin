c73749c3435df98e4fd00a6902eb9078358136b1
Tyler Yasaka <contact@tyleryasaka.me>
2018-09-07 22:42:28 -0700

    Begin testing marketplace resource

---
+++ test/resource_marketplace.test.js
@@ -0,0 +1,53 @@
+import Marketplace from '../src/resources/marketplace.js'
+import contractServiceHelper from './helpers/contract-service-helper'
+import IpfsService from '../src/services/ipfs-service.js'
+import { expect } from 'chai'
+import Web3 from 'web3'
+import listingValid from './data/listing-valid.json'
+
+class StoreMock {
+  constructor() {
+    this.storage = {}
+  }
+
+  get(key) {
+    return this.storage[key]
+  }
+
+  set(key, value) {
+    this.storage[key] = value
+  }
+}
+
+describe('Marketplace Resource', function() {
+  this.timeout(10000) // default is 2000
+  let marketplace
+
+  beforeEach(async () => {
+    const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+    const web3 = new Web3(provider)
+    // const accounts = await web3.eth.getAccounts()
+    const contractService = await contractServiceHelper(web3)
+    const ipfsService = new IpfsService({
+      ipfsDomain: '127.0.0.1',
+      ipfsApiPort: '5002',
+      ipfsGatewayPort: '8080',
+      ipfsGatewayProtocol: 'http'
+    })
+    const store = new StoreMock()
+    marketplace = new Marketplace({
+      contractService,
+      ipfsService,
+      store
+    })
+
+    await marketplace.createListing(listingValid)
+  })
+
+  describe('getListingsCount', () => {
+    it('should return the count of listings', async () => {
+      const listingCount = await marketplace.getListingsCount()
+      expect(listingCount).to.equal(1)
+    })
+  })
+})
