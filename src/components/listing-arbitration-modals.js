f965f37ed1f17b46fcea5a453587705c690e3c87
natasha08 <natasha08@me.com>
2018-09-13 07:23:25 -0600

    format strings & rename vars

--- src/components/listing-arbitration-modals.js
+++ src/components/listing-arbitration-modals.js
@@ -1,49 +1,109 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { FormattedMessage } from 'react-intl'
 import Modal from 'components/modal'
 
-export const RejectionModal = () => (
-  <Modal backdrop="static" className="arbitration-modal rejection" isOpen={true}>
+export const RejectionModal = ({ isOpen }) => (
+  <Modal backdrop="static" className="arbitration-modal rejection" isOpen={isOpen}>
     <div className="image-container">
       <img src="images/reject-icon.svg" role="presentation" />
     </div>
-    <p className="large-text">This offer has been rejected</p>
-    <span className="small-text">You&#8217;ve rejected this buyer&#8217;s offer,</span>
-    <p className="small-text">click below to go back to your listings.</p>
+    <p className="heading">
+      <FormattedMessage
+        id={'listing-arbitration.rejectionHeading'}
+        defaultMessage={'This offer has been rejected'}
+      />
+    </p>
+    <span className="text">
+      <FormattedMessage
+        id={'listing-arbitration.rejectionText1'}
+        defaultMessage={'You\'ve rejected this buyer\'s offer,'}
+      />
+    </span>
+    <p className="text">
+      <FormattedMessage
+        id={'listing-arbitration.rejectionText2'}
+        defaultMessage={'click below to go back to your listings.'}
+      />
+    </p>
     <div className="button-container">
       <Link to="/my-listings">
-        <button className="btn btn-clear">Back to your listings</button>
+        <button className="btn btn-clear">
+          <FormattedMessage
+            id={'listing-arbitration.rejectionListings'}
+            defaultMessage={'Back to your listings'}
+          />
+        </button>
       </Link>
     </div>
   </Modal>
 )
 
-export const ConfirmationModal = () => (
-  <Modal backdrop="static" className="arbitration-modal confirm" isOpen={true}>
-    <p className="large-text">Is there a problem?</p>
+export const ConfirmationModal = ({ isOpen }) => (
+  <Modal backdrop="static" className="arbitration-modal confirm" isOpen={isOpen}>
+    <p className="large-text">
+      <FormattedMessage
+        id={'listing-arbitration.confirmHeading'}
+        defaultMessage={'Is there a problem?'}
+      />
+    </p>
     <div className="small-text">
-      <span>Are you sure you want to report a problem?</span>
+      <span>
+        <FormattedMessage
+          id={'listing-arbitration.confirmText1'}
+          defaultMessage={'Are you sure you want to report a problem?'}
+        />
+      </span>
       <p>
-        This will start the conflict resolution process,
-        someone from Origin will be notified and all chat history will be made
-        public to a moderator.
+        <FormattedMessage
+          id={'listing-arbitration.confirmText2'}
+          defaultMessage={
+            `This will start the conflict resolution process,
+            someone from Origin will be notified and all chat history will be made
+            public to a moderator.`
+          }
+        />
       </p>
     </div>
     <div className="button-container">
-      <button className="btn btn-clear">Oops, no wait...</button>
-      <button className="btn btn-clear">Yes, please</button>
+      <button className="btn btn-clear">
+        <FormattedMessage
+          id={'listing-arbitration.confirmCancel'}
+          defaultMessage={'Oops, no wait...'}
+        />
+      </button>
+      <button className="btn btn-clear">
+        <FormattedMessage
+          id={'listing-arbitration.confirmSubmit'}
+          defaultMessage={'Yes, please'}
+        />
+      </button>
     </div>
   </Modal>
 )
 
-export const IssueModal = () => (
-  <Modal backdrop="static" className="arbitration-modal issue" isOpen={true}>
-    <p className="large-text">Describe your problem below</p>
+export const IssueModal = ({ isOpen }) => (
+  <Modal backdrop="static" className="arbitration-modal issue" isOpen={isOpen}>
+    <p className="large-text">
+      <FormattedMessage
+        id={'listing-arbitration.issueHeading'}
+        defaultMessage={'Describe your problem below'}
+      />
+    </p>
     <textarea rows="3"></textarea>
     <div className="button-container align-self-center">
-      <button className="btn btn-clear">Cancel</button>
-      <button className="btn btn-clear">Submit</button>
+      <button className="btn btn-clear">
+        <FormattedMessage
+          id={'listing-arbitration.issueCancel'}
+          defaultMessage={'Cancel'}
+        />
+      </button>
+      <button className="btn btn-clear">
+        <FormattedMessage
+          id={'listing-arbitration.issueSubmit'}
+          defaultMessage={'Submit'}
+        />
+      </button>
     </div>
   </Modal>
 )
