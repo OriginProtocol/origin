edd1964e7b96d74349832a0f393b5392b3937051
natasha08 <natasha08@me.com>
2018-09-12 20:43:55 -0600

    arbitration modal: when a seller rejects an offer

---
+++ src/components/listing-arbitration-modals.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Modal from 'components/modal'
+
+export function RejectionModal() {
+  return (
+    <Modal backdrop="static" className="rejection-modal" isOpen={true}>
+      <div className="image-container">
+        <span class="helper"></span><img src="images/reject-icon.svg" role="presentation" />
+      </div>
+      <p className="large-text">This offer has been rejected</p>
+      <span className="small-text">You&#8217;ve rejected this buyer&#8217;s offer,</span>
+      <p className="small-text">click below to go back to your listings.</p>
+      <div className="button-container">
+        <Link to="/my-listings">
+          <button className="btn btn-clear">
+            Back to your listings
+          </button>
+        </Link>
+      </div>
+    </Modal>
+  )
+}
+
+export function ConfirmationModal() {
+
+}
+
+export function IssueModal() {
+
+}
