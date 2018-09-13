c3a6ea9859de926b66a5d428a76730734b6de7c2
natasha08 <natasha08@me.com>
2018-09-12 20:52:13 -0600

    replace extra element w/ css

--- src/components/listing-arbitration-modals.js
+++ src/components/listing-arbitration-modals.js
@@ -3,27 +3,28 @@ import { Link } from 'react-router-dom'
 
 import Modal from 'components/modal'
 
-export function RejectionModal() {
-  return (
-    <Modal backdrop="static" className="rejection-modal" isOpen={true}>
-      <div className="image-container">
-        <span class="helper"></span><img src="images/reject-icon.svg" role="presentation" />
-      </div>
-      <p className="large-text">This offer has been rejected</p>
-      <span className="small-text">You&#8217;ve rejected this buyer&#8217;s offer,</span>
-      <p className="small-text">click below to go back to your listings.</p>
-      <div className="button-container">
-        <Link to="/my-listings">
-          <button className="btn btn-clear">
-            Back to your listings
-          </button>
-        </Link>
-      </div>
-    </Modal>
-  )
-}
+export const RejectionModal = () => (
+  <Modal backdrop="static" className="rejection-modal" isOpen={true}>
+    <div className="image-container">
+      <img src="images/reject-icon.svg" role="presentation" />
+    </div>
+    <p className="large-text">This offer has been rejected</p>
+    <span className="small-text">You&#8217;ve rejected this buyer&#8217;s offer,</span>
+    <p className="small-text">click below to go back to your listings.</p>
+    <div className="button-container">
+      <Link to="/my-listings">
+        <button className="btn btn-clear">
+          Back to your listings
+        </button>
+      </Link>
+    </div>
+  </Modal>
+)
 
 export function ConfirmationModal() {
+  <Modal backdrop="static" className="rejection-modal" isOpen={true}>
+
+  </Modal>
 
 }
 
