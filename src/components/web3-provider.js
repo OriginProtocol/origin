b9aad68a5f674746914d7f559ff1392e4718b880
Stan James <stan@wanderingstan.com>
2018-09-28 14:25:22 -0600

    Typo in var name

--- src/components/web3-provider.js
+++ src/components/web3-provider.js
@@ -447,9 +447,9 @@ class Web3Provider extends Component {
     if (currentProvider && networkId && isProduction && networkNotSupported) {
       const url = new URL(window.location)
       if (networkId === 1 && mainnetDappBaseUrl) {
-        window.location.href = mainnetBaseUrl + url.pathname + url.hash
+        window.location.href = mainnetDappBaseUrl + url.pathname + url.hash
       }
-      else if (networkId === 4 && rinkebyBaseUrl) {
+      else if (networkId === 4 && rinkebyDappBaseUrl) {
         window.location.href = rinkebyDappBaseUrl + url.pathname + url.hash
       }
     }
