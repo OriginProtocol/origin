98bff5f8c734ecf9ac72d1e67dd2bbf203ad320d
natasha08 <natasha08@me.com>
2018-10-17 12:37:37 -0600

    remove unused var

--- origin-js/test/resource_users.test.js
+++ origin-js/test/resource_users.test.js
@@ -14,7 +14,6 @@ chai.use(chaiAsPromised)
 chai.use(chaiString)
 const expect = chai.expect
 
-const base64Regex = /(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)/
 const issuerPrivatekey =
   '0000000000000000000000000000000000000000000000000000000000000001'
 
