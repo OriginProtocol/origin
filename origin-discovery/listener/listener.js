7f3c7b01243065965cf57f6b74511fc72fd06864
Daniel Von Fange <daniel@leancoder.com>
2018-10-19 00:53:15 -0400

    🎧 Listener - Add Discord webhook option (#777)
    * 🎧 Listener - Add Discord webhook option
    
    Sends marketplace events to a discord channel, with a different emoji
    for each event type.  
    
    (This functionality probably should move out of the listener
    to the notification system, as soon as we have one.)
    
    * Indent non-title lines for clearer separation between different events.
    
    * Document discord webhook option

--- origin-discovery/listener/listener.js
+++ origin-discovery/listener/listener.js
@@ -415,7 +415,68 @@ async function handleLog(log, rule, contractVersion, context) {
       await postToWebhook(context.config.webhook, json)
     })
   }
+
+  if (context.config.discordWebhook) {
+    postToDiscordWebhook(context.config.discordWebhook, output)
+    console.log('\n-- Discord WEBHOOK to ' + context.config.discordWebhook + ' --')
+  }
 }
+
+/**
+ * Posts a to discord channel via webhook.
+ * This functionality should move out of the listener 
+ * to the notification system, as soon as we have one.
+ */
+ async function postToDiscordWebhook(discordWebhookUrl, data){
+  const eventIcons = {
+    ListingCreated: ':trumpet:',
+    ListingUpdated: ':saxophone:',
+    ListingWithdrawn: ':x:',
+    ListingData: ':cd:',
+    ListingArbitrated: ':boxing_glove:',
+    OfferCreated: ':baby_chick:',
+    OfferWithdrawn: ':feet:',
+    OfferAccepted: ':bird:',
+    OfferDisputed: ':dragon_face:',
+    OfferRuling: ':dove:',
+    OfferFinalized: ':unicorn:',
+    OfferData: ':beetle:'
+  }
+
+  const personDisp = (p)=> {
+    let str = ''
+    if(p.profile && p.profile.firstName){
+      str += ' ' + p.profile.firstName
+    }
+    if(p.profile && p.profile.lastName){
+      str += ' ' + p.profile.lastName
+    }
+    str += ' ' + p.address
+    return str
+  }
+  const priceDisp = (listing) => {
+    const price = listing.price
+    return (price ? `${price.amount}${price.currency}` : '')
+  }
+
+  const icon = eventIcons[data.log.eventName] || ':dromedary_camel: '
+  const lines = []
+  const listing = data.related.listing
+  lines.push(`${icon} ${data.log.eventName} - ${listing.title} - ${priceDisp(listing)}`,)
+  if (data.related.offer !== undefined) { // Offer
+    lines.push(`  https://dapp.originprotocol.com/#/purchases/${data.related.offer.id}`,)
+    lines.push(`  Seller: ${personDisp(data.related.seller)}`)
+    lines.push(`  Buyer: ${personDisp(data.related.buyer)}`)
+  } else { // Listing
+    lines.push(`  https://dapp.originprotocol.com/#/listing/${listing.id}`,)
+    lines.push(`  Seller: ${personDisp(data.related.seller)}`)
+  }
+  const json = JSON.stringify(
+    { content: lines.join("\n")}
+  )
+  await postToWebhook(discordWebhookUrl, json)
+ }
+
 
 /**
  * Sends a blob of json to a webhook.
@@ -435,7 +496,8 @@ async function postToWebhook(urlString, json) {
   return new Promise((resolve, reject) => {
     const client = url.protocol === 'https:' ? https : http
     const req = client.request(postOptions, res => {
-      if (res.statusCode === 200) {
+      console.log(res.statusCode )
+      if (res.statusCode === 200 || res.statusCode === 204) {
         resolve()
       } else {
         reject()
@@ -556,6 +618,8 @@ process.argv.forEach(arg => {
 const config = {
   // Call webhook to process event.
   webhook: args['--webhook'],
+  // Call post to discord webhook to process event.
+  discordWebhook: args['--discord-webhook'],
   // Index events in the search index.
   elasticsearch: args['--elasticsearch'],
   // Index events in the database.
