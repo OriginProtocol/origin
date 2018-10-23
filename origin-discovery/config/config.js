c67a36d0e38fc78fcff6b2be952392844c164cfc
Franck Chastagnol <franck@originprotocol.com>
2018-10-22 23:00:39 -0700

    fix config

--- origin-discovery/config/config.js
+++ origin-discovery/config/config.js
@@ -10,42 +10,12 @@ module.exports = {
       freezeTableName: true,
       // Underscore style for field names.
       underscored: true
-    }
+    },
+    // Disable logging of SQL statements.
+    logging: false
   },
   'test': {
     use_env_variable:'DATABASE_URL',
-    define: {
-      // Add the timestamp attributes (updatedAt, createdAt).
-      timestamps: true,
-      // Disable the modification of table names.
-      freezeTableName: true,
-      // Underscore style for field names.
-      underscored: true
-    }
-  },
-  'production': {
-    use_env_variable: 'DATABASE_URL',
-    define: {
-      // Add the timestamp attributes (updatedAt, createdAt).
-      timestamps: true,
-      // Disable the modification of table names.
-      freezeTableName: true,
-      // Underscore style for field names.
-      underscored: true
-    }
-  }
-}
-
-
-
-module.exports = {
-  development: {
-    username: process.env.DATABASE_USERNAME || 'origin',
-    password: process.env.DATABASE_PASSWORD || 'origin',
-    database: process.env.DATABASE_NAME || 'origin',
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: process.env.DATABASE_PORT || 5432,
-    dialect: 'postgres',
     define: {
       // Add the timestamp attributes (updatedAt, createdAt).
       timestamps: true,
@@ -57,13 +27,8 @@ module.exports = {
     // Disable logging of SQL statements.
     logging: false
   },
-  production: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: 'postgres',
+  'production': {
+    use_env_variable: 'DATABASE_URL',
     define: {
       // Add the timestamp attributes (updatedAt, createdAt).
       timestamps: true,
