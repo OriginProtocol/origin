1edbea7970db61e816a00e44a51d0ddca2585fdd
tomlinton <tlinton@dockyard.nz>
2018-10-24 19:26:14 +1300

    Update discord notify function

--- deployment/utils/gcb-discord-notifications/index.js
+++ deployment/utils/gcb-discord-notifications/index.js
@@ -10,16 +10,16 @@ const hook = new Discord.WebhookClient(
 );
 
 // Subscribe is the main function called by Cloud Functions.
-module.exports.subscribe = (event, callback) => {
- const build = eventToBuild(event.data.data);
+module.exports.subscribe = async (event, callback) => {
+  const build = eventToBuild(event.data);
 
   const status = ['WORKING', 'SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
   if (status.indexOf(build.status) === -1) {
-    return callback();
+    return;
   }
 
   // Send message to Discord.
-  const webhook = createDiscordWebhook(build);
+  const webhook = await createDiscordWebhook(build);
   hook.send(webhook.message, webhook.options).then(callback);
 };
 
@@ -32,21 +32,18 @@ const eventToBuild = (data) => {
 const getBuildCommitData = async (build) => {
   let owner = 'OriginProtocol',
     repo = 'origin',
-    commit_sha = build.sourceProvenance.resolvedRepoSource.commitSha;
-  return await octokit.repos.getCommit({owner, repo, commit_sha});
+    sha = build.sourceProvenance.resolvedRepoSource.commitSha;
+  return await octokit.repos.getCommit({owner, repo, sha});
 };
 
 // createDiscordMessage create a message from a build object.
-const createDiscordWebhook = (build) => {
-  let message;
-  // Base options
-  let options = {
-    username: 'OriginCI',
-    avatarURL: 'https://bit.ly/2PgBEnm',
-  };
+const createDiscordWebhook = async (build) => {
+  let commitData = await getBuildCommitData(build);
 
-  let container = `\`${build.substitutions._CONTAINER}\``;
-  let buildId = `\`${build.id}\``;
+  let container = `${build.substitutions._CONTAINER}`;
+  let buildId = `${build.id}`;
+  let commitHash = `${build.sourceProvenance.resolvedRepoSource.commitSha}`
+  let shortHash = commitHash.substr(0, 8)
   let namespace
   switch(build.source.repoSource.branchName) {
     case 'master':
@@ -60,59 +57,40 @@ const createDiscordWebhook = (build) => {
       break;
   };
 
-  if (build.status === 'WORKING') {
-    let commitData = getBuildCommitData(build);
-    // Embed the GitHub commit data
-    message = `Deployment triggered for ${container} to ${namespace}`;
-    options.embeds = [{
-      author: commitData.author.name,
-      color: '16759552',
-      description: commitData.message,
-    }, {
+  let message;
+  // Base options
+  let options = {
+    username: 'OriginCI',
+    avatarURL: 'https://bit.ly/2PgBEnm',
+    embeds: [{
       fields: [{
         name: 'Build ID',
-        value: buildId
+        value: `[${buildId}](${build.logUrl})`
       }]
-    }];
+    }]
+  };
+
+  if (build.status === 'WORKING') {
+    // Embed the GitHub commit data
+    message = `Deployment triggered for \`${container}\` to \`${namespace}\``;
+    options.embeds[0].color = '16759552';
+    options.embeds.unshift({
+      author: {
+        name: commitData.data.commit.author.name,
+        icon_url: commitData.data.author.avatar_url
+      },
+      color: '16759552',
+      description: `${shortHash} - ${commitData.data.commit.message}`
+    });
   } else if (build.status === 'SUCCESS') {
-    message = `Deployment succeeded for ${container} to ${namespace}`;
-    options.embeds = [{
-      author: {
-        name: 'Google Cloud Build',
-        url: build.logUrl
-      },
-      fields: [{
-        name: 'Build ID',
-        value: buildId
-      }],
-      color: '3524352'
-    }];
+    message = `Deployment succeeded for \`${container}\` to \`${namespace}\``;
+    options.embeds[0].color = '3524352';
   } else if (build.status === 'FAILURE' || build.status === 'INTERNAL_ERROR') {
-    message = `Deployment failure for ${container} to ${namespace}`;
-    options.embeds = [{
-      author: {
-        name: 'Google Cloud Build',
-        url: build.logUrl
-      },
-      fields: [{
-        name: 'Build ID',
-        value: buildId
-      }],
-      color: '16724787'
-    }];
+    message = `Deployment failure for \`${container}\` to \`${namespace}\``;
+    options.embeds[0].color = '16724787';
   } else if (build.status === 'TIMEOUT') {
-    message = `Deployment timeout for ${container} to ${namespace}`;
-    options.embeds = [{
-      author: {
-        name: 'Google Cloud Build',
-        url: build.logUrl
-      },
-      fields: [{
-        name: 'Build ID',
-        value: buildId
-      }],
-      color: '16724787'
-    }];
+    message = `Deployment timeout for \`${container} to \`${namespace}\``;
+    options.embeds[0].color = '16724787';
   }
   return { message: message, options: options };
 }
