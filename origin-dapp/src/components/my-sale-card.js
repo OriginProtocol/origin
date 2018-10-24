4a8be664c786af99bc476c2694943a482d4f3160
Micah Alcorn <mail@micahalcorn.com>
2018-10-24 15:17:03 -0700

    Satisfy tests

--- origin-dapp/src/components/my-sale-card.js
+++ origin-dapp/src/components/my-sale-card.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import $ from 'jquery'
 import moment from 'moment'
-import { FormattedMessage, defineMessages, injectIntl } from 'react-intl'
+import { FormattedMessage, injectIntl } from 'react-intl'
 
 import { fetchUser } from 'actions/User'
 
