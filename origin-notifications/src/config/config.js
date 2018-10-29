9b0646b27b902b9a604604a7fa650162d31319c5
Franck Chastagnol <franck@originprotocol.com>
2018-10-29 11:31:42 -0700

    Cleaned up migration and sequelize config

---
+++ origin-notifications/src/config/config.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    use_env_variable:'DATABASE_URL',
+    define: {
+      // Add the timestamp attributes (updatedAt, createdAt).
+      timestamps: true,
+      // Disable the modification of table names.
+      freezeTableName: true,
+      // Underscore style for field names.
+      underscored: true
+    },
+    // Disable logging of SQL statements.
+    logging: false
+  },
+  'test': {
+    dialect: 'postgres',
+    use_env_variable:'DATABASE_URL',
+    define: {
+      // Add the timestamp attributes (updatedAt, createdAt).
+      timestamps: true,
+      // Disable the modification of table names.
+      freezeTableName: true,
+      // Underscore style for field names.
+      underscored: true
+    },
+    // Disable logging of SQL statements.
+    logging: false
+  },
+  'production': {
+    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
+    define: {
+      // Add the timestamp attributes (updatedAt, createdAt).
+      timestamps: true,
+      // Disable the modification of table names.
+      freezeTableName: true,
+      // Underscore style for field names.
+      underscored: true
+    },
+    // Disable logging of SQL statements.
+    logging: false
+  }
+}
