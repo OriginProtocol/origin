6572b89256f6a16fce7d2c748b18abffa5901aec
Tom Linton <tom@originprotocol.com>
2018-10-30 15:09:41 +1300

    Add notifications deployment config

---
+++ deployment/kubernetes/charts/origin/scripts/start-notification-server.sh
@@ -0,0 +1,12 @@
+set -e
+
+echo "Running database migrations for notification server"
+
+export DATABASE_URL=postgres://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}
+
+# Run database migrations
+node node_modules/.bin/sequelize db:migrate
+
+echo "Starting notiifcation server"
+
+node app.js
