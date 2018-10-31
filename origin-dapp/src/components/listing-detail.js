2be29743cfecb0cc568729b7f6623983ef5a2d0e
John <jordajm@gmail.com>
2018-10-30 23:12:21 -0600

    Listing detail fix

--- origin-dapp/src/components/listing-detail.js
+++ origin-dapp/src/components/listing-detail.js
@@ -458,7 +458,7 @@ class ListingsDetail extends Component {
           className={`container listing-container${loading ? ' loading' : ''}`}
         >
           <div className="row">
-            <div className="col-12 col-md-8 detail-info-box">
+            <div className="col-12">
               <div className="category placehold d-flex">
                 <div>{category}</div>
                 {!loading && (
@@ -478,6 +478,8 @@ class ListingsDetail extends Component {
                 )}
               </div>
               <h1 className="title placehold">{name}</h1>
+            </div>
+            <div className="col-12 col-md-8 detail-info-box">
               {(loading || (pictures && !!pictures.length)) && (
                 <div className="image-wrapper">
                   <img
