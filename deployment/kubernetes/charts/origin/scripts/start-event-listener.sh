5d877353f39853ff02c2fba3be6a1c4a8f6ab829
Franck Chastagnol <franck@originprotocol.com>
2018-11-02 13:58:33 -0700

    set listener-id arg

--- deployment/kubernetes/charts/origin/scripts/start-event-listener.sh
+++ deployment/kubernetes/charts/origin/scripts/start-event-listener.sh
@@ -8,4 +8,4 @@ export DATABASE_URL=postgres://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATAB
 node node_modules/.bin/sequelize db:migrate
 
 # Start event listener
-node listener/listener.js --elasticsearch --db --web3-url=${WEB3_URL} --ipfs-url=${IPFS_URL} --trail-behind-blocks=1 --webhook=http://${NAMESPACE}-notifications.${NAMESPACE}.svc.cluster.local:3456/events
+node listener/listener.js --listener-id=main --elasticsearch --db --web3-url=${WEB3_URL} --ipfs-url=${IPFS_URL} --trail-behind-blocks=1 --webhook=http://${NAMESPACE}-notifications.${NAMESPACE}.svc.cluster.local:3456/events
