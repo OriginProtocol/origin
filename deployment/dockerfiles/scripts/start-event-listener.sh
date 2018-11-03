d2525d26b2861e2b4be06b39006d098b48124282
Tom Linton <tom@originprotocol.com>
2018-11-04 11:43:19 +1300

    Automated deployment configuration (#845)
    * Add Google Cloud Function for Discord event relay
    
    * Rearrange origin-js dependencies
    
    * Move webpack-node-externals dependency
    
    * Further dependency rearranging for DApp
    
    * Update discord notify function
    
    * Move source map loader out of main config
    
    * Update dependencies
    
    * Simplify containers
    
    * Move truffle to production dependencies
    
    * Changing staging dapp to alpine
    
    * Add new secrets
    
    * Add cloudbuild deployment configuration
    
    * Cleanup old file
    
    * Fix faucet network id
    
    * Disable deploy for dev and staging via script
    
    * Update ipfs-proxy containers for cloudbuild
    
    * Fix container command order
    
    * Whitespace removal
    
    * Add Origin testnet details
    
    * Cleanup package.json for messaging and discovery
    
    * Update cloudbuild
    
    * Add deploy tag
    
    * Remove debug line
    
    * Use consistent shell
    
    * Restructure Dockerfiles for EnvKey integration everywhere
    
    * Cleanup from merge
    
    * Use EnvKey for everything on origin-bridge
    
    * Use EnvKey on event-listener
    
    * Move scripts inside Docker containers
    
    * Add notifications container
    
    * Update event-listener startup for EnvKey
    
    * Cleanup values
    
    * Combine defaults for DApp into webpack config and use null strings for empty values
    
    * EnvKey for DApp
    
    * Update secrets
    
    * Fix default discovery server URL
    
    * Update DApp EnvKey integration
    
    * Make deploy.sh handle structure changes
    
    * Whitespace removal
    
    * Tweak cloudbuild file
    
    * Node 8 everywhere
    
    * Fixes to cloudbuild
    
    * Push to deployed branch
    
    * Fix notification URL config
    
    * Fix typo
    
    * Add extra check to webhook delivery
    
    * More EnvKey
    
    * Add listener_id to EnvKey

---
+++ deployment/dockerfiles/scripts/start-event-listener.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Starts the event-listener
+
+set -e
+
+echo "Running database migrations for discovery"
+
+# Run database migrations
+node node_modules/.bin/sequelize db:migrate
+
+echo "Starting event listener"
+
+# Write the BLOCK_EPOCH to the continue file so we don't scan through blocks
+# starting at 0
+echo "{ \"lastLogBlock\": ${BLOCK_EPOCH} }" > ${CONTINUE_FILE}
+
+# Start event listener
+node listener/listener.js
