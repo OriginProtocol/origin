050bc8534ae149413731d779e3c345e92603692b
Tom Linton <tom@originprotocol.com>
2018-11-09 15:47:46 +1300

    Fix origin-js webpack config

--- origin-js/webpack.config.js
+++ origin-js/webpack.config.js
@@ -1,48 +1,4 @@
-var nodeExternals = require('webpack-node-externals');
-
-/*
- * Generate index.js used in `origin` npm package.
- * This is used by our DApp.
-*/
-var serverConfig = {
-  entry: ["babel-polyfill", './src/index.js'],
-  output: {
-    filename: './index.js',
-    libraryTarget: 'commonjs2'
-  },
-  mode: 'development',
-  devtool: 'inline-cheap-module-source-map',
-  target: 'node',
-  externals: [nodeExternals()],
-  resolve: {
-    /**
-    * Overriding the default to allow jsx to be resolved automatically.
-    */
-    extensions: ['.js', '.json', '.jsx'],
-    /**
-    * Access config from anywhere via `import settings from 'settings'``
-    */
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env', 'es2015', 'react'],
-          plugins: ['transform-class-properties']
-        }
-      }
-    ]
-  }
-}
-
-/*
- * Generate `origin.js` file that may be used standalone in browser
- * This is used by our docs, and included in our github release.
-*/
-var clientConfig = {
+var config = {
   entry: ["babel-polyfill", './src/index.js'],
   output: {
     filename: './origin.js',
@@ -65,7 +21,6 @@ var clientConfig = {
       }
     ]
   }
-
 }
 
-module.exports = [ serverConfig, clientConfig ];
+module.exports = config
