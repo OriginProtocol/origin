991e0186473d2f2d144c1c700acab7dd2b330dbe
Tom Linton <tom@originprotocol.com>
2018-11-10 19:40:20 +1300

    Change webpack config

--- origin-js/webpack.config.js
+++ origin-js/webpack.config.js
@@ -1,5 +1,42 @@
-var config = {
-  entry: ["babel-polyfill", './src/index.js'],
+const nodeExternals = require('webpack-node-externals');
+
+/*
+ * Generate `origin.node.js` which is used inside node apps.`
+*/
+const serverConfig = {
+  entry: ["@babel/polyfill", './src/index.js'],
+  output: {
+    filename: './origin.node.js',
+    libraryTarget: 'commonjs2'
+  },
+  mode: 'development',
+  target: 'node',
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: ['.js', '.json', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-class-properties']
+        }
+      }
+    ]
+  }
+}
+
+/*
+ * Generate `origin.js` file that may be used standalone in browser
+ * This is used by our docs, and included in our github release.
+*/
+
+const clientConfig = {
+  entry: ["@babel/polyfill", './src/index.js'],
   output: {
     filename: './origin.js',
     libraryTarget: 'var',
@@ -14,9 +51,14 @@ var config = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-class-properties']
+        }
       }
     ]
   }
 }
 
-module.exports = config
+module.exports = [ serverConfig, clientConfig ]
+
