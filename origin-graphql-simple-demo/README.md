a351bb8fd72c0a221439a0eac53e55ff955b6e47
Nick Poulden <nick@poulden.com>
2018-11-11 19:25:21 -0700

    Move to experimental dir. Add READMEs

--- origin-graphql-simple-demo/README.md
+++
@@ -1,3 +0,0 @@
-# Simple Origin GraphQL Demo
-
-See [here](https://ipfs.originprotocol.com/ipfs/QmYkiVzqmqxEmmMjCXLWFDJHhSGXCCHZ8FL3uwSHKKk4kr) for demo
--- origin-graphql/src/links/schema.js
+++
@@ -1,8 +0,0 @@
-import { makeExecutableSchema } from 'graphql-tools'
-
-import typeDefs from '../typeDefs'
-import resolvers from '../resolvers'
-
-const schema = makeExecutableSchema({ typeDefs, resolvers })
-
-export default schema
