b07b180e1aaf13a27dcde95426b45e75ba49f857
Yu Pan <yupan@originprotocol.com>
2018-11-14 14:12:09 -0800

    Added in origin-mobile app

--- origin-mobile/index.js
+++ origin-mobile/index.js
@@ -1,4 +1,12 @@
+console.log("init import")
 import { AppRegistry } from 'react-native'
+console.log("react native imported")
 import './global'
+console.log("global imported")
+import {EthNotificationTypes} from 'origin/common/enums'
+import origin from 'origin'
+/*
 import App from './src/App'
+console.log("app imported")
 AppRegistry.registerComponent('OriginCatcher', () => App);
+*/
