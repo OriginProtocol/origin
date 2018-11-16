c9897391133134cf65550ad4f4107a5c23889e6c
Nick Poulden <nick@poulden.com>
2018-11-15 18:53:15 -0700

    Fix contract formatting

--- origin-contracts/contracts/token/TestToken.sol
+++ origin-contracts/contracts/token/TestToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-import '../../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+import "../../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 /**
  * This is intended to be used for testing integration between the Marketplace
@@ -8,15 +8,15 @@ import '../../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardT
  */
 
 contract TestToken is StandardToken {
-  string public name;
-  string public symbol;
-  uint8 public decimals;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
 
-  constructor(string _name, string _symbol, uint8 _decimals, uint _supply) public {
-    name = _name;
-    symbol = _symbol;
-    decimals = _decimals;
-    totalSupply_ = _supply;
-    balances[msg.sender] = _supply;
-  }
+    constructor(string _name, string _symbol, uint8 _decimals, uint _supply) public {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply_ = _supply;
+        balances[msg.sender] = _supply;
+    }
 }
