d8cc7867966347d570ff7630b97163a659c03d7e
Natasha <natasha08@me.com>
2018-11-15 20:43:07 -0700

    hide the onboarding modals when on mobile (#865)
    * hide the onboarding modals when on mobile
    
    * add isMobile to Redux
    combine isMobile and onMobile
    
    * Consolidate mobile detection

--- origin-dapp/src/reducers/App.js
+++ origin-dapp/src/reducers/App.js
@@ -16,7 +16,7 @@ const initialState = {
   notificationsSoftPermission: localStorage.getItem('notificationsPermissionResponse'),
   // which soft permission request prompt to display: buyer, seller, warning
   notificationsSubscriptionPrompt: null,
-  onMobile: null,
+  mobileDevice: null,
   pushNotificationsSupported: !!(process.env.NOTIFICATIONS_KEY && process.env.NOTIFICATIONS_URL),
   serviceWorkerRegistration: null,
   translations: {
@@ -59,7 +59,7 @@ export default function App(state = initialState, action = {}) {
     return { ...state, notificationsSubscriptionPrompt: action.role }
 
   case AppConstants.ON_MOBILE:
-    return { ...state, onMobile: action.device }
+    return { ...state, mobileDevice: action.device }
 
   case AppConstants.SAVE_SERVICE_WORKER_REGISTRATION:
     return { ...state, serviceWorkerRegistration: action.registration }
