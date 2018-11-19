3114616868f528d70af19718145c9d018ba659ff
Domen Grabec <grabec@gmail.com>
2018-11-19 17:49:03 +0100

    handle case when ipfsData is undefined

--- origin-js/src/models/offer.js
+++ origin-js/src/models/offer.js
@@ -39,7 +39,7 @@ export class Offer {
       events: chainData.events,
       refund: chainData.refund,
       totalPrice: chainData.totalPrice,
-      unitsPurchased: ipfsData.unitsPurchased
+      unitsPurchased: ipfsData ? ipfsData.unitsPurchased : undefined
     })
   }
 
