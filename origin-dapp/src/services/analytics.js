c26dbfafe898c970558bc61098cc18e99c142add
Micah Alcorn <mail@micahalcorn.com>
2018-11-21 08:13:50 -0800

    Repair comment

--- origin-dapp/src/services/analytics.js
+++ origin-dapp/src/services/analytics.js
@@ -2,7 +2,7 @@ const gaTrackingId = process.env.GA_TRACKING_ID || 'UA-106384880-2'
 
 /**
  * Records an analytics event
- * @param {string} category Type of ojbect this event is about
+ * @param {string} category Type of object this event is about
  * @param {string} action Verb of the action taken that fired this event
  * @param {string} [label=undefined]
  * @param {number} [value=undefined]
