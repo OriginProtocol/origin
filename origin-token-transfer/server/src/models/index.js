a87cb8b924403653459f1245843eec000d51b7fb
Tom Linton <tom@originprotocol.com>
2018-11-21 15:44:28 +1300

    Fix Sequelize config name

--- origin-token-transfer/server/src/models/index.js
+++ origin-token-transfer/server/src/models/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require('../../config/config.json')[env]
+const config = require('../../config/config.js')[env]
 const db = {}
 
 let sequelize
