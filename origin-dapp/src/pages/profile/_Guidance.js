ae366d8ddb10b6b0e0d97ae4bdb5e473a270dd09
Domen Grabec <grabec@gmail.com>
2018-11-23 13:19:55 +0100

    make guidance box collapsable and dismissable on mobile

--- origin-dapp/src/pages/profile/_Guidance.js
+++ origin-dapp/src/pages/profile/_Guidance.js
@@ -3,43 +3,112 @@ import { FormattedMessage, injectIntl } from 'react-intl'
 import { connect } from 'react-redux'
 
 class Guidance extends Component {
+  constructor() {
+    super()
+    this.state = {
+      expanded: false,
+      dismissed: false
+    }
+
+    this.onLearnMoreClick = this.onLearnMoreClick.bind(this)
+    this.onDismissClick = this.onDismissClick.bind(this)
+  }
+
+  onLearnMoreClick(e) {
+    e.stopPropagation()
+    this.setState({ expanded: !this.state.expanded })
+  }
+
+  onDismissClick(e) {
+    e.stopPropagation()
+    this.setState({ dismissed: true })
+  }
+
   render() {
     const { mobileDevice } = this.props
-    let wrapperClass, imageWrapperClass, textClass
-    
+
+    const strongVerifyProfile = (
+      <strong>
+        <FormattedMessage
+          id={'_Guidance.verifyingYourProfile'}
+          defaultMessage={'Verifying your profile'}
+        />
+      </strong>
+    )
+    const guidanceMessage = (<FormattedMessage
+      id={'_Guidance.content'}
+      defaultMessage={
+        '{verifyingYourProfile} allows other users to know that you are a real person and increases the chances of successful transactions on Origin.'
+      }
+      values={{
+        verifyingYourProfile: strongVerifyProfile
+      }}
+    />)
+    const shortGuidanceMessage = (<FormattedMessage
+      id={'_Guidance.contentShort'}
+      defaultMessage={
+        '{verifyingYourProfile} allows others to know and trust you.'
+      }
+      values={{
+        verifyingYourProfile: strongVerifyProfile
+      }}
+    />)
+
+    let wrapperClass, imageWrapperClass, textClass, guidanceText
+    let showGuidance = true
     if (mobileDevice){
       wrapperClass = 'guidance row'
-      imageWrapperClass = 'pl-0 col-4'
-      textClass = 'col-8'
+      imageWrapperClass = 'pl-0 pr-0 col-2'
+      textClass = 'col-10'
+      guidanceText = this.state.expanded ? guidanceMessage : shortGuidanceMessage
+      showGuidance = !this.state.dismissed
+
     } else {
       wrapperClass = 'guidance'
       imageWrapperClass = 'image-container text-center'
       textClass = ''
+      guidanceText = guidanceMessage
     }
 
-    return (
+    return showGuidance && (
       <div className={wrapperClass}>
         <div className={imageWrapperClass}>
           <img src="images/identity.svg" alt="identity icon" />
         </div>
-        <p className={textClass}>
-          <FormattedMessage
-            id={'_Guidance.content'}
-            defaultMessage={
-              '{verifyingYourProfile} allows other users to know that you are a real person and increases the chances of successful transactions on Origin.'
-            }
-            values={{
-              verifyingYourProfile: (
-                <strong>
-                  <FormattedMessage
-                    id={'_Guidance.verifyingYourProfile'}
-                    defaultMessage={'Verifying your profile'}
-                  />
-                </strong>
-              )
-            }}
-          />
-        </p>
+        <div className={textClass}>
+          <p>
+            {guidanceText}
+          </p>
+          {mobileDevice && (<div>
+              <a
+                className="pr-3"
+                onClick={this.onLearnMoreClick}
+                href="javascript:void(0);"
+              >
+              {this.state.expanded ?
+                (<FormattedMessage
+                  id={'_Guidance.showLess'}
+                  defaultMessage={'Show less'}
+                />)
+              :
+                (<FormattedMessage
+                  id={'_Guidance.learnMore'}
+                  defaultMessage={'Learn more'}
+                />)
+              }
+              </a>
+              <a
+                onClick={this.onDismissClick}
+                href="javascript:void(0);"
+              >
+                <FormattedMessage
+                  id={'_Guidance.dismiss'}
+                  defaultMessage={'Dismiss'}
+                />
+              </a>
+            </div>
+          )}
+        </div>
       </div>
     )
   }
