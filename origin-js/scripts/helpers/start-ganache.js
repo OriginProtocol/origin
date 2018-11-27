f952ccd6bed387631121b0ab49f21ab28cd54d80
Tom Linton <tom@originprotocol.com>
2018-11-28 07:07:33 +1300

    blockTime 0

--- origin-js/scripts/helpers/start-ganache.js
+++ origin-js/scripts/helpers/start-ganache.js
@@ -9,7 +9,7 @@ const startGanache = () => {
       default_balance_ether: 100,
       network_id: 999,
       seed: 123,
-      blockTime: process.env.CI ? 0 : 1,
+      blockTime: 0,
       mnemonic:
         'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
     })
