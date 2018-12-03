c9ef9cbd601b87dff80e68f4b84ee84ec9b922b7
Tom Linton <tom@originprotocol.com>
2018-12-03 13:35:26 +1300

    Remove relative import for origin-js test

--- origin-js/scripts/helpers/test-javascript.js
+++ origin-js/scripts/helpers/test-javascript.js
@@ -3,14 +3,14 @@ const { spawn } = require('child_process')
 const testJavascript = () => {
   return new Promise((resolve, reject) => {
     const mocha = spawn(
-      './node_modules/.bin/mocha',
+      'mocha',
       [
         '-r',
         '@babel/register',
         '-r',
         '@babel/polyfill',
         '--timeout',
-        '20000',
+        '10000',
         '--exit',
         '-b'
       ],
