7a3cec382a446ff958a9b64f55c4999a43ed2b0e
Tom Linton <tom@originprotocol.com>
2018-12-03 13:49:04 +1300

    Remove debugging log

--- origin-js/src/services/contract-service.js
+++ origin-js/src/services/contract-service.js
@@ -301,7 +301,6 @@ class ContractService {
       if (!promiseStatus.receiptReceived && promiseStatus.txnFoundCounter > 1){
         // unfortunately transaction logs in transactionReceipt do not contain all needed event information
         const getEventsEmittedByTransaction = async () => {
-          console.log(contract)
           let events = await contract.getPastEvents(
             'allEvents',
             {
