ae97104bcf5d70b8c1b0449f3e7e0f7b0f3d100b
Tom Linton <tom@originprotocol.com>
2018-12-04 17:29:21 +1300

    Rename test folder

--- origin-tests/test/initial-state.js
+++ origin-tests/test/initial-state.js
@@ -1,8 +1,8 @@
 const chai = require('chai')
 const assert = chai.assert
 
-const db = require('../origin-discovery/src/models')
-const Origin = require('origin')
+const db = require('origin-discovery/src/models')
+const Origin = require('origin').default
 const Web3 = require('web3')
 const web3Provider = new Web3.providers.HttpProvider('http://origin:8545')
 const web3 = new Web3(web3Provider)
@@ -14,28 +14,29 @@ describe('initial state', () => {
     assert.equal(count.length, 5)
   })
 
-  it('should return 5 listings', async () => {
+  it('should return 5 listings in performance mode', async () => {
     const origin = new Origin({
-      ipfsDomain: 'localhost',
+      ipfsDomain: 'ipfs-proxy',
       ipfsGatewayProtocol: 'http',
       ipfsGatewayPort: 9999,
+      discoveryServerUrl: 'http://origin-discovery:4000/graphql',
       web3: web3,
       perfModeEnabled: true
     })
     const listings = await origin.marketplace.getListings()
-    console.log(listings)
+    assert.equal(listings.length, 5)
   })
 
-  it('should return 5 listings in performance mode', async () => {
+  it('should return 5 listings', async () => {
     const origin = new Origin({
-      ipfsDomain: 'localhost',
+      ipfsDomain: 'ipfs-proxy',
       ipfsGatewayProtocol: 'http',
       ipfsGatewayPort: 9999,
       web3: web3,
       perfModeEnabled: false
     })
     const listings = await origin.marketplace.getListings()
-    console.log(listings)
+    assert.equal(listings.length, 5)
   })
 
 })
