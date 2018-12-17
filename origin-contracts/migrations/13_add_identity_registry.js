f3fe7c6053466f4bb13c77701251d5429ba80b01
Franck Chastagnol <franck@originprotocol.com>
2018-12-17 12:45:07 -0800

    add migration

---
+++ origin-contracts/migrations/13_add_identity_registry.js
@@ -0,0 +1,11 @@
+var ProfileRegistry = artifacts.require("./ProfileRegistry.sol")
+
+module.exports = function(deployer, network) {
+  return deployer.then(() => {
+    return deployContracts(deployer)
+  })
+}
+
+async function deployContracts(deployer) {
+  await deployer.deploy(ProfileRegistry)
+}
