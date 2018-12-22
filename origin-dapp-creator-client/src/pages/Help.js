4cbb0ac26bc19a26e48a4727cde6bebfd529f9b2
Tom Linton <tom@originprotocol.com>
2018-12-22 13:55:50 +1300

    Update help page

--- origin-dapp-creator-client/src/pages/Help.js
+++ origin-dapp-creator-client/src/pages/Help.js
@@ -4,6 +4,7 @@ class Help extends React.Component {
   render () {
     return (
       <>
+        {/*
         <h2>Configuring Custom Domains</h2>
         <p>To configure a custom domain you will need to add two DNS entries:</p>
         <ul>
@@ -11,10 +12,12 @@ class Help extends React.Component {
           <li>TXT for config.yourdomain.com to "dnslink=/ipfs/{"<ipfs_hash>"}"</li>
         </ul>
         <p>You should have received an IPFS hash for your configuration during creation process.</p>
-        <h2>Configuring Subdomains</h2>
-        <p>If you elected to use a subdomain on orgindapp.com there is no configuration necessary.</p>
-        <h3>My subdomain is not working?</h3>
-        <p>Subdomains rely on DNS, and changes to DNS can take some time to propagate. If you think something else may be wrong, please ping is in our #engineering channel on Discord.</p>
+        */}
+        <h2>Troubleshooting</h2>
+        <h3>My subdomain is not working</h3>
+        <p>Subdomains rely on DNS, and changes to DNS can take some time to propagate.</p>
+        <h3>I'm having another problem</h3>
+        <p>Our #engineering channel on our <a href="https://discord.gg/jyxpUSe">Discord server</a> is full of helpful engineers.</p>
       </>
     )
   }
