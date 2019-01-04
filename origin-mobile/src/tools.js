aad813b1d469dd1e8eb4bc10084d62e9e3ec3d69
crazybuster <crazybuster@gmail.com>
2019-01-04 15:26:16 -0800

    Added support for https on staging config (#1168)

--- origin-mobile/src/tools.js
+++ origin-mobile/src/tools.js
@@ -1,9 +1,7 @@
 import {NativeModules, AsyncStorage} from 'react-native'
 
 let REMOTE_LOCAL_SERVER = null
-console.log('getting local serv ip')
 const LOCALHOST_SERVER_IP = __DEV__ ? NativeModules.SourceCode.scriptURL.split('://')[1].split('/')[0].split(':')[0] : null
-console.log(`Local serv ip: ${LOCALHOST_SERVER_IP}`)
 
 function setRemoteLocal(server_str) {
   REMOTE_LOCAL_SERVER = server_str
@@ -12,7 +10,7 @@ function setRemoteLocal(server_str) {
 function localfy(str) {
   const local_ip = REMOTE_LOCAL_SERVER || LOCALHOST_SERVER_IP
 
-  if (local_ip) {
+  if (str && local_ip) {
     return str.replace('localhost', local_ip).replace(/127\.0\.0\.1(?=[^0-9]|$)/, local_ip)
   } else {
     return str
