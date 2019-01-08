f690d7a6f99c215f08acfd7bb1251059ea822276
Domen Grabec <grabec@gmail.com>
2019-01-08 13:13:06 +0100

    fix tooltips in boost slider and purchase progress

--- origin-dapp/src/components/tooltip.js
+++ origin-dapp/src/components/tooltip.js
@@ -11,13 +11,18 @@ class Tooltip extends Component {
   }
 
   render() {
-    const { trigger, placement, content, children, delay } = this.props
+    const { trigger, placement, content, children, delay, triggerClass } = this.props
     const overlay = <TooltipBS id={this.id}>{content}</TooltipBS>
     const overlayProps = { trigger, placement, overlay, delay }
 
     return (
       <OverlayTrigger {...overlayProps}>
-        <Fragment>{children}</Fragment>
+        <a
+          href="javascript:void(0);"
+          className={triggerClass}
+        >
+          {children}
+        </a>
       </OverlayTrigger>
     )
   }
