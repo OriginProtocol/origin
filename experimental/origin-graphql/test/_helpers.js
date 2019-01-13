047111847baef739ddba41e64218e283437146c4
Nick Poulden <nick@poulden.com>
2019-01-13 13:20:12 -0700

    Create listing updates. origin-services to manage ganache / ipfs

--- experimental/origin-graphql/test/_helpers.js
+++ experimental/origin-graphql/test/_helpers.js
@@ -4,24 +4,35 @@ import contracts from '../src/contracts'
 import queries from './_queries'
 import client from '../src/index'
 
-// Convenience function to run a mutation and return the transaction receipt
+// Convenience function to run a mutation and return either the raw transaction
+// receipt, or an object containing the returned events and their params & values
 export async function mutate(mutation, variables, getEvents) {
+  // First, run the mutation and get the result
   const result = await client.mutate({ mutation, variables })
+
+  // Assume mutation returns an object with a transaction hash in the format
+  // data.mutationName.id
   const key = Object.keys(result.data)[0]
   const id = result.data[key].id
-  const receipt = await client.query({
-    query: queries.GetReceipt,
-    variables: {
-      id
-    }
-  })
+
+  // If we didn't want an events object, just return the raw receipt
   if (!getEvents) {
     return await contracts.web3.eth.getTransactionReceipt(id)
   }
+
+  // Query GraphQL for the receipt. This will include decoded event params.
+  const receipt = await client.query({
+    query: queries.GetReceipt,
+    variables: { id }
+  })
+
   const events = get(receipt, 'data.web3.transactionReceipt.events')
   if (!events) {
     return
   }
+
+  // Produce a nice object in the following format:
+  // { event1Name: { param1: value, param2: value }, event2Name: { param1: value } }
   const res = events.reduce((m, o) => {
     m[o.event] = o.returnValuesArr.reduce((am, ao) => {
       am[ao.field] = ao.value
@@ -29,5 +40,6 @@ export async function mutate(mutation, variables, getEvents) {
     }, {})
     return m
   }, {})
+
   return res
 }
