078570434a9c1ec371876a78a3d38bb06661afa2
Micah Alcorn <mail@micahalcorn.com>
2019-01-15 15:46:38 -0800

    Prepare UI for public consumption

--- origin-mobile/src/utils/currencies.js
+++ origin-mobile/src/utils/currencies.js
@@ -1,11 +1,11 @@
 const IMAGES_PATH = '../../assets/images/'
 
 export default currencies = {
-  dai: {
-    color: '#fec100',
-    icon: require(`${IMAGES_PATH}dai-icon.png`),
-    name: 'Maker Dai',
-  },
+  // dai: {
+  //   color: '#fec100',
+  //   icon: require(`${IMAGES_PATH}dai-icon.png`),
+  //   name: 'Maker Dai',
+  // },
   eth: {
     color: '#7a26f3',
     icon: require(`${IMAGES_PATH}eth-icon.png`),
