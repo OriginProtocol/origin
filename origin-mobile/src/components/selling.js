f2925d49b00ba6fa49c0689a85449815adc59714
Micah Alcorn <mail@micahalcorn.com>
2019-01-22 07:53:49 -0800

    Wallet UX Improvements (#1272)
    * Update carousel copy
    
    * Prevent duplicate button clicks
    
    * Improve notifications requests
    
    * Updated build number

--- origin-mobile/src/components/selling.js
+++ origin-mobile/src/components/selling.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Alert, Dimensions, Image, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
 
 import OriginButton from 'components/origin-button'
 
