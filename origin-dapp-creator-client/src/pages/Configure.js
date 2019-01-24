0ade13b24b0ffad4aed9c6b8b1f042672d0f3e78
Tom Linton <tom@originprotocol.com>
2019-01-24 14:15:08 -0800

    Prototype category tree view

--- origin-dapp-creator-client/src/pages/Configure.js
+++ origin-dapp-creator-client/src/pages/Configure.js
@@ -3,6 +3,7 @@
 import React from 'react'
 
 import Redirect from 'components/Redirect'
+import listingSchemaMetadata from 'origin-dapp/src/utils/listingSchemaMetadata'
 
 class Configure extends React.Component {
   constructor(props) {
@@ -10,13 +11,18 @@ class Configure extends React.Component {
 
     this.state = {
       config: props.config,
-      redirect: null,
-      filterByTypeEnabled: false
+      expandedCategories: [],
+      filterByTypeEnabled: false,
+      listingTypes: listingSchemaMetadata.listingTypes,
+      listingSchemasByCategory: listingSchemaMetadata.listingSchemasByCategory,
+      redirect: null
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.isExpandedCategory = this.isExpandedCategory.bind(this)
     this.toggleFilterByOwn = this.toggleFilterByOwn.bind(this)
     this.toggleFilterByType = this.toggleFilterByType.bind(this)
+    this.toggleCategory = this.toggleCategory.bind(this)
   }
 
   toggleFilterByOwn (event) {
@@ -39,6 +45,22 @@ class Configure extends React.Component {
     this.setState({
       filterByTypeEnabled: event.target.checked
     })
+  }
+
+  toggleCategory (type) {
+    if (this.state.expandedCategories.includes(type)) {
+      this.setState((prevState) => {
+        return { expandedCategories: prevState.expandedCategories.filter((x) => x !== type) }
+      })
+    } else {
+      this.setState((prevState) => {
+        return { expandedCategories: [...prevState.expandedCategories, type] }
+      })
+    }
+  }
+
+  isExpandedCategory(type) {
+    return this.state.expandedCategories.includes(type)
   }
 
   async handleSubmit () {
@@ -65,14 +87,32 @@ class Configure extends React.Component {
               onClick={this.toggleFilterByOwn} />
           </div>
 
-          <div className="option">
-            Limit by type
+          <div className={`option category-select ${this.state.filterByTypeEnabled ? 'expanded' : 'collapsed' }`}>
+            Only use listings from specific categories
             <input className="form-check-input"
               type="checkbox"
               onClick={this.toggleFilterByType} />
           </div>
+
           {this.state.filterByTypeEnabled &&
-            <div>Category</div>
+            <div className="category-dropdown">
+              {this.state.listingTypes.map((listingType, i) =>
+                <>
+                  <div className={`category ${this.isExpandedCategory(listingType.type) ? 'expanded' : 'collapsed'}`}
+                      onClick={() => this.toggleCategory(listingType.type)}
+                      key={i}>
+                      <input type="checkbox" /> {listingType.translationName.defaultMessage}
+                  </div>
+                  {this.isExpandedCategory(listingType.type) &&
+                    this.state.listingSchemasByCategory[listingType.type].map((listingSchema, y) =>
+                      <div className="subcategory">
+                        <input type="checkbox" />{listingSchema.translationName.defaultMessage}
+                      </div>
+                    )
+                  }
+                </>
+              )}
+            </div>
           }
         </div>
 
@@ -113,6 +153,59 @@ require('react-styl')(`
 
   .option .form-check-input
     right: 1rem
+
+  .category-dropdown
+    padding: 1rem
+    border: 1px solid var(--light)
+    margin-top: -5px
+    border-bottom-left-radius: var(--default-radius)
+    border-bottom-right-radius: var(--default-radius)
+    background-color: var(--pale-grey-four)
+
+  .category-dropdown ul
+    margin-bottom: 0
+
+  .category-select.expanded
+    border-bottom-left-radius: 0
+    border-bottom-right-radius: 0
+
+  .category
+    position: relative
+    padding-left: 1.2rem
+    cursor: pointer
+
+  .category
+    input
+      margin-right: 0.5rem
+
+  .category.collapsed:before
+    content: ''
+    width: 0
+    height: 0
+    border-top: 5px solid transparent
+    border-bottom: 5px solid transparent
+    border-left: 5px solid black
+    position: absolute
+    left: 0
+    top: 0.5rem
+
+  .category.expanded:before
+    content: ''
+    width: 0
+    height: 0
+    border-left: 5px solid transparent
+    border-right: 5px solid transparent
+    border-top: 5px solid black
+    position: absolute
+    left: 0
+    top: 0.75rem
+
+  .subcategory
+    padding-left: 2rem
+
+  .subcategory
+    input
+      margin-right: 0.5rem
 `)
 
 export default Configure
