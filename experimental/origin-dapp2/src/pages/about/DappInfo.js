03cb1ee89f658d37d924ad3b81732a240f11c5a1
Daniel Von Fange <daniel@leancoder.com>
2019-01-24 10:01:55 -0500

    Fix lint

--- experimental/origin-dapp2/src/pages/about/DappInfo.js
+++ experimental/origin-dapp2/src/pages/about/DappInfo.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
-import { Query } from "react-apollo";
-import query from "queries/Config";
+import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import query from 'queries/Config'
 
 const DAPP_VERSION = require('../../../package.json').version
 
-
-
-const sectionThead = ({title}) => (<thead>
+const sectionThead = ({ title }) => (<thead>
         <tr><th colSpan="2"><h3>{title}</h3></th></tr>
     </thead>)
-const dataTr = ({key, value}) => <tr key={key}><th>{key}</th><td>{value}</td></tr>
+const dataTr = ({ key, value }) => <tr key={key}><th>{key}</th><td>{value}</td></tr>
+
 
 class DappInfo extends Component {
   render() {
     return <div className="container">
       <h1>About Dapp</h1>
-      <p>Developer information about this Dapp's current build and configuration.</p>
-      <table className="ConfigTable">
+      <p>Developer information about this Dapp&#39;s current build and configuration.</p>
+      <table className="config-table">
 
-        {sectionThead({title: "Dapp Config"})}
+        {sectionThead({ title: 'Dapp Config' })}
         <tbody>
-            {dataTr({ key:"DAPP Version", value: DAPP_VERSION })}
+            {dataTr({ key: 'DAPP Version', value: DAPP_VERSION })}
         </tbody>
 
-        {sectionThead({title: "Origin GraphQL Config"})}
+        {sectionThead({ title: 'Origin GraphQL Config' })}
         <Query
           query={query}
           notifyOnNetworkStatusChange={true}
         >
-            {({ error, data, fetchMore, networkStatus }) => {
+            {({ error, data, networkStatus }) => {
               if (networkStatus === 1) {
-                  return <tbody><tr><td>Loading...</td></tr></tbody>;
+                  return <tbody><tr><td>Loading...</td></tr></tbody>
               } else if (error) {
-                  return <tbody><tr><td>Error :(</td></tr></tbody>;
+                  return <tbody><tr><td>Error :(</td></tr></tbody>
               }
               return <tbody>
-                {dataTr({ key:"Network", value: data.config })}
+                {dataTr({ key: 'Network', value: data.config })}
                 {Object.entries(data.configObj).map((entry)=>{
-                  let [key, value] = entry
-                  if(key == "__typename") { return }
+                  const [key, value] = entry
+                  if(key == '__typename') { return }
                   return dataTr({ key, value })
                 })}
               </tbody>
@@ -50,15 +49,15 @@ class DappInfo extends Component {
   }
 }
 
-require("react-styl")(`
-  .ConfigTable
+require('react-styl')(`
+  .config-table
     thead
       th
         border-bottom: solid 1px #eee
       h3
         margin-top: 1.4rem
         margin-bottom: 0.3rem
-`);
+`)
 
 export default DappInfo
 
