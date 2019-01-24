0f213a98545233eabe006ba7ba4f043255de2e6a
Tom Linton <tom@originprotocol.com>
2019-01-23 16:24:20 -0800

    Checkpoint

--- deployment/dockerfiles/scripts/ssl-issuer-entrypoint.sh
+++ deployment/dockerfiles/scripts/ssl-issuer-entrypoint.sh
@@ -10,21 +10,9 @@ chmod 777 /etc/resty-auto-ssl
 
 # Check if dhparam.pem has already been generated, otherwise copy it
 if [ ! -f "/data/resty-auto-ssl/dhparam.pem" ]; then
-  if [ -n "$DIFFIE_HELLMAN" ]; then
-    openssl dhparam -out /data/resty-auto-ssl/dhparam.pem 2048
-  else
-    cp ${RESTY_CONF_DIR}/dhparam.pem /data/resty-auto-ssl/dhparam.pem
-  fi
-fi
-
-cp ${RESTY_CONF_DIR}/server-default.conf ${NGINX_CONF_DIR}/default.conf
-
-if [ "$FORCE_HTTPS" == "true" ]; then
-  # only do this, if it's first run
-  if ! grep -q "force-https.conf" ${RESTY_CONF_DIR}/resty-server-http.conf
-  then
-    echo "include force-https.conf;" >> ${RESTY_CONF_DIR}/resty-server-http.conf
-  fi
+  openssl dhparam -out /data/resty-auto-ssl/dhparam.pem 2048
+else
+  cp ${RESTY_CONF_DIR}/dhparam.pem /data/resty-auto-ssl/dhparam.pem
 fi
 
 envsubst '$LETSENCRYPT_URL' \
