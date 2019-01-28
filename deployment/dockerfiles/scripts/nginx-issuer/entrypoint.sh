4561f2170b06309d01ad6501709d99ceb75c8703
Tom Linton <tom@originprotocol.com>
2019-01-28 13:02:39 -0800

    Container updates

--- deployment/dockerfiles/scripts/nginx-issuer/entrypoint.sh
+++ deployment/dockerfiles/scripts/nginx-issuer/entrypoint.sh
@@ -5,14 +5,20 @@
 RESTY_CONF_DIR="/usr/local/openresty/nginx/conf"
 NGINX_CONF_DIR="/etc/nginx/conf.d"
 
+mkdir -p /data/resty-auto-ssl
+
 # openresty will change it later on his own, right now we're just giving it access
-chmod 777 /etc/resty-auto-ssl
+chmod 777 /data/resty-auto-ssl
 
 # Check if dhparam.pem has already been generated, otherwise copy it
 if [ ! -f "/data/resty-auto-ssl/dhparam.pem" ]; then
   openssl dhparam -out /data/resty-auto-ssl/dhparam.pem 2048
 else
   cp ${RESTY_CONF_DIR}/dhparam.pem /data/resty-auto-ssl/dhparam.pem
+fi
+
+if [ ! -f "/data/resty-auto-ssl/resty-auto-ssl-fallback.key" ]; then
+  openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj '/CN=sni-support-required-for-valid-ssl' -keyout /data/resty-auto-ssl-fallback.key -out /data/resty-auto-ssl-fallback.crt
 fi
 
 envsubst '$LETSENCRYPT_URL' \
