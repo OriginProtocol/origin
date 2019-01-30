8e4c6817d5a429f488234f12af24c5f8d31f0cd3
Daniel Von Fange <daniel@leancoder.com>
2019-01-30 14:24:36 -0500

    Add About Origin Tokens page.
    Using content from old DAPP.

--- experimental/origin-dapp2/src/pages/App.js
+++ experimental/origin-dapp2/src/pages/App.js
@@ -19,6 +19,7 @@ import CreateListing from './create-listing/CreateListing'
 import Messages from './messaging/Messages'
 import Notifications from './notifications/Notifications'
 import DappInfo from './about/DappInfo'
+import AboutToken from './about/AboutTokens'
 
 class App extends Component {
   state = { hasError: false }
@@ -66,6 +67,7 @@ class App extends Component {
             <Route path="/messages/:room?" component={Messages} />
             <Route path="/notifications" component={Notifications} />
             <Route path="/about/dapp-info" component={DappInfo} />
+            <Route path="/about/tokens" component={AboutToken} />
             <Route component={Listings} />
           </Switch>
         </main>
