01769a130c6534bc9bed12883400a37dde56dea9
Nick Poulden <nick@poulden.com>
2019-01-31 13:14:02 -0700

    Path fix

--- experimental/origin-graphql/src/utils/balancesFromWei.js
+++ experimental/origin-graphql/src/utils/balancesFromWei.js
@@ -1,4 +1,4 @@
-import contracts from '../../contracts'
+import contracts from '../contracts'
 export default function balancesFromWei(wei) {
   const eth = contracts.web3.utils.fromWei(wei, 'ether').substr(0, 7)
   return { wei, eth }
