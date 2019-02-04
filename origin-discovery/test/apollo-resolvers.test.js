f504fc33cc79ec11a16153788f2e7204a9fd43e9
Mike Shultz <shultzm@gmail.com>
2019-02-04 11:38:27 -0700

    Graphql upgrade and some other stuff (#1383)
    * fixes #1085. upgraded graphql to 14.1.1
    
    * fixed a failing test
    
    * fixed URL to GraphQL playground in README

--- origin-discovery/test/apollo-resolvers.test.js
+++ origin-discovery/test/apollo-resolvers.test.js
@@ -1,11 +1,12 @@
 const chai = require('chai')
 const expect = chai.expect
+const { GraphQLScalarType } = require('graphql')
 
 const resolvers = require('../src/apollo/resolvers')
 
 describe('GraphQL resolvers', () => {
   it(`Should export a resolver for each high level type`, () => {
-    expect(resolvers.JSON).to.be.an('object')
+    expect(resolvers.JSON).to.be.instanceOf(GraphQLScalarType)
     expect(resolvers.Query).to.be.an('object')
     expect(resolvers.Listing).to.be.an('object')
     expect(resolvers.Offer).to.be.an('object')
