15170f06a2af20566eccfd00de2f79da3d47494c
Domen Grabec <grabec@gmail.com>
2019-02-04 17:27:01 +0100

    style fixes

--- experimental/origin-dapp2/src/pages/growth/Campaigns.js
+++ experimental/origin-dapp2/src/pages/growth/Campaigns.js
@@ -9,13 +9,15 @@ import query from 'queries/AllGrowthCampaigns'
 
 function CampaignNavItem(props) {
   const { campaign, selected, onClick } = props
-  const completedIndicator = campaign.status === 'completed' || campaign.status === 'capReached'
+  const completedIndicator =
+    campaign.status === 'completed' || campaign.status === 'capReached'
 
   let statusClass = ''
-  if (campaign.status === 'active')
+  if (campaign.status === 'active') {
     statusClass = 'active'
-  else if (campaign.status === 'pending')
+  } else if (campaign.status === 'pending') {
     statusClass = 'inactive'
+  }
 
   return (
     <a
@@ -28,10 +30,12 @@ function CampaignNavItem(props) {
     >
       <div className="campaign d-flex flex-column align-items-center">
         <div className={`status ${statusClass}`}>
-          { completedIndicator && <img src="images/circular-check-button.svg"/>}
+          {completedIndicator && <img src="images/circular-check-button.svg" />}
         </div>
-        <div className={`name ${selected ? 'active' : ''}`}>{campaign.name}</div>
-        {selected && <div className="select-bar"/>}
+        <div className={`name ${selected ? 'active' : ''}`}>
+          {campaign.name}
+        </div>
+        {selected && <div className="select-bar" />}
       </div>
     </a>
   )
@@ -39,33 +43,30 @@ function CampaignNavItem(props) {
 
 function CampaignNavList(props) {
   const { campaigns, onCampaignClick, selectedCampaignId } = props
-  return <div className="campaign-list d-flex justify-content-center mt-4">
-    {
-      campaigns.map(campaign => 
+  return (
+    <div className="campaign-list d-flex justify-content-center mt-4">
+      {campaigns.map(campaign => (
         <CampaignNavItem
           key={campaign.id}
           campaign={campaign}
           selected={campaign.id === selectedCampaignId}
           onClick={onCampaignClick}
         />
-      )
-    }
-  </div>
+      ))}
+    </div>
+  )
 }
 
 function ProgressBar(props) {
   const { progress } = props
 
-  return (<Fragment>
-      <div
-        className="campaign-progress mt-3"
-        style={{ width: '100%' }}
-      >
-        <div className="background"/>
-        { progress > 0 && <div
-            className="foreground"
-            style={{ width: `${progress}%` }}
-          />}
+  return (
+    <Fragment>
+      <div className="campaign-progress mt-3" style={{ width: '100%' }}>
+        <div className="background" />
+        {progress > 0 && (
+          <div className="foreground" style={{ width: `${progress}%` }} />
+        )}
       </div>
       <div className="indicators d-flex justify-content-between mt-2">
         <div>0</div>
@@ -74,22 +75,26 @@ function ProgressBar(props) {
         <div>75</div>
         <div>100</div>
       </div>
-    </Fragment>)
+    </Fragment>
+  )
 }
 
 function Campaign(props) {
   const { campaign } = props
-  const timeLeftDays = dayjs(campaign.endDate).diff(dayjs(), "day")
-  const timeLeftHours = dayjs(campaign.endDate).diff(dayjs(), "hour") % 24
-  const timeLeftMinutes = dayjs(campaign.endDate).diff(dayjs(), "minute") % 60
+  const timeLeftDays = dayjs(campaign.endDate).diff(dayjs(), 'day')
+  const timeLeftHours = dayjs(campaign.endDate).diff(dayjs(), 'hour') % 24
+  const timeLeftMinutes = dayjs(campaign.endDate).diff(dayjs(), 'minute') % 60
 
   let timeLeftLabel = ''
-  if (timeLeftDays > 0)
+  if (timeLeftDays > 0) {
     timeLeftLabel += ` ${timeLeftDays}d`
-  if (timeLeftHours > 0)
+  }
+  if (timeLeftHours > 0) {
     timeLeftLabel += ` ${timeLeftHours}h`
-  if (timeLeftMinutes > 0)
+  }
+  if (timeLeftMinutes > 0) {
     timeLeftLabel += ` ${timeLeftMinutes}m`
+  }
 
   const tokensEarned = campaign.rewardEarned.amount
   const tokenEarnProgress = Math.min(100, tokensEarned)
@@ -98,24 +103,21 @@ function Campaign(props) {
       <div className="d-flex justify-content-between">
         <h1 className="mb-2 pt-3">{campaign.name}</h1>
         <a>
-          <img src="images/eth-icon.svg"/>
+          <img src="images/eth-icon.svg" />
         </a>
       </div>
       <div>Get Origin Tokens by completing tasks below</div>
       <div className="d-flex justify-content-between campaign-info">
         <div>
           <span className="font-weight-bold">Tokens earned</span>
-          <img className="ogn-icon pl-2 pr-1" src="images/ogn-icon.svg"/>
+          <img className="ogn-icon pl-2 pr-1" src="images/ogn-icon.svg" />
           <span className="ogn-amount font-weight-bold">{tokensEarned}</span>
         </div>
-        <div className="font-weight-bold">
-          Time left:{timeLeftLabel}
-        </div>
+        <div className="font-weight-bold">Time left:{timeLeftLabel}</div>
       </div>
-      <ProgressBar
-        progress={tokenEarnProgress}
-      />
-    </Fragment>)
+      <ProgressBar progress={tokenEarnProgress} />
+    </Fragment>
+  )
 }
 
 class GrowthCampaigns extends Component {
@@ -130,42 +132,44 @@ class GrowthCampaigns extends Component {
 
     return (
       <div className="container campaigns">
-      <Query
-        query={query}
-        variables={vars}
-        notifyOnNetworkStatusChange={true}
-      >
-        {({ error, data, fetchMore, networkStatus, loading }) => {
-          if (networkStatus === 1) {
-            return <h5 className="p-2">Loading...</h5>
-          } else if (error) {
-            return <QueryError error={error} query={query} vars={vars} />
-          }
+        <Query
+          query={query}
+          variables={vars}
+          notifyOnNetworkStatusChange={true}
+        >
+          {({ error, data, networkStatus, loading }) => {
+            if (networkStatus === 1 || loading) {
+              return <h5 className="p-2">Loading...</h5>
+            } else if (error) {
+              return <QueryError error={error} query={query} vars={vars} />
+            }
 
-          const campaigns = data.campaigns.nodes
-          if (campaigns.length == 0) {
-            return <h5 className="p-2">No campaigns detected</h5>
-          }
+            const campaigns = data.campaigns.nodes
+            if (campaigns.length == 0) {
+              return <h5 className="p-2">No campaigns detected</h5>
+            }
 
-          selectedCampaignId = selectedCampaignId !== null ? selectedCampaignId : campaigns[0].id
-          const selectedCampaign = find(campaigns, campaign => campaign.id === selectedCampaignId)
+            selectedCampaignId =
+              selectedCampaignId !== null ? selectedCampaignId : campaigns[0].id
+            const selectedCampaign = find(
+              campaigns,
+              campaign => campaign.id === selectedCampaignId
+            )
 
-          return (
-            <Fragment>
-              <CampaignNavList 
-                campaigns={campaigns}
-                onCampaignClick={(campaignId) => {
-                  this.setState({ selectedCampaignId: campaignId })
-                }}
-                selectedCampaignId={selectedCampaignId}
-              />
-              <Campaign
-                campaign={selectedCampaign}
-              />
-            </Fragment>
-          )
-        }}
-      </Query>
+            return (
+              <Fragment>
+                <CampaignNavList
+                  campaigns={campaigns}
+                  onCampaignClick={campaignId => {
+                    this.setState({ selectedCampaignId: campaignId })
+                  }}
+                  selectedCampaignId={selectedCampaignId}
+                />
+                <Campaign campaign={selectedCampaign} />
+              </Fragment>
+            )
+          }}
+        </Query>
       </div>
     )
   }
