dfa89c16a09cd5bb7898e97158a1083aefd0a3bd
Domen Grabec <grabec@gmail.com>
2019-02-05 00:10:12 +0100

    top right design build of the growth welcome page

--- experimental/origin-dapp2/src/pages/growth/Welcome.js
+++ experimental/origin-dapp2/src/pages/growth/Welcome.js
@@ -7,12 +7,23 @@ class GrowthWelcome extends Component {
   }
 
   render() {
-    //const vars = pick(this.state, 'first')
-
     return (
-      <div className="growth-welcome">
-        <div className="container">
+      <div className="container growth-welcome d-flex">
+        <div className="col-6 d-flex flex-column">
+          <img className="logo" src="/images/origin-logo-footer.svg"/>
+          <div className="title-text">
+            Your friend Aure has invited you to earn <b>FREE Origin Tokens</b>
+          </div>
+          <div className="sub-title-text">
+            Create an account on Origin today and start completing tasks for the chance to will up to 2000 OGN currently valued at 2000 USD. Don’t miss this amazing opportunity
+          </div>
+          <button
+            className="btn btn-primary btn-rounded"
+            type="submit"
+            children="Sign up for Origin"
+          />
         </div>
+        <div className="col-6"> bsd</div>
       </div>
     )
   }
@@ -22,5 +33,20 @@ export default GrowthWelcome
 
 require('react-styl')(`
   .growth-welcome
-    background-color: var(--dark-grey-blue);
+    margin-top: 100px;
+    .logo
+      width: 118px;
+    .title-text
+      margin-top: 40px;
+      margin-bottom: 16px;
+      font-family: Poppins;
+      font-size: 50px;
+      font-weight: 200;
+      line-height: 1.3;
+    .btn
+      margin-top: 40px;
+      width: 336px;
+      height: 60px;
+      font-size: 24px;
+      font-weight: 900;
 `)
