be9c573c6a35878441aec9ff718ea1908ddd6d82
Franck Chastagnol <franck@originprotocol.com>
2019-02-06 09:38:37 -0800

    Tweak

--- origin-discovery/src/listener/listener.js
+++ origin-discovery/src/listener/listener.js
@@ -226,7 +226,7 @@ async function liveTracking (context) {
       start = new Date()
       const currentBlockNumber = await context.web3.eth.getBlockNumber()
       if (currentBlockNumber === lastCheckedBlock) {
-        logger.info('No new block.')
+        logger.debug('No new block.')
         return scheduleNextCheck()
       }
       logger.info(`New block: ${currentBlockNumber}`)
