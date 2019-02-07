4b36c2e4ac68d00a85e79bedac62982ffdfe7bdd
Domen Grabec <grabec@gmail.com>
2019-02-07 17:39:57 +0100

    make a higher order components that can open Growth Flow modal

--- experimental/origin-dapp2/src/pages/growth/Welcome.js
+++ experimental/origin-dapp2/src/pages/growth/Welcome.js
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react'
+import withEnrolmentModal from './WithEnrolmentModal'
 import Link from 'components/Link'
-import { Mutation } from 'react-apollo'
-import SetOpenedModalMutation from 'mutations/SetOpenedModal'
 
 function InfographicsBox(props) {
   const { image, title, text } = props
@@ -18,7 +17,12 @@ function InfographicsBox(props) {
 }
 
 class GrowthWelcome extends Component {
-  state = {}
+  constructor(props) {
+    super(props)
+    this.state = {}
+
+    this.EnrollButton = withEnrolmentModal('button')
+  }
 
   onSignUp(setOpenedModal) {
     setOpenedModal({
@@ -29,66 +33,57 @@ class GrowthWelcome extends Component {
   }
 
   render() {
-    // <Mutation
-    //     mutation={SetOpenedModalMutation}
-    //     onCompleted={}
-    //     onError={}
-    //   >
     return (
-      <Mutation mutation={SetOpenedModalMutation}>
-        {setOpenedModal => (
-          <div className="container growth-welcome">
-            <div className="row">
-              <div className="col-6 d-flex flex-column">
-                <Link to="/">
-                  <img className="logo" src="/images/origin-logo-footer.svg" />
-                </Link>
-                <div className="title-text">
-                  Your friend Aure has invited you to earn
-                  <b>FREE Origin Tokens</b>
-                </div>
-                <div className="sub-title-text">
-                  Create an account on Origin today and start completing tasks
-                  for the chance to will up to 2000 OGN currently valued at 2000
-                  USD. Don’t miss this amazing opportunity
-                </div>
-                <button
-                  className="btn btn-primary btn-rounded"
-                  children="Sign up for Origin"
-                  onClick={() => this.onSignUp(setOpenedModal)}
-                />
-              </div>
-              <div className="col-6"> Here be very nice graphic someday.</div>
+      <div className="container growth-welcome">
+        <div className="row">
+          <div className="col-6 d-flex flex-column">
+            <Link to="/">
+              <img className="logo" src="/images/origin-logo-footer.svg" />
+            </Link>
+            <div className="title-text">
+              Your friend Aure has invited you to earn
+              <b>FREE Origin Tokens</b>
             </div>
-            <div className="row">
-              <div className="col-12 d-flex flex-column mt-5">
-                <div className="info-title">What are Origin Tokens?</div>
-                <div className="text-center">
-                  Origin Tokens are a unique cryptocurrency that can be used in
-                  the Origin Marketplace.
-                </div>
-              </div>
+            <div className="sub-title-text">
+              Create an account on Origin today and start completing tasks
+              for the chance to will up to 2000 OGN currently valued at 2000
+              USD. Don’t miss this amazing opportunity
             </div>
-            <div className="row mt-3">
-              <InfographicsBox
-                image="images/ogn-icon-horiz.svg"
-                title="Placeholder title"
-                text="Etiam et lacus ut nisi rutrum egestas in nec mi. Morbi auctor metus eu ante condimentum, in tempus enim hendrerit. Donec a molestie velit."
-              />
-              <InfographicsBox
-                image="images/ogn-icon-horiz.svg"
-                title="Placeholder title"
-                text="Etiam et lacus ut nisi rutrum egestas in nec mi. Morbi auctor metus eu ante condimentum, in tempus enim hendrerit. Donec a molestie velit."
-              />
-              <InfographicsBox
-                image="images/ogn-icon-horiz.svg"
-                title="Placeholder title"
-                text="Etiam et lacus ut nisi rutrum egestas in nec mi. Morbi auctor metus eu ante condimentum, in tempus enim hendrerit. Donec a molestie velit."
-              />
+            <this.EnrollButton
+              className="btn btn-primary btn-rounded"
+              type="submit"
+              children="Sign up for Origin"
+            />
+          </div>
+          <div className="col-6"> Here be very nice graphic someday.</div>
+        </div>
+        <div className="row">
+          <div className="col-12 d-flex flex-column mt-5">
+            <div className="info-title">What are Origin Tokens?</div>
+            <div className="text-center">
+              Origin Tokens are a unique cryptocurrency that can be used in
+              the Origin Marketplace.
             </div>
           </div>
-        )}
-      </Mutation>
+        </div>
+        <div className="row mt-3">
+          <InfographicsBox
+            image="images/ogn-icon-horiz.svg"
+            title="Placeholder title"
+            text="Etiam et lacus ut nisi rutrum egestas in nec mi. Morbi auctor metus eu ante condimentum, in tempus enim hendrerit. Donec a molestie velit."
+          />
+          <InfographicsBox
+            image="images/ogn-icon-horiz.svg"
+            title="Placeholder title"
+            text="Etiam et lacus ut nisi rutrum egestas in nec mi. Morbi auctor metus eu ante condimentum, in tempus enim hendrerit. Donec a molestie velit."
+          />
+          <InfographicsBox
+            image="images/ogn-icon-horiz.svg"
+            title="Placeholder title"
+            text="Etiam et lacus ut nisi rutrum egestas in nec mi. Morbi auctor metus eu ante condimentum, in tempus enim hendrerit. Donec a molestie velit."
+          />
+        </div>
+      </div>
     )
   }
 }
---
+++ experimental/origin-dapp2/src/pages/growth/WithEnrolmentModal.js
@@ -0,0 +1,87 @@
+import React, { Component, Fragment } from 'react'
+import Modal from 'components/Modal'
+import { Query } from 'react-apollo'
+import query from 'queries/GrowthEligibility'
+import QueryError from 'components/QueryError'
+
+function withEnrolmentModal(WrappedComponent) {
+  return class WithEnrolmentModal extends Component {
+    constructor(props) {
+      super(props)
+      this.handleClick = this.handleClick.bind(this)
+    }
+
+    state = {
+      open: false,
+      stage: 'EligibilityCheck'
+    }
+
+    handleClick(e) {
+      e.preventDefault()
+      this.setState({
+        open: true
+      })
+    }
+
+    renderEligibilityCheck() {
+      return (
+        <Query query={query}>
+        {({ networkStatus, error, loading, data, refetch }) => {
+          if (networkStatus === 1 || loading)
+            return `Loading...`
+          else if (error) {
+            return <QueryError error={error} query={query} />
+          } else if (!data || !data.isEligible || data.isEligible.code !== '200') {
+            if (data && data.isEligible){
+              console.error(`Unexpected result received. Code: ${data.isEligible.code} message: ${data.isEligible.message}`)
+            }
+            return 'Something is wrong, please try again later.'
+          }
+
+          const { country, eligibility } = data.isEligible
+          if (eligibility === 'Eligible') {
+
+          } else if (eligibility === 'Restricted') {
+
+          } else if (eligibility === 'Forbidden') {
+            return (
+              <div>
+                YOU ARE NOT ALLOWED TO USE THIS
+              </div>
+            )
+          }
+        }}
+        </Query>
+      )
+    }
+
+    render() {
+      const { open } = this.state
+      return (
+        <Fragment>
+          <WrappedComponent
+            {...this.props}
+            onClick={this.handleClick}
+          />
+          {open && <Modal
+            className="growth-enrollment-modal"
+            onClose={() => {
+              this.setState({
+                open: false
+              })
+            }}
+          >
+            {this[`render${this.state.stage}`]()}
+          </Modal>}
+        </Fragment>
+      )
+    }
+  }
+}
+
+export default withEnrolmentModal
+
+require('react-styl')(`
+  .growth-enrollment-modal
+    padding-top: 20px;
+`)
