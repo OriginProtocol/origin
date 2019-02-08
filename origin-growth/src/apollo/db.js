3d12d9c4c715d44c0447fd7486aac87e6dbeda49
Franck <franck@originprotocol.com>
2019-02-08 08:35:53 -0800

    Run prettier on origin-growth (#1435)
    * Run prettier on origin-growth
    
    * Delete unwanted file
    
    * Linter

--- origin-growth/src/apollo/db.js
+++ origin-growth/src/apollo/db.js
@@ -2,18 +2,15 @@ const db = require('../models')
 
 function _makeCampaign(row) {
   let status
-  const endDate = new Date(row.endDate)/ 1000
-  const startDate = new Date(row.startDate)/ 1000
-  const currentDate = Date.now()/ 1000
+  const endDate = new Date(row.endDate) / 1000
+  const startDate = new Date(row.startDate) / 1000
+  const currentDate = Date.now() / 1000
   const capReached = row.capUsed > row.cap
 
-  if (currentDate > startDate && currentDate < endDate){
+  if (currentDate > startDate && currentDate < endDate) {
     status = capReached ? 'capReached' : 'active'
-  }
-  else if (currentDate < startDate)
-    status = 'pending'
-  else if (currentDate > endDate)
-    status = 'completed'
+  } else if (currentDate < startDate) status = 'pending'
+  else if (currentDate > endDate) status = 'completed'
 
   return {
     id: row.id,
@@ -22,13 +19,12 @@ function _makeCampaign(row) {
     endDate: endDate,
     distributionDate: row.distributionDate,
     status: status,
-    actions: [], // TODO implement 
+    actions: [], // TODO implement
     rewardEarned: { currency: 'OGN', amount: 0 } // TODO implement
   }
 }
 
 async function getCampaigns() {
-
   const rows = await db.GrowthCampaign.findAll({
     order: [['id', 'DESC']]
   })
