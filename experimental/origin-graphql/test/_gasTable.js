fd386ffd4d961ec4b94652045cb4fd4dbf2f35bf
Daniel Von Fange <daniel@leancoder.com>
2019-02-19 13:23:16 -0500

    Show table of gas usage in Origin GraphQL tests. (#1487)

---
+++ experimental/origin-graphql/test/_gasTable.js
@@ -0,0 +1,42 @@
+import pubsub from '../src/utils/pubsub'
+
+let subscriptionId = undefined
+const used = {}
+
+export async function trackGas(){
+  subscriptionId = await pubsub.subscribe('TRANSACTION_UPDATED', data => {
+    try{
+      if(data.transactionUpdated == undefined) {
+        return
+      }
+      if(data.transactionUpdated.status != 'receipt'){
+        return
+      }
+      const { mutation, gasUsed } = data.transactionUpdated
+      used[mutation] = used[mutation] ? used[mutation] : []
+      used[mutation].push(gasUsed)
+    } catch (e) {
+      // Do nothing
+    }
+  })
+}
+
+export function showGasTable() {
+  pubsub.unsubscribe(subscriptionId)
+  console.log('')
+  console.log('--------------------------------------------------')
+  console.log('Gas used (max, min)')
+  console.log('--------------------------------------------------')
+  const keys = Object.keys(used).sort()
+  keys.forEach(key => {
+    const values = used[key].sort((a,b) => a - b)
+    const min = values[0]
+    const max = values[values.length-1]
+    console.log([
+      key.padEnd(18),
+      max.toLocaleString().padStart(10),
+      min.toLocaleString().padStart(10)
+    ].join('\t'))
+  })
+}
+
