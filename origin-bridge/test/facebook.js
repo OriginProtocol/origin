fd8b88548e6aef47b9e38c356ec1dd8d20438938
Tom Linton <tom@originprotocol.com>
2019-02-20 15:13:22 +1300

    Fix facebook test

--- origin-bridge/test/facebook.js
+++ origin-bridge/test/facebook.js
@@ -2,20 +2,24 @@ const chai = require('chai')
 const expect = chai.expect
 const request = require('supertest')
 const app = require('../src/app')
+const { getAbsoluteUrl } = require('../src/utils')
 
 const Logger = require('logplease')
 Logger.setLogLevel('NONE')
 
 describe('facebook attestations', () => {
   it('should generate a correct auth url', done => {
+    process.env.FACEBOOK_CLIENT_ID = 'facebook-client-id'
+    const redirectUrl = getAbsoluteUrl('/redirects/facebook/')
+
     response = request(app)
       .get('/facebook/auth-url')
       .expect(200)
       .end((err, response) => {
         expect(response.body.url).equal(
-          'https://www.facebook.com/v2.12/dialog/oauth?' +
-            'client_id=facebook-client-id' +
-            '&redirect_uri=https://testhost.com/redirects/facebook/'
+          `https://www.facebook.com/v3.2/dialog/oauth?client_id=${
+            process.env.FACEBOOK_CLIENT_ID
+          }&redirect_uri=${redirectUrl}`
         )
         if (err) return done(err)
         done()
