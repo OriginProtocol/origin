b37b3fa258c45bb7bc828905f4bd08dae8dc767a
Tom Linton <tom@originprotocol.com>
2019-02-20 16:29:05 +1300

    Dapp2 marketplace creator (#1497)
    * Fix configuration snippet error
    
    * Start implementation of marketplace creator in dapp2
    
    * Improve query string parsing
    
    * Updates
    
    * Checkpoint
    
    * Checkpoint
    
    * No lockfiles
    
    * Add docker environment overrides
    
    * Checkpoint
    
    * Add discovery to docker config
    
    * Prototyping all volume mounts
    
    * Checkpoint
    
    * Cleanup volume mountings
    
    * Enable hoisting on babel-plugin-fbt
    
    * Add build to origin-messaging
    
    * Make elasticsearch work
    
    * Add build folders to docker ignore
    
    * Restructure creator integration and prettier run
    
    * Checkpoint
    
    * Checkpoint
    
    * Linting fixes
    
    * Checkpoint
    
    * Filters passing into listing page
    
    * Handle __typename
    
    * Working listing filtering
    
    * Checkpoint
    
    * Add arbitrator account
    
    * Fix merge mistake
    
    * Update port and add linker client to docker
    
    * Update DApp2 port
    
    * Change IPFS port
    
    * Improve nav and footer
    
    * Update empty listing states and run prettier
    
    * Cleanup double copy of linking client package.json
    
    * Fix casing of sample listing
    
    * Fix create listing link
    
    * Update create listing to pass marketplacePublisher
    
    * Add marketplacePublisher to listing data
    
    * Set locale fix
    
    * Linting fixes
    
    * Linting fix
    
    * Enable faucet in production (#1496)
    
    * Update chart to deploy faucet to prod
    
    * Add path for production
    
    * Single faucet
    
    * Fix config update on change of address (#1495)
    
    * Update deployed image tag for origin-dapp-creator-client [ci skip]
    
    * Fix line breaks in faucet ingress template
    
    * Update deployed image tag for origin-faucet [ci skip]
    
    * Add pg to package.json (#1499)
    
    * Update deployed image tag for origin-faucet [ci skip]
    
    * Tomlinton/faucet db access (#1500)
    
    * Add cloudsql proxy for faucet
    
    * Fix start command
    
    * Update deployed image tag for origin-faucet [ci skip]
    
    * GraphQL test lint (#1501)
    
    * Marketplace creator updates
    
    * Back to localhost
    
    * Var name

--- experimental/origin-dapp2/src/pages/listings/Listings.js
+++ experimental/origin-dapp2/src/pages/listings/Listings.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import { Query } from 'react-apollo'
+import omit from 'lodash/omit'
 import pick from 'lodash/pick'
+import get from 'lodash/get'
 import { fbt } from 'fbt-runtime'
+
+import withCreatorConfig from 'hoc/withCreatorConfig'
 
 import BottomScrollListener from 'components/BottomScrollListener'
 import QueryError from 'components/QueryError'
 import PageTitle from 'components/PageTitle'
+import Link from 'components/Link'
 
 import store from 'utils/store'
 import nextPageFactory from 'utils/nextPageFactory'
@@ -27,7 +32,16 @@ class Listings extends Component {
   }
 
   render() {
-    const vars = pick(this.state, 'first', 'sort', 'hidden', 'search')
+    const isCreatedMarketplace = get(
+      this.props,
+      'creatorConfig.isCreatedMarketplace'
+    )
+    const filters = get(this.props, 'creatorConfig.listingFilters', [])
+
+    const vars = {
+      ...pick(this.state, 'first', 'sort', 'hidden', 'search'),
+      filters: filters.map(filter => omit(filter, '__typename'))
+    }
 
     return (
       <>
@@ -69,28 +83,86 @@ class Listings extends Component {
                   }}
                 >
                   <>
-                    <h5 className="listings-count">
-                      <fbt desc="Num Listings">
-                        <fbt:plural count={totalCount} showCount="yes">
-                          Listing
-                        </fbt:plural>
-                      </fbt>
-                    </h5>
-                    <ListingsGallery
-                      listings={nodes}
-                      hasNextPage={hasNextPage}
-                    />
-                    {!hasNextPage ? null : (
-                      <button
-                        className="btn btn-outline-primary btn-rounded mt-3"
-                        onClick={() => {
-                          if (!loading) {
-                            nextPage(fetchMore, { ...vars, after })
-                          }
-                        }}
-                      >
-                        {loading ? 'Loading...' : 'Load more'}
-                      </button>
+                    {totalCount == 0 && (
+                      <div className="listings-empty">
+                        <div className="row">
+                          <div className="col text-center">
+                            <img src="images/empty-listings-graphic.svg" />
+                            {this.state.search && (
+                              <h1>
+                                <fbt desc="listings.noListingsSearch">
+                                  No search results found
+                                </fbt>
+                              </h1>
+                            )}
+
+                            {isCreatedMarketplace && !this.state.search && (
+                              <>
+                                <h1>
+                                  <fbt desc="listings.noListingsWhitelabel">
+                                    Your marketplace doesn&apos;t have any
+                                    listings yet
+                                  </fbt>
+                                </h1>
+                                <p>
+                                  <fbt desc="listings.noListingsWhitelabelMessage">
+                                    You can create listings yourself or invite
+                                    sellers to join your platform!
+                                  </fbt>
+                                </p>
+                                <div className="row">
+                                  <div className="col text-center">
+                                    <Link
+                                      to="/create"
+                                      className="btn btn-lg btn-primary"
+                                    >
+                                      <fbt desc="listings.createListingButton">
+                                        Create a Listing
+                                      </fbt>
+                                    </Link>
+                                  </div>
+                                </div>
+                              </>
+                            )}
+
+                            {!isCreatedMarketplace && !this.state.search && (
+                              <h1>
+                                <fbt desc="listings.noListings">
+                                  No listings found
+                                </fbt>
+                              </h1>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    )}
+
+                    {totalCount > 0 && (
+                      <>
+                        <h5 className="listings-count">
+                          <fbt desc="Num Listings">
+                            <fbt:plural count={totalCount} showCount="yes">
+                              Listing
+                            </fbt:plural>
+                          </fbt>
+                        </h5>
+                        <ListingsGallery
+                          listings={nodes}
+                          hasNextPage={hasNextPage}
+                        />
+                        {!hasNextPage ? null : (
+                          <button
+                            className="btn btn-outline-primary btn-rounded mt-3"
+                            onClick={() => {
+                              if (!loading) {
+                                nextPage(fetchMore, { ...vars, after })
+                              }
+                            }}
+                          >
+                            {loading ? 'Loading...' : 'Load more'}
+                          </button>
+                        )}
+                      </>
                     )}
                   </>
                 </BottomScrollListener>
@@ -108,7 +180,7 @@ class Listings extends Component {
   }
 }
 
-export default Listings
+export default withCreatorConfig(Listings)
 
 require('react-styl')(`
   .listings-count
@@ -117,6 +189,8 @@ require('react-styl')(`
     font-weight: 200;
     color: var(--dark);
     margin-top: 3rem
+  .listings-empty
+    margin-top: 10rem
   @media (max-width: 767.98px)
     .listings-count
       margin: 1rem 0 0 0
