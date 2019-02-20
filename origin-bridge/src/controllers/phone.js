38c22f7c3e48d9d41e1a34158a5460a87ad62399
Tom Linton <tom@originprotocol.com>
2019-02-20 16:31:00 +1300

    Start on phone verification

--- origin-bridge/src/controllers/phone.js
+++ origin-bridge/src/controllers/phone.js
@@ -1,9 +1,65 @@
 const express = require('express')
 const router = express.Router()
+const request = require('superagent')
 
 const Attestation = require('../models/attestation')
 
-router.post('/generate-code', (req, res) => {})
+router.post('/generate-code', (req, res) => {
+  if (!['sms', 'call'].includes(req.body.method)) {
+    return res
+      .status(400)
+      .json({
+        errors: [`Invalid phone verification method: ${req.body.method}`]
+      })
+  }
+  const params = {
+    country_code: req.body.country_calling_code,
+    phone_number: req.body.phone_number,
+    via: req.body.method,
+    code_length: 6
+  }
+
+  if (req.body.locale) {
+    params.locale = req.body.locale
+  } else if (req.body.country_calling_code == '91') {
+    // Locale not provided and calling India, override locale to English rather
+    // than Hindi which is Twilio's default. English is more widely understood
+    // among the Indian population
+    params.locale = 'en'
+  }
+
+  request
+    .post('https://api.authy.com/protected/json/phones/verification/start')
+    .send(params)
+    .set('X-Authy-API-Key', process.env.TWILIO_VERIFY_API_KEY)
+    .then(response => {
+      return res.status(200).end()
+    })
+    .catch(error => {
+      console.log(error)
+      const twilioErrorCode = error.body['error_code']
+      if (twilioErrorCode === '60033') {
+        return res.status(400).json({
+          errors: {
+            phone: 'Phone number is invalid.'
+          }
+        })
+      } else if (twilioErrorCode === '60083') {
+        return res.status(400).json({
+          errors: {
+            phone: 'Cannot send SMS to landline.'
+          }
+        })
+      } else {
+        logger.error(`Could not send phone verification code: ${error}`)
+        return res
+          .status(500)
+          .send(
+            'Could not send phone verification code, please try again later.'
+          )
+      }
+    })
+})
 
 router.post('/verify', (req, res) => {})
 
--- origin-bridge/test/phone.js
+++ origin-bridge/test/phone.js
@@ -1,12 +1,53 @@
 const chai = require('chai')
 const expect = chai.expect
+const nock = require('nock')
 const request = require('supertest')
+const app = require('../src/app')
 
 const Logger = require('logplease')
 Logger.setLogLevel('NONE')
 
 describe('phone attestations', () => {
-  it('should generate a verification code', () => {})
+  beforeEach(() => {
+    process.env.TWILIO_VERIFY_API_KEY = '1234'
+  })
+
+  it('should generate a verification code', async () => {
+    const params = {
+      country_calling_code: '1',
+      phone_number: '12341234',
+      method: 'sms',
+      locale: 'en'
+    }
+
+    nock('https://api.authy.com')
+      .post('/protected/json/phones/verification/start')
+      .reply(200)
+
+    await request(app)
+      .post('/phone/generate-code')
+      .send(params)
+      .expect(200)
+  })
+
+  it('should error on generate code with invalid method', async () => {
+    const params = {
+      country_calling_code: '1',
+      phone_number: '12341234',
+      method: 'magic',
+      locale: 'en'
+    }
+
+    await request(app)
+      .post('/phone/generate-code')
+      .send(params)
+      .expect(400)
+      .then(response => {
+        expect(response.body.errors[0]).to.equal(
+          'Invalid phone verification method: magic'
+        )
+      })
+  })
 
   it('should error on generate code with incorrect number format', () => {})
 
@@ -22,5 +63,52 @@ describe('phone attestations', () => {
 
   it('should error on expired verification code', () => {})
 
-  it('should use en locale for sms in india', () => {})
+  it('should use en locale for sms in india', async () => {
+    const params = {
+      country_calling_code: '91',
+      phone_number: '12341234',
+      method: 'sms'
+    }
+
+    let parsedBody = null
+    const scope = nock('https://api.authy.com')
+      .post('/protected/json/phones/verification/start', body => {
+        parsedBody = body
+        return body
+      })
+      .reply(200)
+
+    await request(app)
+      .post('/phone/generate-code')
+      .send(params)
+      .expect(200)
+      .then(response => {
+        expect(parsedBody.locale).to.equal('en')
+      })
+  })
+
+  it('should allow locale override for sms in india', async () => {
+    const params = {
+      country_calling_code: '91',
+      phone_number: '12341234',
+      method: 'sms',
+      locale: 'de'
+    }
+
+    let parsedBody = null
+    const scope = nock('https://api.authy.com')
+      .post('/protected/json/phones/verification/start', body => {
+        parsedBody = body
+        return body
+      })
+      .reply(200)
+
+    await request(app)
+      .post('/phone/generate-code')
+      .send(params)
+      .expect(200)
+      .then(response => {
+        expect(parsedBody.locale).to.equal('de')
+      })
+  })
 })
