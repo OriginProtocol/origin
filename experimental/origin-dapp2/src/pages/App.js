ef5b3469e69d5647da68317d06f9f35a74f5520c
Domen Grabec <grabec@gmail.com>
2019-02-21 14:55:07 +0100

    linter fix

--- experimental/origin-dapp2/src/pages/App.js
+++ experimental/origin-dapp2/src/pages/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { Switch, Route, withRouter } from 'react-router-dom'
 import get from 'lodash/get'
 
