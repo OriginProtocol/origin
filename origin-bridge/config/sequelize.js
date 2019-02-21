14a5786062b0e2edf3ce6502a6c4c4147ac05e71
Tom Linton <tom@originprotocol.com>
2019-02-21 14:27:22 +1300

    Update sequelize config

---
+++ origin-bridge/config/sequelize.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+module.exports = {
+  'development': {
+    use_env_variable: 'DATABASE_URL',
+    define: {
+      // Add the timestamp attributes (updatedAt, createdAt).
+      timestamps: true,
+      // Disable the modification of table names.
+      freezeTableName: true,
+      // Underscore style for field names.
+      underscored: true
+    },
+    // Disable logging of SQL statements.
+    logging: false
+  },
+  'test': {
+    use_env_variable: 'DATABASE_URL',
+    define: {
+      // Add the timestamp attributes (updatedAt, createdAt).
+      timestamps: true,
+      // Disable the modification of table names.
+      freezeTableName: true,
+      // Underscore style for field names.
+      underscored: true
+    },
+    // Disable logging of SQL statements.
+    logging: false
+  },
+  'production': {
+    use_env_variable: 'DATABASE_URL',
+    define: {
+      // Add the timestamp attributes (updatedAt, createdAt).
+      timestamps: true,
+      // Disable the modification of table names.
+      freezeTableName: true,
+      // Underscore style for field names.
+      underscored: true
+    },
+    // Disable logging of SQL statements.
+    logging: false
+  }
+}
