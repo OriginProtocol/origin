c560b7498e3b4eea9e10eb565a120b38433843f6
Micah Alcorn <mail@micahalcorn.com>
2019-02-28 13:04:39 -0800

    Manually remove Sentry

--- origin-mobile/ios/OriginCatcher/AppDelegate.m
+++ origin-mobile/ios/OriginCatcher/AppDelegate.m
@@ -9,11 +9,6 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#if __has_include(<React/RNSentry.h>)
-#import <React/RNSentry.h> // This is used for versions of react >= 0.40
-#else
-#import "RNSentry.h" // This is used for versions of react < 0.40
-#endif
 #import <React/RCTPushNotificationManager.h>
 #import <React/RCTLinkingManager.h>
 
