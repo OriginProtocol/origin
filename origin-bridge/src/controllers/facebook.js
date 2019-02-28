313f16a77054f7ce9c9c4831a84bc6124a480449
Tom Linton <tom@originprotocol.com>
2019-03-01 09:49:04 +1300

    Commenting

--- origin-bridge/src/controllers/facebook.js
+++ origin-bridge/src/controllers/facebook.js
@@ -9,11 +9,15 @@ const Attestation = require('../models/index').Attestation
 const AttestationTypes = Attestation.AttestationTypes
 const { generateAttestation } = require('../utils/attestation')
 const { facebookVerify } = require('../utils/validation')
-const logger = require('./logger')
+const logger = require('../logger')
 
 const { getAbsoluteUrl, mapObjectToQueryParams } = require('../utils')
 const constants = require('../constants')
 
+/* Generate a URL for the user to be redirected to that prompts for a Facebook
+ * login and then redirects to the URL specified in the redirect_uri parameter.
+ *
+ */
 router.get('/auth-url', (req, res) => {
   const dappRedirectUrl = req.query.dappRedirectUrl || null
   const params = {
@@ -24,6 +28,9 @@ router.get('/auth-url', (req, res) => {
   res.send({ url: url })
 })
 
+/* Exchange code from login dialog for an access token and generate attestation
+ * from the user data.
+ */
 router.post('/verify', facebookVerify, async (req, res) => {
   const params = {
     client_id: process.env.FACEBOOK_CLIENT_ID,
@@ -32,6 +39,7 @@ router.post('/verify', facebookVerify, async (req, res) => {
     code: req.body.code
   }
 
+  // Exchange code for an access token
   let accessToken
   try {
     const response = await request
@@ -40,7 +48,7 @@ router.post('/verify', facebookVerify, async (req, res) => {
     accessToken = response.body.access_token
   } catch (error) {
     logger.error(error)
-    return res.send(500).send({
+    return res.status(500).send({
       errors: ['Could not get access token']
     })
   }
@@ -50,12 +58,22 @@ router.post('/verify', facebookVerify, async (req, res) => {
     .update(accessToken)
     .digest('hex')
 
-  const userDataResponse = await request
-    .get(constants.FACEBOOK_BASE_GRAPH_URL + '/me')
-    .query({
-      appsecret_proof: appSecretProof,
-      access_token: accessToken
+  // Verify the token and determine the user account that it was generated for,
+  // use that data to generate the attetation
+  let useDataResponse
+  try {
+    userDataResponse = await request
+      .get(constants.FACEBOOK_BASE_GRAPH_URL + '/me')
+      .query({
+        appsecret_proof: appSecretProof,
+        access_token: accessToken
+      })
+  } catch (error) {
+    logger.error(error)
+    return res.status(500).send({
+      errors: ['Error validating access token']
     })
+  }
 
   const attestationBody = {
     verificationMethod: {
