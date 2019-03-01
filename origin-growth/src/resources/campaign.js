f654dca100b9eab66b1e84a70334726d01ae9091
Franck <franck@originprotocol.com>
2019-02-28 22:13:41 -0800

    [Growth] Misc cleanups (#1571)
    * stuff
    
    * Fix travis
    
    * Misc improvements/fixes

--- origin-growth/src/resources/campaign.js
+++ origin-growth/src/resources/campaign.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 const db = require('../models')
-const { CampaignRules } = require('../rules/rules')
+const { CampaignRules } = require('./rules')
 
 class GrowthCampaign {
   /**
