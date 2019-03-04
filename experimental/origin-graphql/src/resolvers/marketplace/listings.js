ceb0aab329d9786b86762275c0651c4713ceac0b
Daniel Von Fange <daniel@leancoder.com>
2019-03-04 13:17:08 -0500

    Fix for lint.

--- experimental/origin-graphql/src/resolvers/marketplace/listings.js
+++ experimental/origin-graphql/src/resolvers/marketplace/listings.js
@@ -80,7 +80,7 @@ async function resultsFromIds({ after, ids, first, totalCount, fields }) {
     nodes = (await Promise.all(
       ids.map(id => contracts.eventSource.getListing(id).catch(e => e) )
     ))
-    .filter(node => !(node instanceof Error));
+    .filter(node => !(node instanceof Error))
   }
   const firstNodeId = ids[0] || 0
   const lastNodeId = ids[ids.length - 1] || 0
