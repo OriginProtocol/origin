32476b95759f680152bc351366968d456704410a
Nick Poulden <nick@poulden.com>
2019-03-05 20:04:13 -0700

    Change css to rems. Add unitsPending to GraphQL query

--- experimental/origin-dapp2/src/pages/listing/_ListingEditOnly.js
+++ experimental/origin-dapp2/src/pages/listing/_ListingEditOnly.js
@@ -40,16 +40,16 @@ export default EditOnly
 require('react-styl')(`
   .listing-buy
     .listing-buy-editonly
-      border-top: 1px solid var(--dark)
-      border-bottom: 1px solid var(--dark)
-      padding: 16px
-      margin-bottom: 16px
+      border-top: 1px solid var(--light)
+      border-bottom: 1px solid var(--light)
+      padding: 1rem
+      margin-bottom: 1rem
       .row
         div:nth-child(1)
           flex: 1
-          padding-left: 16px
+          padding-left: 1rem
         div:nth-child(2)
           flex: 1
           text-align: right
-          padding-right: 16px
+          padding-right: 1rem
 `)
