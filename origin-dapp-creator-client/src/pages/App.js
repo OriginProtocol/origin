8a2b40774c66b84adf4f03000946f7b8a2762b6d
Tom Linton <tom@originprotocol.com>
2019-03-07 12:16:10 +1300

    Fix creator client config dependency

--- origin-dapp-creator-client/src/pages/App.js
+++ origin-dapp-creator-client/src/pages/App.js
@@ -1,6 +1,6 @@
 import { Route, Switch } from 'react-router-dom'
 import { Web3Provider } from 'react-web3'
-import { baseConfig } from 'origin-dapp/src/config'
+import { baseConfig } from 'origin-graphql/src/constants/CreatorConfig'
 import React from 'react'
 import superagent from 'superagent'
 
