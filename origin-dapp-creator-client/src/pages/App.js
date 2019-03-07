6e348040f695801b971b22e0feeb7273099361c9
Tom Linton <tom@originprotocol.com>
2019-03-08 07:17:27 +1300

    Fixes for marketplace creator + dapp2 (#1630)
    * Update preview URL
    
    * Disable continue button while subdomain validating
    
    * Fix logic issue in publish
    
    * Ensure subdomain field exists
    
    * Run prettier

--- origin-dapp-creator-client/src/pages/App.js
+++ origin-dapp-creator-client/src/pages/App.js
@@ -25,7 +25,8 @@ class App extends React.Component {
     this.state = {
       config: store.get('creator-config', {
         ...creatorConfig,
-        marketplacePublisher: ''
+        marketplacePublisher: '',
+        subdomain: ''
       }),
       publishedIpfsHash: null
     }
