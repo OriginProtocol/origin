f6b92f7f34b9427814f74b06d59d51fb4e44db2b
Tom Linton <tom@originprotocol.com>
2019-03-07 16:00:18 +1300

    Implement and run prettier on origin-linking

--- origin-linking/src/models/linked-token.js
+++ origin-linking/src/models/linked-token.js
@@ -1,22 +1,26 @@
-'use strict';
+'use strict'
 
 module.exports = (sequelize, DataTypes) => {
-  const LinkedToken = sequelize.define('LinkedToken', {
-    clientToken: DataTypes.STRING(255),
-    walletToken: DataTypes.STRING(255),
-    code: DataTypes.STRING(255),
-    codeExpires: DataTypes.DATE,
-    currentDeviceContext: DataTypes.JSON,
-    pendingCallContext: DataTypes.JSON,
-    clientPubKey:DataTypes.STRING(128),
-    linked: DataTypes.BOOLEAN,
-    appInfo: DataTypes.JSON,
-    linkedAt: DataTypes.DATE
-  }, {
-    tableName:'linked_token'
-  });
+  const LinkedToken = sequelize.define(
+    'LinkedToken',
+    {
+      clientToken: DataTypes.STRING(255),
+      walletToken: DataTypes.STRING(255),
+      code: DataTypes.STRING(255),
+      codeExpires: DataTypes.DATE,
+      currentDeviceContext: DataTypes.JSON,
+      pendingCallContext: DataTypes.JSON,
+      clientPubKey: DataTypes.STRING(128),
+      linked: DataTypes.BOOLEAN,
+      appInfo: DataTypes.JSON,
+      linkedAt: DataTypes.DATE
+    },
+    {
+      tableName: 'linked_token'
+    }
+  )
   LinkedToken.associate = function(models) {
     // associations can be defined here
-  };
-  return LinkedToken;
-};
+  }
+  return LinkedToken
+}
--- origin-linking/src/models/wallet-notification-endpoint.js
+++ origin-linking/src/models/wallet-notification-endpoint.js
@@ -1,17 +1,21 @@
-'use strict';
-const {EthNotificationTypes} = require('origin/common/enums');
+'use strict'
+const { EthNotificationTypes } = require('origin/common/enums')
 
 module.exports = (sequelize, DataTypes) => {
-  const WalletNotificationEndpoint = sequelize.define('WalletNotificationEndpoint', {
-    ethAddress: DataTypes.STRING(255),
-    walletToken: DataTypes.STRING(255),
-    deviceToken: DataTypes.STRING(255),
-    deviceType: DataTypes.ENUM(EthNotificationTypes),
-  }, {
-    tableName:'wallet_notification_endpoint'
-  });
+  const WalletNotificationEndpoint = sequelize.define(
+    'WalletNotificationEndpoint',
+    {
+      ethAddress: DataTypes.STRING(255),
+      walletToken: DataTypes.STRING(255),
+      deviceToken: DataTypes.STRING(255),
+      deviceType: DataTypes.ENUM(EthNotificationTypes)
+    },
+    {
+      tableName: 'wallet_notification_endpoint'
+    }
+  )
   WalletNotificationEndpoint.associate = function(models) {
     // associations can be defined here
-  };
-  return WalletNotificationEndpoint;
-};
+  }
+  return WalletNotificationEndpoint
+}
