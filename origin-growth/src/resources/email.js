3c6ca74e0605f38c396cb7e798df30c47dfb299c
Franck Chastagnol <franck@originprotocol.com>
2019-03-14 23:44:36 -0700

    reminder email

--- origin-growth/src/resources/email.js
+++ origin-growth/src/resources/email.js
@@ -12,49 +12,64 @@ const logger = require('../logger')
 // Do not allow referrer to blast invites to more than maxNumInvites recipients.
 const maxNumInvites = 50
 
+// Email templates.
 const templateDir = `${__dirname}/../templates`
-const textTemplate = fs
+const inviteTextTemplate = fs
   .readFileSync(`${templateDir}/emailInvite.txt`)
   .toString()
-const htmlTemplate = fs
-  .readFileSync(`${templateDir}/emailInvite.txt`)
+const inviteHtmlTemplate = fs
+  .readFileSync(`${templateDir}/emailInvite.html`)
+  .toString()
+const reminderTextTemplate = fs
+  .readFileSync(`${templateDir}/emailReminder.txt`)
+  .toString()
+const reminderHtmlTemplate = fs
+  .readFileSync(`${templateDir}/emailReminder.html`)
   .toString()
 
 /**
  * Returns the content for invite email.
  * TODO: localize the content.
  *
+ * @param {string} emailType: 'invite' or 'reminder'
  * @param {string} friendName
  * @param {string} targetUrl
  * @returns {{subject: string, html: *, text: *}}
  */
-function generateInviteEmail(referrerName, targetUrl) {
-  const subject = 'Join Origin and earn free cryptocurrency'
+function generateEmail(emailType, referrerName, targetUrl) {
+  let subject, textTemplate, htmlTemplate
+  if (emailType === 'invite') {
+    subject = 'Join Origin and earn free cryptocurrency'
+    textTemplate = inviteTextTemplate
+    htmlTemplate = inviteHtmlTemplate
+  } else {
+    subject = 'Earn Origin cryptocurrency'
+    textTemplate = reminderTextTemplate
+    htmlTemplate = reminderHtmlTemplate
+  }
   const text = textTemplate
-    .replace('${referrerName}', referrerName)
-    .replace('${targetUrl}', targetUrl)
+    .replace(/\${referrerName}/g, referrerName)
+    .replace(/\${targetUrl}/g, targetUrl)
   const html = htmlTemplate
-    .replace('${referrerName}', referrerName)
-    .replace('${targetUrl}', targetUrl)
+    .replace(/\${referrerName}/g, referrerName)
+    .replace(/\${targetUrl}/g, targetUrl)
 
   return { subject, text, html }
 }
 
 /**
- * Send invite codes by email.
- * @param {string} referrer - Eth address of the referrer.
- * @param {Array<string>>} recipients - List of email addresses.
+ * Loads values for variables used in email templates.
+ *
+ * @param {string}referrer - wallet address of the referrer.
+ * @returns {Promise<{referrerName: string, targetUrl: string}>}
+ * @private
  */
-async function sendInvites(referrer, recipients) {
-  if (recipients.length > maxNumInvites) {
-    throw new Error(`Exceded number of invites limit.`)
-  }
-
+async function _getEmailVars(referrer) {
   // Load the invite code for the referrer.
-  const code = await db.GrowthInviteCode.findOne({
+  const inviteCode = await db.GrowthInviteCode.findOne({
     where: { ethAddress: referrer.toLowerCase() }
   })
-  if (!code) {
+  if (!inviteCode) {
     throw new Error(`No invite code for ${referrer}`)
   }
 
@@ -69,10 +84,28 @@ async function sendInvites(referrer, recipients) {
     (identity.firstName || '') + ' ' + (identity.lastName || '')
 
   const dappUrl = process.env.DAPP_URL || 'http://localhost:3000'
+  const targetUrl = `${dappUrl}/#/welcome/${inviteCode.code}`
 
+  return { referrerName, targetUrl }
+}
+
+/**
+ * Send invite codes by email.
+ *
+ * @param {string} referrer - Eth address of the referrer.
+ * @param {Array<string>>} recipients - List of email addresses.
+ * @returns {Promise<void>}
+ */
+async function sendInvites(referrer, recipients) {
+  if (recipients.length > maxNumInvites) {
+    throw new Error(`Exceded number of invites limit.`)
+  }
   logger.info(
     `Sending ${recipients.length} invitation emails on behalf of ${referrer}`
   )
+
+  const { referrerName, targetUrl } = await _getEmailVars(referrer)
+
   for (const recipient of recipients) {
     // Validate recipient is a proper email.
     if (!validator.isEmail(recipient)) {
@@ -81,8 +114,7 @@ async function sendInvites(referrer, recipients) {
     }
 
     // Send the invite code to the recipient.
-    const targetUrl = `${dappUrl}/#/welcome/${code}`
-    const { subject, text, html } = generateInviteEmail(referrerName, targetUrl)
+    const { subject, text, html } = generateEmail(referrerName, targetUrl)
     const email = {
       to: recipient,
       from: process.env.SENDGRID_FROM_EMAIL,
@@ -118,4 +150,48 @@ async function sendInvites(referrer, recipients) {
   }
 }
 
-module.exports = { generateInviteEmail, sendInvites }
+/**
+ * Sends a reminder email.
+ *
+ * @param {string} referrer - Wallet address of referrer.
+ * @param {string} inviteId - growth_invite.id
+ * @returns {Promise<void>}
+ */
+async function sendInviteReminder(referrer, inviteId) {
+  // Load the invite and do some checks.
+  const invite = await db.GrowthInvite.findOne({
+    where: {
+      id: inviteId,
+      referrerEthAddress: referrer.toLowerCase()
+    }
+  })
+  if (!invite) {
+    throw new Error(`No invite with id ${inviteId} for referrer ${referrer}`)
+  }
+  if (invite.refereeContactType !== enums.GrowthInviteContactTypes.Email) {
+    logger.error('Reminder sending only supports email for now.')
+    return
+  }
+
+  logger.info(`Sending reminder email on behalf od ${referrer}`)
+  const { referrerName, targetUrl } = await _getEmailVars(referrer)
+  const recipient = invite.refereeContact
+
+  // Send the reminder email.
+  const { subject, text, html } = generateEmail(referrerName, targetUrl)
+  const email = {
+    to: recipient,
+    from: process.env.SENDGRID_FROM_EMAIL,
+    subject,
+    text,
+    html
+  }
+  try {
+    await sendgridMail.send(email)
+  } catch (error) {
+    logger.error(`Failed sending reminder: ${error}`)
+    throw new Error(`Failed sending reminder: ${error}`)
+  }
+}
+
+module.exports = { generateEmail, sendInvites, sendInviteReminder }
