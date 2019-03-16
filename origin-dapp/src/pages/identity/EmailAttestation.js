cc3a2ebd305d553fc423ddeaea49aadb0d5aea77
Domen Grabec <grabec@gmail.com>
2019-03-15 17:13:41 -0700

    Growth updates (#1719)
    * update terms design and conditions Also add location check to enroll mutation
    
    * minor style changes
    
    * move campaign max value from 100 to 1000 ogn
    
    * move campaign limitation progress bar to 1000 Ogn
    
    * prettify
    
    * change email and phone texts
    
    * prettify

--- origin-dapp/src/pages/identity/EmailAttestation.js
+++ origin-dapp/src/pages/identity/EmailAttestation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
+import { fbt } from 'fbt-runtime'
 
 import Modal from 'components/Modal'
 
@@ -93,8 +94,10 @@ class EmailAttestation extends Component {
               <div className="alert alert-danger mt-3">{this.state.error}</div>
             )}
             <div className="help">
-              Other users will know that you have a verified email address. Your
-              actual email will not be published on the blockchain.
+              <fbt desc="Attestation.emailPublishClarification">
+                By verifying your email, you give Origin permission to send you
+                occasional emails such as notifications about your transactions.
+              </fbt>
             </div>
             <div className="actions">
               <button
