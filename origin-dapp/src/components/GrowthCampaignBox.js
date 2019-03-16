a7c8ec8a9b0b6a43fc10a27998a7382f20b89c97
Domen Grabec <grabec@gmail.com>
2019-03-15 21:12:37 -0700

    prettify

--- origin-dapp/src/components/GrowthCampaignBox.js
+++ origin-dapp/src/components/GrowthCampaignBox.js
@@ -18,7 +18,7 @@ const EnrollButton = withEnrolmentModal('button')
 //TODO: put this into constants
 const maxProgressBarTokens = 1000
 
-const renderNotEnrolledBox = (openmodalonstart) => {
+const renderNotEnrolledBox = openmodalonstart => {
   return (
     <div className="enroll-gray-box campaign-enroll d-flex flex-column align-items-center">
       <fbt desc="profile.enrollExplanation">
@@ -58,13 +58,7 @@ const renderEnrolledBox = (props, walletAddress) => {
           if (networkStatus === 1 || loading) {
             return <h5 className="p-2">Loading...</h5>
           } else if (error) {
-            return (
-              <QueryError
-                error={error}
-                query={allCampaignsQuery}
-                vars={vars}
-              />
-            )
+            return <QueryError error={error} query={allCampaignsQuery} />
           }
 
           const campaigns = data.campaigns.nodes
@@ -75,11 +69,9 @@ const renderEnrolledBox = (props, walletAddress) => {
             campaign => campaign.status === 'Active'
           )
 
-          if (activeCampaign === null)
-            return 'No active campaign found'
+          if (activeCampaign === null) return 'No active campaign found'
 
-
-          return(
+          return (
             <Query
               query={AccountTokenBalance}
               variables={{ account: walletAddress, token: 'OGN' }}
@@ -94,17 +86,10 @@ const renderEnrolledBox = (props, walletAddress) => {
                   if (tokenHolder && tokenHolder.token) {
                     decimalDivision = web3.utils
                       .toBN(10)
-                      .pow(
-                        web3.utils.toBN(
-                          tokenHolder.token.decimals
-                        )
-                      )
+                      .pow(web3.utils.toBN(tokenHolder.token.decimals))
                   }
                 }
-                const {
-                  rewardEarned,
-                  endDate
-                } = activeCampaign
+                const { rewardEarned, endDate } = activeCampaign
                 const tokensEarned = web3.utils
                   .toBN(rewardEarned ? rewardEarned.amount : 0)
                   .div(decimalDivision)
@@ -121,11 +106,16 @@ const renderEnrolledBox = (props, walletAddress) => {
                       </fbt>
                     </div>
                     <div className="mt-2">
-                      <img className="ogn-icon pr-2" src="images/ogn-icon.svg" />
+                      <img
+                        className="ogn-icon pr-2"
+                        src="images/ogn-icon.svg"
+                      />
                       <span className="ogn-amount font-weight-bold big">
                         {tokensEarned.toString()}
                       </span>
-                      <span className="ogn-amount font-weight-bold small ml-1">OGN</span>
+                      <span className="ogn-amount font-weight-bold small ml-1">
+                        OGN
+                      </span>
                     </div>
                     <ProgressBar
                       maxValue={maxProgressBarTokens}
@@ -133,7 +123,10 @@ const renderEnrolledBox = (props, walletAddress) => {
                       showIndicators={false}
                     />
                     <div className="small-dark">
-                      {`Time left: ${formatTimeDifference(Date.now(), endDate)}`}
+                      {`Time left: ${formatTimeDifference(
+                        Date.now(),
+                        endDate
+                      )}`}
                     </div>
                     <div className="small-steel">
                       <fbt desc="profile.growthPaidOut">
@@ -142,17 +135,17 @@ const renderEnrolledBox = (props, walletAddress) => {
                     </div>
                     <div className="small-steel">
                       <fbt desc="profile.notSeeingEarnings">
-                        Not seeing your earnings? Make sure you publish your changes.
+                        Not seeing your earnings? Make sure you publish your
+                        changes.
                       </fbt>
                     </div>
                   </div>
                 )
-
               }}
             </Query>
           )
         }}
-        </Query>
+      </Query>
     </div>
   )
 }
