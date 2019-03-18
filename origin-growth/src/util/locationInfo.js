1ed2db6598dfef207a630330809556964714146e
Franck Chastagnol <franck@originprotocol.com>
2019-03-18 10:50:26 -0700

    Updated list

--- origin-growth/src/util/locationInfo.js
+++ origin-growth/src/util/locationInfo.js
@@ -1,7 +1,29 @@
 const { ip2geo } = require('./ip2geo')
 
-const restrictedCountryCodes = ['US', 'CA']
-const forbiddenCountryCodes = []
+const restrictedCountryCodes = ['US']
+// All OFAC countries are forbidden.
+// https://www.treasury.gov/resource-center/sanctions/Programs/Pages/Programs.aspx
+const forbiddenCountryCodes = [
+  'BY',
+  'BI',
+  'CF',
+  'CU',
+  'CD',
+  'IQ',
+  'IR',
+  'LB',
+  'LY',
+  'NG',
+  'KP',
+  'RU',
+  'SD',
+  'SO',
+  'SY',
+  'UA',
+  'VE',
+  'YE',
+  'ZW'
+]
 
 /**
  * Returns country code, country name and
