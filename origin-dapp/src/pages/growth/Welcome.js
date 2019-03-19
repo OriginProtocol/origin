51abd04ff9ecb113a0db9855be9388fa388672a5
Domen Grabec <grabec@gmail.com>
2019-03-18 18:07:10 -0700

    some progress on new growth welcome page

--- origin-dapp/src/pages/growth/Welcome.js
+++ origin-dapp/src/pages/growth/Welcome.js
@@ -10,20 +10,6 @@ import PageTitle from 'components/PageTitle'
 import Onboard from 'pages/onboard/Onboard'
 import Link from 'components/Link'
 
-function InfographicsBox(props) {
-  const { image, title, text } = props
-
-  return (
-    <Fragment>
-      <div className="col infographics d-flex flex-column m-3">
-        <img className="pt-4 mt-auto" src={image} />
-        <div className="text-center title pt-3 pl-3 pr-3">{title}</div>
-        <div className="text-center text p-3 pb-4">{text}</div>
-      </div>
-    </Fragment>
-  )
-}
-
 class GrowthWelcome extends Component {
   constructor(props) {
     super(props)
@@ -106,21 +92,31 @@ class GrowthWelcome extends Component {
     const personalised = !!identity
 
     return (
-      <div className="container growth-welcome">
-        <div className="row">
+      <div className="growth-welcome">
+        <div className="container d-flex">
           <div className="col-6 d-flex flex-column">
             <Link to="/" className="mr-auto">
-              <img className="logo" src="/images/origin-logo-footer.svg" />
+              <img className="logo" src="/images/origin-logo.svg" />
             </Link>
             {personalised && (
-              <div className="title-text">
-                Your friend {firstName} has invited you to earn{' '}
-                <b>FREE Origin Tokens</b>
-              </div>
+              <Fragment>
+                <div className="personalised-bar d-flex justify-content-left">
+                  <div className="profile-holder d-flex justify-content-center">
+                    <img src="images/growth/profile-person.svg" />
+                  </div>
+                  <div className="invited-by ml-3 d-flex align-items-center">
+                    Invited by <br/>
+                    {firstName}
+                  </div>
+                </div>
+                <div className="title-text">
+                  Your friend {firstName} has invited you to join Origin
+                </div>
+              </Fragment>
             )}
             {!personalised && (
               <div className="title-text">
-                Singn up and earn <b>FREE Origin Tokens</b>
+                Singn up and earn FREE Origin Tokens
               </div>
             )}
             <div className="sub-title-text">
@@ -130,62 +126,26 @@ class GrowthWelcome extends Component {
               Origin.
             </div>
             <this.EnrollButton
-              className="btn btn-primary btn-rounded"
+              className="btn btn-primary btn-rounded enroll-button"
               type="submit"
               children="Sign Up Now"
               urlforonboarding={urlForOnboarding}
               startopen={arrivedFromOnboarding.toString()}
             />
           </div>
-          <div className="col-6 token-stack-holder">
-            {personalised && (
-              <div className="personalised-holder d-flex flex-column">
-                <div className="message-bubble ml-auto d-flex align-items-center">
-                  Hey, come earn some tokens on Origin!
-                </div>
-                <div className="d-flex justify-content-end">
-                  <div className="d-flex flex-column align-items-end">
-                    <div className="triangle" />
-                    <div className="referrer">{`${firstName} ${lastName}`}</div>
-                  </div>
-                  <div className="profile-holder d-flex justify-content-center">
-                    <img src="images/growth/profile-person.svg" />
-                  </div>
-                </div>
+          <div className="spaceman col-10" />
+        </div>
+        <div className="second-fold-holder">
+          <div className="container d-flex">
+            <div className="col-6 d-flex flex-column">
+              <div>WHAT IS ORIGIN?- translate</div>
+              <div>
+                Origin is the first peer-to-peer marketplace built entirely
+                on the blockchain .- translate
               </div>
-            )}
-            <img
-              className="m-4 token-stack"
-              src="images/growth/token-stack.svg"
-            />
-            <img className="free-badge" src="images/growth/free-badge.svg" />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12 d-flex flex-column mt-5">
-            <div className="info-title">What is Origin?</div>
-            <div className="text-center">
-              Origin is the first peer-to-peer marketplace built entirely on the
-              blockchain.
             </div>
+            <div className="origin-showcase col-10" />
           </div>
-        </div>
-        <div className="row mt-3">
-          <InfographicsBox
-            image="images/growth/wallet-graphic.svg"
-            title="Boosting"
-            text="Sellers use OGN to boost their listings on the marketplace. This gives their listings higher visibility and placement. Listings with OGN have a higher chance of being sold quickly."
-          />
-          <InfographicsBox
-            image="images/growth/messaging-graphic.svg"
-            title="Rewards"
-            text="OGN is a rewards cryptocurrency earned by Origin users. Earn rewards when you verify your account or invite your friends to join Origin. Even get OGN as cash back when you buy and sell."
-          />
-          <InfographicsBox
-            image="images/growth/alerts-graphic.svg"
-            title="Payment"
-            text="In the future, OGN can be used to pay for goods and services on the marketplace. Use OGN just like ETH to transfer funds to other buyers and sellers."
-          />
         </div>
       </div>
     )
@@ -240,71 +200,53 @@ export default GrowthWelcome
 
 require('react-styl')(`
   .growth-welcome
-    margin-top: 100px
+    padding-top: 60px
+    background-color: #131d27
     .logo
       width: 118px
+    .enroll-button
+      margin-bottom: 134px
     .title-text
       margin-top: 40px
       margin-bottom: 16px
       font-family: Poppins
-      font-size: 50px
-      font-weight: 200
-      line-height: 1.3
+      font-size: 48px
+      font-weight: 600
+      line-height: 1.27
+      color: white;
+    .sub-title-text
+      font-size: 24px
+      font-weight: normal
+      color: white
+      line-height: 1.38
+    .invited-by
+      color: white
+      font-weight: normal
+    .spaceman
+      background-image: url(images/growth/spaceman.svg)
+      background-position: bottom left
+      background-repeat: no-repeat
     .btn
       margin-top: 40px
       width: 336px
       height: 60px
       font-size: 24px
       font-weight: 900
-    .info-title
-      font-size: 28px
-      font-family: Poppins
-      font-weight: bold
-      text-align: center
-    .token-stack-holder
-      position: relative
-    .token-stack
-      width: 440px
-    .free-badge
-      position: absolute
-      width: 168px
-      right: 15px
-      bottom: 35px
-    .infographics
-      background-color: #f1f6f9
-      border-radius: 5px
-      height: 350px
-      .title
-        text-align: center
-        font-weight: bold
-      .text
-        text-align: center
-        font-size: 14px
-    .personalised-holder
-      position: absolute
-      right: 0
-    .message-bubble
-      padding: 10px 20px
-      background-color: var(--pale-grey-eight)
-      height: 77px;
-      border-radius: 45px
-      border: solid 2px var(--white)
-    .triangle
-      width: 0
-      height: 0
-      border-left: 23px solid transparent
-      border-right: 23px solid transparent
-      border-top: 23px solid var(--pale-grey-eight)
-      transform: rotate(225deg)
-      margin-top: -16px
-    .profile-holder
-      background-color: var(--dark-grey-blue)
-      width: 60px
-      height: 60px
-      border-radius: 75px
-      border: solid 2px var(--pale-grey-eight)
-      overflow: hidden
-    .referrer
-      margin-top: 20px
-      margin-right: 10px
+    .personalised-bar
+      margin-top: 64px
+      .profile-holder
+        background-color: var(--dark-grey-blue)
+        width: 80px
+        height: 80px
+        border-radius: 75px
+        overflow: hidden
+      .profile-holder img
+        margin-top: 20px
+    .second-fold-holder
+      background-color: white
+    .origin-showcase
+      background-image: url(images/growth/marketplace-screenshots-graphic.png)
+      background-position: bottom left
+      background-repeat: no-repeat
+
 `)
