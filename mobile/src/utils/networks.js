58034b353e4968bd23cc71315b35216c4cdcf32a
Tom Linton <tom@originprotocol.com>
2019-03-21 11:27:00 +1300

    Revert "Switch to process.env with webpack dotenv"
    This reverts commit fa32453dd262f764915de0a4b386c92b0f1e467b.

--- mobile/src/utils/networks.js
+++ mobile/src/utils/networks.js
@@ -1,6 +1,11 @@
+import {
+  MAINNET_API_SERVER,
+  RINKEBY_API_SERVER
+} from 'react-native-dotenv'
+
 const networks = [
-  { id: 1, name: 'Mainnet', url: process.env.MAINNET_API_SERVER, custom: false },
-  { id: 4, name: 'Rinkeby', url: process.env.RINKEBY_API_SERVER, custom: false },
+  { id: 1, name: 'Mainnet', url: MAINNET_API_SERVER, custom: false },
+  { id: 4, name: 'Rinkeby', url: RINKEBY_API_SERVER, custom: false },
   { id: 999, name: 'Custom', url: '', custom: true },
 ]
 
