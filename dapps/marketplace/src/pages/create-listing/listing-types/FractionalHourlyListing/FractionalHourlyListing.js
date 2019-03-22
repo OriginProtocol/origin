3f6dd29b3342a87c8f537231c6a336ecfce2a4e8
Shah <shahthepro@outlook.com>
2019-03-22 09:20:15 +0530

    Prettify

--- dapps/marketplace/src/pages/create-listing/listing-types/FractionalHourlyListing/FractionalHourlyListing.js
+++ dapps/marketplace/src/pages/create-listing/listing-types/FractionalHourlyListing/FractionalHourlyListing.js
@@ -5,14 +5,19 @@ import Details from './Details'
 import Boost from './Boost'
 import Availability from './Availability'
 import Review from './Review'
-import withWallet from '../../../../hoc/withWallet';
+import withWallet from '../../../../hoc/withWallet'
 
 const FractionalHourlyListing = props => (
   <Steps
     {...props}
     steps={[
       { step: 1, component: Details, require: 'subCategory' },
-      { step: 2, component: withWallet(Boost), path: 'boost', require: 'title' },
+      {
+        step: 2,
+        component: withWallet(Boost),
+        path: 'boost',
+        require: 'title'
+      },
       { step: 3, component: Availability, path: 'availability' },
       { step: 4, component: Review, path: 'review' }
     ]}
