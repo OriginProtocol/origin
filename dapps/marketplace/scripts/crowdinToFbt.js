af65687717c9df4d317494cf528fa8862ff707b6
Stan James <stan@wanderingstan.com>
2019-03-25 13:02:01 -0700

    Crowdin translation pipeline minimally working.
    Notes added to README.md

---
+++ dapps/marketplace/scripts/crowdinToFbt.js
@@ -0,0 +1,46 @@
+// Iterates over files in `translation/crowdin` over our languages, expecting them to be in key-value format that crowdin uses
+// Output: files in fbt formatin in `translations` dir
+//
+
+const locales = 'de_DE el_GR es_ES fil_PH fr_FR hr_HR id_ID it_IT ja_JP ko_KR nl_NL pt_PT ro_RO ru_RU th_TH tr_TR uk_UA vi_VN zh_CN zh_TW'.split(
+  ' '
+)
+
+locales.forEach(locale => {
+  const srcFile = `${__dirname}/../translation/crowdin/all-messages_${locale}.json`
+  const dstFile = `${__dirname}/../translations/${locale}.json`
+
+  const fs = require('fs')
+  const translations = {}
+
+  let stringKeyValue=''
+  try {
+    stringKeyValue = JSON.parse(fs.readFileSync(srcFile))
+  }
+  catch (error) {
+    console.warn(`Could not find or parse file: ${srcFile}`)
+    return
+  }
+  console.log(`Processing file: ${srcFile}`)
+
+  Object.keys(stringKeyValue).forEach(key => {
+    var val = stringKeyValue[key]
+    translations[key] = {
+      "translations": [
+        {"translation": val}
+      ]
+    }
+  })
+
+
+  const file = {
+    "fb-locale": locale,
+    "translations" : translations
+  }
+
+  const output = JSON.stringify(file, null, 2)
+
+  // This should be writign to the fbt translations in ./translations
+  fs.writeFileSync(dstFile, output)
+
+})
