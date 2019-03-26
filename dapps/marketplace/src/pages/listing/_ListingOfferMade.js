65eba4fe59215ecbd3c1e613b2953dd82be1808a
Stan James <stan@wanderingstan.com>
2019-03-26 10:25:46 -0700

    wrapping

--- dapps/marketplace/src/pages/listing/_ListingOfferMade.js
+++ dapps/marketplace/src/pages/listing/_ListingOfferMade.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { fbt } from 'fbt-runtime'
 
 import Link from 'components/Link'
 
 const OfferMade = () => (
   <div className="listing-buy pending">
-    <div>This listing is</div>
-    <div>Pending</div>
-    <div>You have already made an offer on this listing.</div>
-    <Link to="/my-purchases">View My Purchases</Link>
+    <fbt desc="listingOfferMade.text">
+      <div>This listing is</div>
+      <div>Pending</div>
+      <div>You have already made an offer on this listing.</div>
+    </fbt>
+    <Link to="/my-purchases">
+      <fbt desc="listingOfferMade.viewPurchasesLink">View My Purchases</fbt>
+    </Link>
   </div>
 )
 
