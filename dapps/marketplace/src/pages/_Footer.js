3e780b559b0d502d9ff39deab5ec24907848af8b
Stan James <stan@wanderingstan.com>
2019-03-27 10:24:40 -0700

    Few more unwrapped text, plus combining untranslatable sentence fragments

--- dapps/marketplace/src/pages/_Footer.js
+++ dapps/marketplace/src/pages/_Footer.js
@@ -76,27 +76,27 @@ class Footer extends Component {
                       <div>
                         <fbt desc="footer.foundABug">
                           Found a bug or have feedback? Send an email to
-                        </fbt>{' '}
-                        <a href={`mailto:${SupportEmail}`}>{SupportEmail}</a>{' '}
-                        <fbt desc="footer.openIssue">, open an issue on</fbt>{' '}
-                        <a
-                          href={GitHubLink}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          GitHub
-                        </a>{' '}
-                        <fbt desc="footer.foundABug">
+                          <a href={`mailto:${SupportEmail}`}>
+                            <fbt:param name="SupportEmail">{SupportEmail}</fbt:param>
+                          </a>{' '}
+                          , open an issue on
+                          <a
+                            href={GitHubLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            GitHub
+                          </a>
                           or post in our #bug-reports channel on
-                        </fbt>{' '}
-                        <a
-                          href="https://discord.gg/jyxpUSe"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          Discord
-                        </a>
-                        .
+                          <a
+                            href="https://discord.gg/jyxpUSe"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            Discord
+                          </a>
+                          .
+                        </fbt>
                       </div>
                       <br />
                       <div className="copyright">
