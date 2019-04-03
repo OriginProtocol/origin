41965df027165dcc38706f09b0f97bec6d9cae13
Domen Grabec <grabec@gmail.com>
2019-04-03 17:31:04 +0200

    Minor UX fixes (#1870)
    * various ux fixes
    
    * prettier
    
    * find a more elegant solution for navigation padding

--- dapps/marketplace/src/pages/_Nav.js
+++ dapps/marketplace/src/pages/_Nav.js
@@ -59,14 +59,17 @@ class Nav extends Component {
                 </li>
                 <Sell {...navProps('sell')} />
                 <li className="nav-item d-none d-md-flex">
-                  <this.EarnTokens className="nav-link earn-tokens text">
+                  <this.EarnTokens className="nav-link icon-padding earn-tokens text">
                     <span>
                       <fbt desc="navbar.earnTokens">Earn Tokens</fbt>
                     </span>
                   </this.EarnTokens>
                 </li>
-                <li className="nav-item d-none d-md-flex">
-                  <NavLink to="/create" className="nav-link add-listing text">
+                <li className="nav-item d-none d-lg-flex">
+                  <NavLink
+                    to="/create"
+                    className="nav-link icon-padding add-listing text"
+                  >
                     <span>
                       <fbt desc="navbar.addListing">Add Listing</fbt>
                     </span>
@@ -127,9 +130,10 @@ require('react-styl')(`
               background-color: var(--dark-grey-blue)
           &.active span
             background-color: var(--dark-grey-blue)
+        &.icon-padding span
+          padding-left: 2rem
         span
           display: inline-block
-          padding-left: 2rem !important
 
         &.add-listing span
           background: url(images/add-listing-icon.svg) no-repeat 0.5rem center
