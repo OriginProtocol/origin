a619a111de76965b2c2896f7298c4daa4ff4de18
Domen Grabec <grabec@gmail.com>
2019-04-09 22:27:28 +0200

    prettify

--- dapps/marketplace/src/pages/identity/mutations/DeployIdentity.js
+++ dapps/marketplace/src/pages/identity/mutations/DeployIdentity.js
@@ -26,8 +26,9 @@ class DeployIdentity extends Component {
         {upsertIdentity => (
           <>
             <button
-              className={`${this.props.className} ${this.props.disabled ?
-                'disabled' : ''}`}
+              className={`${this.props.className} ${
+                this.props.disabled ? 'disabled' : ''
+              }`}
               onClick={() => {
                 if (this.props.disabled) {
                   return
