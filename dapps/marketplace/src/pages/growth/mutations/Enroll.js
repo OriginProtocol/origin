75c5c6229332927026f2ec711b370fcd947c8e5d
Domen Grabec <grabec@gmail.com>
2019-04-12 01:05:07 +0200

    prettify

--- dapps/marketplace/src/pages/growth/mutations/Enroll.js
+++ dapps/marketplace/src/pages/growth/mutations/Enroll.js
@@ -83,7 +83,8 @@ class Enroll extends Component {
                               variables: {
                                 address: accountId,
                                 // TODO: change version programatically
-                                message: 'I accept the terms of growth campaign version: 1.0'
+                                message:
+                                  'I accept the terms of growth campaign version: 1.0'
                               }
                             })
                           }}
