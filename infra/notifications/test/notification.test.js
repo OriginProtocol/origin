e83d8835cb1ba0e31d372b2bedbe8b081cb730e8
Stan James <stan@wanderingstan.com>
2019-04-12 17:17:03 -0600

    prettier + lint

--- infra/notifications/test/notification.test.js
+++ infra/notifications/test/notification.test.js
@@ -16,7 +16,13 @@ describe('processableEvent', () => {
 
 describe('getNotificationMessage', () => {
   it(`Messages on buyer side`, async () => {
-    let msg = getNotificationMessage('OfferAccepted', '0x123', '0x456', 'buyer', 'mobile')
+    let msg = getNotificationMessage(
+      'OfferAccepted',
+      '0x123',
+      '0x456',
+      'buyer',
+      'mobile'
+    )
     expect(msg)
       .to.have.property('title')
       .that.is.a('string')
@@ -25,16 +31,34 @@ describe('getNotificationMessage', () => {
       .that.is.a('string')
 
     // Not an event of interest. No notification expected.
-    msg = getNotificationMessage('RandomEvent', '0x123', '0x456', 'buyer', 'mobile')
+    msg = getNotificationMessage(
+      'RandomEvent',
+      '0x123',
+      '0x456',
+      'buyer',
+      'mobile'
+    )
     expect(msg).to.equal(null)
 
     // Buyer is the initiator. No notification expected.
-    msg = getNotificationMessage('OfferAccepted', '0x123', '0x123', 'buyer', 'mobile')
+    msg = getNotificationMessage(
+      'OfferAccepted',
+      '0x123',
+      '0x123',
+      'buyer',
+      'mobile'
+    )
     expect(msg).to.equal(null)
   })
 
   it(`Messages on seller side`, async () => {
-    let msg = getNotificationMessage('OfferCreated', '0x123', '0x456', 'seller', 'mobile')
+    let msg = getNotificationMessage(
+      'OfferCreated',
+      '0x123',
+      '0x456',
+      'seller',
+      'mobile'
+    )
     expect(msg)
       .to.have.property('title')
       .that.is.a('string')
@@ -43,11 +67,23 @@ describe('getNotificationMessage', () => {
       .that.is.a('string')
 
     // Not an event of interest. No notification expected.
-    msg = getNotificationMessage('RandomEvent', '0x123', '0x456', 'seller', 'mobile')
+    msg = getNotificationMessage(
+      'RandomEvent',
+      '0x123',
+      '0x456',
+      'seller',
+      'mobile'
+    )
     expect(msg).to.equal(null)
 
     // Seller is the initiator. No notification expected.
-    msg = getNotificationMessage('OfferCreated', '0x123', '0x123', 'seller', 'mobile')
+    msg = getNotificationMessage(
+      'OfferCreated',
+      '0x123',
+      '0x123',
+      'seller',
+      'mobile'
+    )
     expect(msg).to.equal(null)
   })
 })
