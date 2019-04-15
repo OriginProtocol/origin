1d59c8bd2bfd5117fc570e386d7a5b1adcfd9e43
Tom Linton <tom@originprotocol.com>
2019-04-15 14:05:19 +1200

    Add DaiExchange support to contract decoder

--- mobile/src/utils/contractDecoder.js
+++ mobile/src/utils/contractDecoder.js
@@ -2,22 +2,21 @@
 
 import Web3 from 'web3'
 
-import MarketplaceContract from '@origin/contracts/build/contracts/V00_Marketplace'
-import IdentityEventsContract from '@origin/contracts/build/contracts/IdentityEvents'
+import graphqlContext from '@origin/graphql/src/contracts'
 
 const web3 = new Web3()
 
 export function decodeTransaction(data) {
   const functionSignature = data.substr(0, 10)
-  const marketplaceContract = new web3.eth.Contract(MarketplaceContract.abi)
-  const identityEventsContract = new web3.eth.Contract(
-    IdentityEventsContract.abi
-  )
 
   const interfaces = [
-    ...marketplaceContract._jsonInterface,
-    ...identityEventsContract._jsonInterface
+    ...graphqlContext.marketplace._jsonInterface,
+    ...graphqlContext.identityEvents._jsonInterface
   ]
+
+  if (graphqlContext.daiExchange) {
+    interfaces.push(...graphqlContext.daiExchange._jsonInterface)
+  }
 
   let functionInterface
   for (const iface of interfaces) {
