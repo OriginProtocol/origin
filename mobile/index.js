7543308ca8c744b0b2562f0f27c61de164c8ded3
Tom Linton <tom@originprotocol.com>
2019-04-15 12:48:59 +1200

    Fix import

--- mobile/index.js
+++ mobile/index.js
@@ -4,8 +4,8 @@ import '@babel/polyfill'
 
 import { AppRegistry } from 'react-native'
 
+import { Sentry } from 'react-native-sentry'
 if (!__DEV__) {
-  import { Sentry } from 'react-native-sentry'
   Sentry.config(
     'https://0711315529954f25bb2ee58315fe477b@sentry.io/1399965'
   ).install()
