5ad48bdc96a0b1a0aa38cfbcb5728c1a2574823d
Tom Linton <tom@originprotocol.com>
2019-04-15 12:47:01 +1200

    Only init Sentry in production

--- mobile/index.js
+++ mobile/index.js
@@ -3,10 +3,13 @@
 import '@babel/polyfill'
 
 import { AppRegistry } from 'react-native'
-import { Sentry } from 'react-native-sentry'
-Sentry.config(
-  'https://0711315529954f25bb2ee58315fe477b@sentry.io/1399965'
-).install()
+
+if (!__DEV__) {
+  import { Sentry } from 'react-native-sentry'
+  Sentry.config(
+    'https://0711315529954f25bb2ee58315fe477b@sentry.io/1399965'
+  ).install()
+}
 
 import './global'
 import App from './src/App'
