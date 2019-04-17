d71885894de4b2dc919f8ed8ceee8ace18355658
Tom Linton <tom@originprotocol.com>
2019-04-18 01:42:19 +1200

    Mobile fixes for Android (#2059)
    * Dont load marketplace creator configs if hostname is IP address
    
    * Fixes for onMessage on Android
    
    * Fix package.json
    
    * Prettier
    
    * Run prettier on mobile-bridge
    
    * Linting
    
    * Handle bad saved network
    
    * Remove trim from account name
    
    * Dont delete legacy data just in case
    
    * Update Info.plist
    
    * Fix toWei conversion
    
    * Fix function call

--- mobile/src/OriginWallet.js
+++ mobile/src/OriginWallet.js
@@ -18,10 +18,12 @@ import Web3 from 'web3'
 import { connect } from 'react-redux'
 import CryptoJS from 'crypto-js'
 
-import graphqlContext, { setNetwork } from '@origin/graphql/src/contracts'
+import graphqlContext, {
+  setNetwork as setGraphqlNetwork
+} from '@origin/graphql/src/contracts'
 
 import { addNotification } from 'actions/Notification'
-import { setDeviceToken } from 'actions/Settings'
+import { setDeviceToken, setNetwork } from 'actions/Settings'
 import {
   addAccount,
   removeAccount,
@@ -32,7 +34,8 @@ import {
 import {
   BALANCE_POLL_INTERVAL,
   DEFAULT_NOTIFICATION_PERMISSIONS,
-  ETH_NOTIFICATION_TYPES
+  ETH_NOTIFICATION_TYPES,
+  NETWORKS
 } from './constants'
 import { loadData, deleteData } from './tools'
 
@@ -146,10 +149,10 @@ class OriginWallet extends Component {
             }
             if (privateKey) {
               this.addAccount(privateKey)
+              console.debug('Migrated legacy account')
             }
           }
         }
-        await deleteData('WALLET_STORE')
       }
     })
   }
@@ -157,17 +160,23 @@ class OriginWallet extends Component {
   async _migrateLegacyDeviceTokens() {
     loadData('WALLET_INFO').then(async walletInfo => {
       if (walletInfo && walletInfo.deviceToken) {
-        this.setDeviceToken(walletInfo.deviceToken)
+        this.props.setDeviceToken(walletInfo.deviceToken)
+        console.debug('Migrated legacy device token')
       }
     })
-    await deleteData('WALLET_INFO')
   }
 
   /* Set the provider for web3 to the provider for the current network in the
    * graphql configuration for the current network
    */
   initWeb3() {
-    setNetwork(this.props.settings.network.name.toLowerCase())
+    // Verify that the saved network is valid
+    const networkExists = NETWORKS.find(n => n === this.props.settings.network)
+    if (!networkExists) {
+      // Set to mainnet if for some reason the network doesn't exist
+      this.props.setNetwork(NETWORKS.find(n => n.id === 1))
+    }
+    setGraphqlNetwork(this.props.settings.network.name.toLowerCase())
     const provider = graphqlContext.config.provider
     console.debug(`Setting provider to ${provider}`)
     this.web3.setProvider(new Web3.providers.HttpProvider(provider, 20000))
@@ -220,7 +229,6 @@ class OriginWallet extends Component {
       privateKey = '0x' + privateKey
     }
     const account = this.web3.eth.accounts.wallet.add(privateKey)
-    // TODO: verify that this won't add multiples of the same account
     this.props.addAccount(account)
     this.props.setAccountActive(account)
     return account.address
@@ -359,7 +367,7 @@ class OriginWallet extends Component {
         if (wallet.activeAccount && wallet.activeAccount.address) {
           this.registerDeviceToken(deviceToken['token'])
           // Save the device token into redux for later use with other accounts
-          this.setDeviceToken(deviceToken['token'])
+          this.props.setDeviceToken(deviceToken['token'])
         }
       }.bind(this),
       // Called when a remote or local notification is opened or received
@@ -463,6 +471,7 @@ const mapDispatchToProps = dispatch => ({
   setAccountServerNotifications: payload =>
     dispatch(setAccountServerNotifications(payload)),
   setDeviceToken: payload => dispatch(setDeviceToken(payload)),
+  setNetwork: network => dispatch(setNetwork(network)),
   addNotification: notification => dispatch(addNotification(notification))
 })
 
