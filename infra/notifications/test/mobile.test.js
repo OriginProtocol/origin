e5224d292706a82da1759cb2b01c9c13aee3b37c
Tom Linton <tom@originprotocol.com>
2019-04-23 22:58:40 +1200

    Add test file

---
+++ infra/notifications/test/mobile.test.js
@@ -0,0 +1,8 @@
+const chai = require('chai')
+const expect = chai.expect
+
+describe('register device token endpoint', () => {
+  it(`should add a new row`, async () => {})
+
+  it(`should upsert on existing row`, async () => {})
+})
