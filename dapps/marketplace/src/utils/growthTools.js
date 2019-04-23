e214634dee59d25a8aeb9c2950bf9fd0ecead632
Nick Poulden <nick@poulden.com>
2019-04-23 14:03:46 -0600

    Growth util fix

--- dapps/marketplace/src/utils/growthTools.js
+++ dapps/marketplace/src/utils/growthTools.js
@@ -3,27 +3,31 @@ export function getAttestationReward({
   attestation,
   tokenDecimals
 }) {
-  if (!growthCampaigns) return 0
+  try {
+    if (!growthCampaigns) return 0
 
-  const activeCampaign = growthCampaigns.find(
-    campaign => campaign.status === 'Active'
-  )
-  if (!activeCampaign) {
-    return ''
+    const activeCampaign = growthCampaigns.find(
+      campaign => campaign.status === 'Active'
+    )
+    if (!activeCampaign) {
+      return ''
+    }
+
+    const reward = activeCampaign.actions
+      .filter(action => action.type === attestation)
+      .map(action => action.reward)[0]
+
+    const decimalDivision = web3.utils
+      .toBN(10)
+      .pow(web3.utils.toBN(tokenDecimals))
+
+    return parseInt(
+      web3.utils
+        .toBN(reward ? reward.amount : 0)
+        .div(decimalDivision)
+        .toString()
+    )
+  } catch (e) {
+    return '?'
   }
-
-  const reward = activeCampaign.actions
-    .filter(action => action.type === attestation)
-    .map(action => action.reward)[0]
-
-  const decimalDivision = web3.utils
-    .toBN(10)
-    .pow(web3.utils.toBN(tokenDecimals))
-
-  return parseInt(
-    web3.utils
-      .toBN(reward ? reward.amount : 0)
-      .div(decimalDivision)
-      .toString()
-  )
 }
