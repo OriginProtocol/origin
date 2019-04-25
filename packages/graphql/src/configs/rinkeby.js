180c6c5adf263da922a9cf189b199181a3adbfc4
Tom Linton <tom@originprotocol.com>
2019-04-25 15:41:54 +1200

    Fix dupe keys

--- packages/graphql/src/configs/rinkeby.js
+++ packages/graphql/src/configs/rinkeby.js
@@ -9,8 +9,6 @@ export default {
   growth: 'https://growth.staging.originprotocol.com',
   bridge: 'https://bridge.staging.originprotocol.com',
   notifications: 'https://notifications.staging.originprotocol.com',
-  IdentityEvents: '0x160455a06d8e5aa38862afc34e4eca0566ee4e7e',
-  IdentityEvents_Epoch: '3670528',
   OriginToken: '0xa115e16ef6e217f7a327a57031f75ce0487aadb8',
   V00_Marketplace: '0xe842831533c4bf4b0f71b4521c4320bdb669324e',
   V00_Marketplace_Epoch: '3086315',
