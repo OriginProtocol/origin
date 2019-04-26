bb8371ab1663fb6e8a4e85d980a693e9582a1519
Franck Chastagnol <franck@originprotocol.com>
2019-04-25 21:06:55 -0700

    Added tests for legacy users

--- infra/growth/test/rules-may.test.js
+++ infra/growth/test/rules-may.test.js
@@ -303,72 +303,91 @@ describe('May campaign rules', () => {
     expect(this.crules.levels[2]).to.be.an('object')
     expect(this.crules.levels[2].rules.length).to.equal(7)
 
-    this.ethAddress = '0x123'
-    this.referee = '0x456'
+    this.userA = '0x123'
+    this.userB = '0x456' // User A is the referrer for user B.
+    this.userC = '0x789' // User C has legacy event from prior the start of May campaign.
+    
     this.expectedRewards = []
+
+    // Mock the rule's getEvent method.
+    this.events = []
+    this.crules.getEvents = () => { return this.events }
 
     // Mock the _getReferees method of the Referral rule.
     this.crules.levels[2].rules[0]._getReferees = () => { return [] }
   })
 
-  it(`Should start at level 0`, async () => {
-    const events = []
-    this.crules.getEvents = () => {
-      return events
-    }
-    const level = await this.crules.getCurrentLevel(this.ethAddress)
-    expect(level).to.equal(0)
-  })
-
-  it(`Should graduate to level 1`, async () => {
+  it(`Legacy user with profile and email should start at level 1`, async () => {
     this.events = [
       {
-        id: 1,
+        id: 'C1',
         type: GrowthEventTypes.ProfilePublished,
         status: GrowthEventStatuses.Logged,
-        ethAddress: this.ethAddress,
-        createdAt: this.duringCampaign
+        ethAddress: this.userC,
+        createdAt: this.beforeCampaign
       },
       {
-        id: 2,
+        id: 'C2',
         type: GrowthEventTypes.EmailAttestationPublished,
         status: GrowthEventStatuses.Logged,
-        ethAddress: this.ethAddress,
-        createdAt: this.duringCampaign
+        ethAddress: this.userC,
+        createdAt: this.beforeCampaign
       }
     ]
-    this.crules.getEvents = () => {
-      return this.events
-    }
 
-    const rewards = await this.crules.getRewards(this.ethAddress)
+    const rewards = await this.crules.getRewards(this.userC)
     expect(rewards).to.deep.equal([])
 
-    const level = await this.crules.getCurrentLevel(this.ethAddress)
+    const level = await this.crules.getCurrentLevel(this.userC)
     expect(level).to.equal(1)
   })
 
-  it(`Should graduate to level 2`, async () => {
+  it(`Legacy user with profile, email and 2 attestations should start at level 2`, async () => {
     this.events.push(
       {
-        id: 3,
+        id: 'C3',
+        type: GrowthEventTypes.FacebookAttestationPublished,
+        status: GrowthEventStatuses.Logged,
+        ethAddress: this.userC,
+        createdAt: this.beforeCampaign
+      },
+      {
+        id: 'C4',
+        type: GrowthEventTypes.AirbnbAttestationPublished,
+        status: GrowthEventStatuses.Logged,
+        ethAddress: this.userC,
+        createdAt: this.beforeCampaign
+      }
+    )
+
+    // User should not be rewarded for those legacy attestations.
+    const rewards = await this.crules.getRewards(this.userC)
+    expect(rewards).to.deep.equal([])
+
+    const level = await this.crules.getCurrentLevel(this.userC)
+    expect(level).to.equal(2)
+  })
+
+  it(`Legacy user at level 2 should get reward for new attestations`, async () => {
+    this.events.push(
+      {
+        id: 'C3',
         type: GrowthEventTypes.TwitterAttestationPublished,
         status: GrowthEventStatuses.Logged,
-        ethAddress: this.ethAddress,
+        ethAddress: this.userC,
         createdAt: this.duringCampaign
       },
       {
-        id: 4,
+        id: 'C4',
         type: GrowthEventTypes.GoogleAttestationPublished,
         status: GrowthEventStatuses.Logged,
-        ethAddress: this.ethAddress,
+        ethAddress: this.userC,
         createdAt: this.duringCampaign
       }
     )
-    this.crules.getEvents = () => { return this.events }
 
-    const rewards = await this.crules.getRewards(this.ethAddress)
-    this.expectedRewards.push(
+    const rewards = await this.crules.getRewards(this.userC)
+    this.expectedRewards = [
       {
         campaignId: 1,
         levelId: 1,
@@ -387,49 +406,123 @@ describe('May campaign rules', () => {
           amount: tokenNaturalUnits(25)
         }
       }
-    )
+    ]
     expect(rewards).to.deep.equal(this.expectedRewards)
 
-    const level = await this.crules.getCurrentLevel(this.ethAddress)
+    const level = await this.crules.getCurrentLevel(this.userC)
     expect(level).to.equal(2)
   })
 
-  it(`Should remain on level 2 when a referee signs up`, async () => {
-    this.crules.levels[2].rules[0]._getReferees = () => { return [this.referee] }
+  it(`Should start at level 0`, async () => {
+    const level = await this.crules.getCurrentLevel(this.userA)
+    expect(level).to.equal(0)
+  })
+
+  it(`Should graduate to level 1`, async () => {
+    this.events.push(
+      {
+        id: 1,
+        type: GrowthEventTypes.ProfilePublished,
+        status: GrowthEventStatuses.Logged,
+        ethAddress: this.userA,
+        createdAt: this.duringCampaign
+      },
+      {
+        id: 2,
+        type: GrowthEventTypes.EmailAttestationPublished,
+        status: GrowthEventStatuses.Logged,
+        ethAddress: this.userA,
+        createdAt: this.duringCampaign
+      }
+    )
+
+    const rewards = await this.crules.getRewards(this.userA)
+    expect(rewards).to.deep.equal([])
+
+    const level = await this.crules.getCurrentLevel(this.userA)
+    expect(level).to.equal(1)
+  })
+
+  it(`Should graduate to level 2`, async () => {
+    this.events.push(
+      {
+        id: 3,
+        type: GrowthEventTypes.TwitterAttestationPublished,
+        status: GrowthEventStatuses.Logged,
+        ethAddress: this.userA,
+        createdAt: this.duringCampaign
+      },
+      {
+        id: 4,
+        type: GrowthEventTypes.GoogleAttestationPublished,
+        status: GrowthEventStatuses.Logged,
+        ethAddress: this.userA,
+        createdAt: this.duringCampaign
+      }
+    )
+
+    const rewards = await this.crules.getRewards(this.userA)
+    this.expectedRewards = [
+      {
+        campaignId: 1,
+        levelId: 1,
+        ruleId: 'TwitterAttestation',
+        value: {
+          currency: 'OGN',
+          amount: tokenNaturalUnits(25)
+        },
+      },
+      {
+        campaignId: 1,
+        levelId: 1,
+        ruleId: 'GoogleAttestation',
+        value: {
+          currency: 'OGN',
+          amount: tokenNaturalUnits(25)
+        }
+      }
+    ]
+    expect(rewards).to.deep.equal(this.expectedRewards)
+
+    const level = await this.crules.getCurrentLevel(this.userA)
+    expect(level).to.equal(2)
+  })
+
+  it(`Should get a reward when a referee signs up`, async () => {
+    this.crules.levels[2].rules[0]._getReferees = () => { return [this.userB] }
     this.crules.getPriorLevel = () => { return 0 }
     this.events.push(
       {
         id: 7,
         type: GrowthEventTypes.ProfilePublished,
         status: GrowthEventStatuses.Logged,
-        ethAddress: this.referee,
+        ethAddress: this.userB,
         createdAt: this.duringCampaign
       },
       {
         id: 8,
         type: GrowthEventTypes.EmailAttestationPublished,
         status: GrowthEventStatuses.Logged,
-        ethAddress: this.referee,
+        ethAddress: this.userB,
         createdAt: this.duringCampaign
       },
       {
         id: 9,
         type: GrowthEventTypes.TwitterAttestationPublished,
         status: GrowthEventStatuses.Logged,
-        ethAddress: this.referee,
+        ethAddress: this.userB,
         createdAt: this.duringCampaign
       },
       {
         id: 10,
         type: GrowthEventTypes.PhoneAttestationPublished,
         status: GrowthEventStatuses.Logged,
-        ethAddress: this.referee,
+        ethAddress: this.userB,
         createdAt: this.duringCampaign
       }
     )
-    this.crules.getEvents = () => { return this.events }
 
-    const rewards = await this.crules.getRewards(this.ethAddress)
+    const rewards = await this.crules.getRewards(this.userA)
     this.expectedRewards.push({
       campaignId: 1,
       levelId: 2,
@@ -438,28 +531,46 @@ describe('May campaign rules', () => {
         currency: 'OGN',
         amount: tokenNaturalUnits(50)
       },
-      refereeEthAddress: this.referee
+      refereeEthAddress: this.userB
     })
     expect(rewards).to.deep.equal(this.expectedRewards)
 
-    const level = await this.crules.getCurrentLevel(this.ethAddress)
+    const level = await this.crules.getCurrentLevel(this.userA)
     expect(level).to.equal(2)
   })
 
-  it(`Should remain on level 2 when a listing is purchased`, async () => {
+  it(`Should not get a reward when a non-qualifying listing is purchased`, async () => {
+    this.events.push(
+      {
+        id: 11,
+        type: GrowthEventTypes.ListingPurchased,
+        customId: '1-000-1973',
+        status: GrowthEventStatuses.Logged,
+        ethAddress: this.userA,
+        createdAt: this.duringCampaign
+      }
+    )
+
+    const rewards = await this.crules.getRewards(this.userA)
+    expect(rewards).to.deep.equal(this.expectedRewards)
+
+    const level = await this.crules.getCurrentLevel(this.userA)
+    expect(level).to.equal(2)
+  })
+
+  it(`Should get a reward when a qualifying listing is purchased`, async () => {
     this.events.push(
       {
         id: 11,
         type: GrowthEventTypes.ListingPurchased,
         customId: '1-000-1',
         status: GrowthEventStatuses.Logged,
-        ethAddress: this.ethAddress,
+        ethAddress: this.userA,
         createdAt: this.duringCampaign
       }
     )
-    this.crules.getEvents = () => { return this.events }
 
-    const rewards = await this.crules.getRewards(this.ethAddress)
+    const rewards = await this.crules.getRewards(this.userA)
     this.expectedRewards.push({
       campaignId: 1,
       levelId: 2,
@@ -471,7 +582,7 @@ describe('May campaign rules', () => {
     })
     expect(rewards).to.deep.equal(this.expectedRewards)
 
-    const level = await this.crules.getCurrentLevel(this.ethAddress)
+    const level = await this.crules.getCurrentLevel(this.userA)
     expect(level).to.equal(2)
   })
 
