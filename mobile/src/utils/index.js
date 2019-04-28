8e28deee0d7ca0ebb223e6c25913c9169e2e8817
Tom Linton <tom@originprotocol.com>
2019-04-28 23:35:20 +1200

    Prettier

--- mobile/src/utils/index.js
+++ mobile/src/utils/index.js
@@ -1,5 +1,5 @@
 // Safe get method
 export function get(object, path, defval = null) {
-  if (typeof path === "string") path = path.split(".");
-  return path.reduce((xs, x) => (xs && xs[x] ? xs[x] : defval), object);
+  if (typeof path === 'string') path = path.split('.')
+  return path.reduce((xs, x) => (xs && xs[x] ? xs[x] : defval), object)
 }
