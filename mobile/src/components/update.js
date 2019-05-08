d56da46c9589d47f8e8df9c0771b59efe4b95db0
Tom Linton <tom@originprotocol.com>
2019-05-08 15:24:38 +1200

    Prettier

--- mobile/src/components/update.js
+++ mobile/src/components/update.js
@@ -27,7 +27,8 @@ class RecommendForceUpdate extends React.Component {
   }
 
   async componentDidMount() {
-    const versionUrl = 'https://raw.githubusercontent.com/OriginProtocol/origin/master/mobile/.version'
+    const versionUrl =
+      'https://raw.githubusercontent.com/OriginProtocol/origin/master/mobile/.version'
     let versions
     try {
       const response = await fetch(versionUrl)
@@ -38,16 +39,20 @@ class RecommendForceUpdate extends React.Component {
 
     if (versions && compareVersions(version, versions.force) === -1) {
       this.setState({ upgrade: 'force' })
-    } else if (versions && compareVersions(version, versions.recommend) === -1) {
+    } else if (
+      versions &&
+      compareVersions(version, versions.recommend) === -1
+    ) {
       this.setState({ upgrade: 'recommend' })
     }
   }
 
   openStore() {
-    const APP_STORE_LINK = 'itms-apps://itunes.apple.com/us/app/apple-store/id1446091928?mt=8'
+    const APP_STORE_LINK =
+      'itms-apps://itunes.apple.com/us/app/apple-store/id1446091928?mt=8'
     const PLAY_STORE_LINK = 'market://details?id=com.origincatcher'
     const storeLink = Platform.OS === 'ios' ? APP_STORE_LINK : PLAY_STORE_LINK
-    Linking.canOpenURL(storeLink).then((supported) => {
+    Linking.canOpenURL(storeLink).then(supported => {
       if (supported) {
         Linking.openURL(storeLink).catch(error => {
           console.error('Error opening store URL: ', error.message)
@@ -118,7 +123,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#293f55',
     justifyContent: 'center',
-    flex: 1,
+    flex: 1
   },
   buttonContainer: {
     backgroundColor: '#293f55',
@@ -127,7 +132,7 @@ const styles = StyleSheet.create({
   },
   button: {
     marginBottom: 10,
-    marginHorizontal: 20,
+    marginHorizontal: 20
   },
   title: {
     color: 'white',
