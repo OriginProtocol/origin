f0c61941b61712ab9f2fadab6c386c793b12f525
Domen Grabec <grabec@gmail.com>
2019-05-10 00:47:29 +0200

    prettify

--- infra/growth/src/util/bannedUserError.js
+++ infra/growth/src/util/bannedUserError.js
@@ -2,7 +2,7 @@ class BannedUserError extends Error {
   constructor(msg) {
     super(msg)
     this.name = this.constructor.name
-   }
- }
+  }
+}
 
- module.exports = { BannedUserError }
+module.exports = { BannedUserError }
