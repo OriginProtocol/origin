8222815009d57df6cb060ede363a9d33d9b91bc0
Shah <shahthepro@outlook.com>
2019-05-14 10:11:16 +0530

    Fix AvailabilityCalculator.js

--- packages/graphql/src/utils/AvailabilityCalculator.js
+++ packages/graphql/src/utils/AvailabilityCalculator.js
@@ -35,10 +35,19 @@ class AvailabilityCalculator {
     const start = dayjs(startStr).subtract(1, 'day'),
       end = dayjs(endStr).add(1, 'day')
 
-    if (
-      start.isBefore(dayjs().subtract(1, 'day')) ||
-      end.isAfter(slotRangeMax)
-    ) {
+    /**
+     * Hack:
+     *
+     * dayjs('2019-05-15') will return date object with value 2019-05-15 00:00:00
+     * dayjs() will return current date with current time. e.g. 2019-05-15 12:30:23
+     * So if startStr is current date, start.isBefore(dayjs()) will always fail
+     * The following line will give date object with the current date at 00:00:00 time
+     */
+    const currentDateIgnoringTime = dayjs(
+      dayjs().format('YYYY-MM-DD')
+    ).subtract(1, 'day') // This subtract is to match it with `start` vairable in L35
+
+    if (start.isBefore(currentDateIgnoringTime) || end.isAfter(slotRangeMax)) {
       throw 'Cannot update() range outside of one year limit.'
     }
 
