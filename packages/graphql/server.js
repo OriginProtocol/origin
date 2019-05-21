0d40442db7c6583c4b2d1333a2a87ccf046c60ae
Mike Shultz <shultzm@gmail.com>
2019-05-21 12:25:29 -0600

    minor linting

--- packages/graphql/server.js
+++ packages/graphql/server.js
@@ -16,7 +16,7 @@ server
       const v = parseInt(process.env.GRAPHQL_SERVER_PORT)
       if (v && Number.isInteger(v)) {
         return v
-      } else  if (v && Number.isNaN(v)) {
+      } else if (v && Number.isNaN(v)) {
         console.warn('GRAPHQL_SERVER_PORT is NaN.  Using default')
       }
       return 4002
