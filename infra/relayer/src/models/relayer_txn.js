a42b44ab5a570ee60f779bee8e62c3521365a51c
Franck <franck@originprotocol.com>
2019-05-24 08:30:38 -0700

    [relayer] Add DB logging and risk hook (#2335)
    * Log relayer transactions in the DB
    
    * Log declined txn
    
    * Add hook for dev/prod fraud model

---
+++ infra/relayer/src/models/relayer_txn.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const enums = require('../enums')
+
+module.exports = (sequelize, DataTypes) => {
+  const RelayerTxn = sequelize.define(
+    'RelayerTxn',
+    {
+      status: DataTypes.ENUM(enums.RelayerTxnStatuses),
+      from: DataTypes.STRING, // Issuer ETH address.
+      to: DataTypes.STRING, // Contract address.
+      method: DataTypes.STRING, // Contract method called.
+      forwarder: DataTypes.STRING, // Address of the hot wallet used to forward the transaction.
+      gas: DataTypes.INTEGER, // Gas used, in gwei.
+      txHash: DataTypes.STRING, // Blockchain transaction hash.
+      data: DataTypes.JSONB // Stores various metadata.
+    },
+    {
+      tableName: 'relayer_txn'
+    }
+  )
+
+  return RelayerTxn
+}
