8177c1a663b264cb7a9ceb638035a3c7b42ce7aa
Shahul Hameed <shah@Shahs-MBP.local>
2019-05-26 22:02:49 +0530

    Restrict links on menu

--- dapps/marketplace/src/pages/nav/Mobile.js
+++ dapps/marketplace/src/pages/nav/Mobile.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import { fbt } from 'fbt-runtime'
 
+import withIdentity from 'hoc/withIdentity'
+
 import Dropdown from 'components/Dropdown'
 import Link from 'components/Link'
+import RouteToUserActivation from 'components/RouteToUserActivation'
 import withEnrolmentModal from 'pages/growth/WithEnrolmentModal'
+
 
 class MobileNav extends Component {
   constructor(props) {
@@ -13,7 +17,7 @@ class MobileNav extends Component {
   }
 
   render() {
-    const { onClose } = this.props
+    const { onClose, identity } = this.props
     const hasUnread = ''
     /* react uses upper/lower case convention to distinguish between DOM tags
      * and user defined components. For that reason if the components starts with
@@ -21,93 +25,109 @@ class MobileNav extends Component {
      */
     const EarnTokens = this.EarnTokens
 
+
+    const RestrictedLink = props => {
+      if (!identity) {
+        return (
+          <a className={`dropdown-item${props.className ? ' ' + props.className : ''}`} onClick={() => this.setState({ userActivation: true })}>
+            { props.children }
+          </a>
+        )
+      }
+
+      return <Link {...props} />
+    }
+
     return (
-      <Dropdown
-        className="nav-item mobile d-flex d-md-none"
-        open={this.props.open}
-        onClose={() => this.props.onClose()}
-        content={
-          <div className="dropdown-menu show">
-            <Link
-              onClick={() => onClose()}
-              to="/"
-              className="dropdown-item browse"
-              children={fbt('Browse Categories', 'navigation.BrowseCategories')}
-            />
-            <Link
-              onClick={() => onClose()}
-              to="/create"
-              className="dropdown-item add"
-              children={fbt('Add a Listing', 'navigation.AddaListing')}
-            />
-            <EarnTokens
-              className="dropdown-item earn"
-              onClose={() => onClose()}
-              onNavigation={() => onClose()}
-            >
-              <fbt desc="navbar.earnTokens">Earn Tokens</fbt>
-            </EarnTokens>
-            <div className="dropdown-divider" />
-            <h6 className="dropdown-header">My Items</h6>
-            <Link
-              onClick={() => onClose()}
-              to="/my-purchases"
-              className="dropdown-item purchases"
-              children={fbt('Purchases', 'navigation.purchases')}
-            />
-            <Link
-              onClick={() => onClose()}
-              to="/my-listings"
-              className="dropdown-item listings"
-              children={fbt('Listings', 'navigation.listings')}
-            />
-            <Link
-              onClick={() => onClose()}
-              to="/my-sales"
-              className="dropdown-item sales"
-              children={fbt('Sales', 'navigation.sales')}
-            />
-            <div className="dropdown-divider" />
-            <Link
-              onClick={() => onClose()}
-              to="/messages"
-              className="dropdown-item messages"
-              children={fbt('Messages', 'navigation.messages')}
-            />
-            <Link
-              onClick={() => onClose()}
-              to="/notifications"
-              className="dropdown-item notifications"
-              children={fbt('Notifications', 'navigation.notifications')}
-            />
-            <Link
-              onClick={() => onClose()}
-              to="/settings"
-              className="dropdown-item settings"
-              children={fbt('Settings', 'navigation.settings')}
-            />
-            {/* <a href="#" className="dropdown-item transactions">
-              Transactions
-            </a> */}
-          </div>
-        }
-      >
-        <a
-          className="nav-link"
-          href="#"
-          onClick={e => {
-            e.preventDefault()
-            this.props.open ? this.props.onClose() : this.props.onOpen()
-          }}
+      <>
+        { this.state.userActivation && <RouteToUserActivation onClose={() => this.setState({ userActivation: false })} /> }
+        <Dropdown
+          className="nav-item mobile d-flex d-md-none"
+          open={this.props.open}
+          onClose={() => this.props.onClose()}
+          content={
+            <div className="dropdown-menu show">
+              <Link
+                onClick={() => onClose()}
+                to="/"
+                className="dropdown-item browse"
+                children={fbt('Browse Categories', 'navigation.BrowseCategories')}
+              />
+              <RestrictedLink
+                onClick={e => onClose()}
+                to="/create"
+                className="dropdown-item add"
+                children={fbt('Add a Listing', 'navigation.AddaListing')}
+              />
+              <EarnTokens
+                className="dropdown-item earn"
+                onClose={() => onClose()}
+                onNavigation={() => onClose()}
+              >
+                <fbt desc="navbar.earnTokens">Earn Tokens</fbt>
+              </EarnTokens>
+              <div className="dropdown-divider" />
+              <h6 className="dropdown-header">My Items</h6>
+              <Link
+                onClick={() => onClose()}
+                to="/my-purchases"
+                className="dropdown-item purchases"
+                children={fbt('Purchases', 'navigation.purchases')}
+              />
+              <Link
+                onClick={() => onClose()}
+                to="/my-listings"
+                className="dropdown-item listings"
+                children={fbt('Listings', 'navigation.listings')}
+              />
+              <Link
+                onClick={() => onClose()}
+                to="/my-sales"
+                className="dropdown-item sales"
+                children={fbt('Sales', 'navigation.sales')}
+              />
+              <div className="dropdown-divider" />
+              <RestrictedLink
+                onClick={() => onClose()}
+                to="/messages"
+                className="dropdown-item messages"
+                children={fbt('Messages', 'navigation.messages')}
+              />
+              <Link
+                onClick={() => onClose()}
+                to="/notifications"
+                className="dropdown-item notifications"
+                children={fbt('Notifications', 'navigation.notifications')}
+              />
+              <Link
+                onClick={() => onClose()}
+                to="/settings"
+                className="dropdown-item settings"
+                children={fbt('Settings', 'navigation.settings')}
+              />
+              {/* <a href="#" className="dropdown-item transactions">
+                Transactions
+              </a> */}
+            </div>
+          }
         >
-          <div className={`mobile-icon${hasUnread}`} />
-        </a>
-      </Dropdown>
+          <a
+            className="nav-link"
+            href="#"
+            onClick={e => {
+              e.preventDefault()
+              this.props.open ? this.props.onClose() : this.props.onOpen()
+            }}
+          >
+            <div className={`mobile-icon${hasUnread}`} />
+          </a>
+        </Dropdown>
+      </>
     )
   }
 }
 
-export default MobileNav
+export default withIdentity(MobileNav)
 
 require('react-styl')(`
   .navbar .nav-item.mobile
