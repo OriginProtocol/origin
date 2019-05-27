577ad713d3be7efc3fc60821531453a5c2d46968
Mike Shultz <shultzm@gmail.com>
2019-05-27 11:23:38 -0600

    disables performanceMode for the listener, and also the docker network

--- packages/graphql/src/configs/docker.js
+++ packages/graphql/src/configs/docker.js
@@ -18,7 +18,7 @@ const config = {
   growth: `http://${HOST}:4001`,
   discovery: `http://${HOST}:4000/graphql`,
   notifications: `http://${HOST}:3456`,
-  performanceMode: true,
+  performanceMode: false,
   graphql: `http://${HOST}:4002`,
   automine: 2000,
   messaging: {
