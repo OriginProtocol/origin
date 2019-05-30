68fe06a9ac5341bc0f9418fd5b666adc204c2543
Franck Chastagnol <franck@originprotocol.com>
2019-05-30 15:30:24 -0700

    Added icons

--- infra/growth/src/scripts/manageCampaign.js
+++ infra/growth/src/scripts/manageCampaign.js
@@ -3,8 +3,10 @@ const db = require('../models')
 const enums = require('../enums')
 const { tokenToNaturalUnits } = require('../../src/util/token')
 
-const aprilConfig = require('../../campaigns/april.js')
-const mayConfig = require('../../campaigns/may.js')
+const aprilConfig = require('../../campaigns/april')
+const mayConfig = require('../../campaigns/may')
+const juneConfig = require('../../campaigns/june')
+
 
 async function createAprilProdCampaign() {
   console.log('Creating April campaign data in prod...')
@@ -53,6 +55,26 @@ async function updateMayProdRules() {
   await campaign.update({ rules: JSON.stringify(mayConfig) })
 }
 
+async function createJuneProdCampaign() {
+  console.log('Creating June campaign data in prod...')
+
+  /* IMPORTANT when adding new translatable fields update the enums document:
+   * origin-dapp/src/constants/Growth$FbtEnum.js
+   */
+  await db.GrowthCampaign.create({
+    nameKey: 'growth.june2019.name',
+    shortNameKey: 'growth.june2019.short_name',
+    rules: JSON.stringify(juneConfig),
+    startDate: Date.parse('June 1, 2019, 00:00 UTC'),
+    endDate: Date.parse('July 1, 2019, 00:00 UTC'),
+    distributionDate: Date.parse('June 1, 2019, 00:00 UTC'),
+    cap: tokenToNaturalUnits(1000000), // Set cap to 1M tokens
+    capUsed: 0,
+    currency: 'OGN',
+    rewardStatus: enums.GrowthCampaignRewardStatuses.Active
+  })
+}
+
 const args = {}
 process.argv.forEach(arg => {
   const t = arg.split('=')
@@ -68,6 +90,11 @@ if (args['--action'] === 'create') {
     })
   } else if (args['--month'] === 'may') {
     createMayProdCampaign().then(() => {
+      console.log('Done')
+      process.exit()
+    })
+  } else if (args['--month'] === 'june') {
+    createJuneProdCampaign().then(() => {
       console.log('Done')
       process.exit()
     })
