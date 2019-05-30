7cae5d81f93f526b720dfd8d48082d937398c676
Stan James <stan@wanderingstan.com>
2019-05-30 09:48:15 -0600

    Add listing name and message sender to mobile templates

--- infra/notifications/templates/messageTemplates.js
+++ infra/notifications/templates/messageTemplates.js
@@ -10,8 +10,10 @@ const messageTemplates = {
   message: {
     mobile: {
       messageReceived: {
-        title: _.template('New Origin Message'),
-        body: _.template('You have received a message on Origin.')
+        title: _.template('New Origin Message from <%- senderName %>'),
+        body: _.template(
+          'You have received a message on Origin from <%- senderName %>.'
+        )
       }
     },
     email: {
@@ -29,24 +31,34 @@ const messageTemplates = {
   seller: {
     mobile: {
       OfferCreated: {
-        title: _.template('New Offer'),
-        body: _.template('A buyer has made an offer on your listing.')
+        title: _.template('New Offer for <%= listing.title %>'),
+        body: _.template(
+          'A buyer has made an offer on your listing <%= listing.title %>'
+        )
       },
       OfferWithdrawn: {
-        title: _.template('Offer Withdrawn'),
-        body: _.template('An offer on your listing has been withdrawn.')
+        title: _.template('Offer Withdrawn for <%= listing.title %>'),
+        body: _.template(
+          'An offer on your listing <%= listing.title %> has been withdrawn.'
+        )
       },
       OfferDisputed: {
-        title: _.template('Dispute Initiated'),
-        body: _.template('A problem has been reported with your transaction.')
+        title: _.template('Dispute Initiated for <%= listing.title %>'),
+        body: _.template(
+          'A problem has been reported with your transaction for <%= listing.title %>.'
+        )
       },
       OfferRuling: {
-        title: _.template('Dispute Resolved'),
-        body: _.template('A ruling has been issued on your disputed transaction.')
+        title: _.template('Dispute Resolved for <%= listing.title %>'),
+        body: _.template(
+          'A ruling has been issued on your disputed transaction for <%= listing.title %>.'
+        )
       },
       OfferFinalized: {
-        title: _.template('Sale Completed'),
-        body: _.template('Your transaction has been completed.')
+        title: _.template('Sale Completed for <%= listing.title %>'),
+        body: _.template(
+          'Your transaction for <%= listing.title %> has been completed.'
+        )
       }
     },
     email: {
@@ -104,24 +116,34 @@ const messageTemplates = {
   buyer: {
     mobile: {
       OfferWithdrawn: {
-        title: _.template('Offer Rejected'),
-        body: _.template('An offer you made has been rejected.')
+        title: _.template('Offer Rejected for <%= listing.title %>'),
+        body: _.template(
+          'An offer you made for <%= listing.title %> has been rejected.'
+        )
       },
       OfferAccepted: {
-        title: _.template('Offer Accepted'),
-        body: _.template('An offer you made has been accepted.')
+        title: _.template('Offer Accepted for <%= listing.title %>'),
+        body: _.template(
+          'An offer you made for <%= listing.title %> has been accepted.'
+        )
       },
       OfferDisputed: {
-        title: _.template('Dispute Initiated'),
-        body: _.template('A problem has been reported with your transaction.')
+        title: _.template('Dispute Initiated for <%= listing.title %>'),
+        body: _.template(
+          'A problem has been reported with your transaction for <%= listing.title %>.'
+        )
       },
       OfferRuling: {
-        title: _.template('Dispute Resolved'),
-        body: _.template('A ruling has been issued on your disputed transaction.')
+        title: _.template('Dispute Resolved for <%= listing.title %>'),
+        body: _.template(
+          'A ruling has been issued on your disputed transaction for <%= listing.title %>.'
+        )
       },
       OfferData: {
-        title: _.template('New Review'),
-        body: _.template('A review has been left on your transaction.')
+        title: _.template('New Review for <%= listing.title %>'),
+        body: _.template(
+          'A review has been left on your transaction for <%= listing.title %>.'
+        )
       }
     },
     email: {
