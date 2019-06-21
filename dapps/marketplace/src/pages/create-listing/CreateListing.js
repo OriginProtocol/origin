29e2cc85d9a4726eb155434f397b1ed8a7696f17
Nick Poulden <nick@poulden.com>
2019-06-21 13:41:27 -0600

    Create listing

--- dapps/marketplace/src/pages/create-listing/CreateListing.js
+++ dapps/marketplace/src/pages/create-listing/CreateListing.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { fbt } from 'fbt-runtime'
 import get from 'lodash/get'
@@ -10,166 +10,154 @@ import withIdentity from 'hoc/withIdentity'
 import DocumentTitle from 'components/DocumentTitle'
 import UserActivationLink from 'components/UserActivationLink'
 
-import UnitListing from './listing-types/UnitListing/UnitListing'
-import FractionalListing from './listing-types/FractionalListing/FractionalListing'
-import AnnouncementListing from './listing-types/AnnouncementListing/AnnouncementListing'
-import FractionalHourlyListing from './listing-types/FractionalHourlyListing/FractionalHourlyListing'
-import GiftCardListing from './listing-types/GiftCardListing/GiftCardListing'
-
+import listingTypes from './listing-types'
 import ChooseListingType from './ChooseListingType'
 
 import Store from 'utils/store'
 const store = Store('sessionStorage')
 
-class CreateListing extends Component {
-  constructor(props) {
-    super(props)
-    // If a listing is passed in (as when editing) use that, otherwise
-    // fall back to anything in `store` (an unfinished listing creation)
-    const preexistingListingData =
-      props.listing || store.get('create-listing') || {}
-    this.state = {
-      listing: {
-        __typename: 'UnitListing', // Default
-        title: '',
-        description: '',
-        category: '',
-        subCategory: '',
-        location: '',
-        boost: '0',
-        boostLimit: '0',
-        media: [],
+function initialState(props) {
+  // If a listing is passed in (as when editing) use that, otherwise
+  // fall back to anything in `store` (an unfinished listing creation)
+  const existingListing = props.listing || store.get('create-listing') || {}
 
-        // Unit fields:
-        quantity: '1',
-        price: '',
-        currency: 'fiat-USD',
-        acceptedTokens: ['token-DAI'],
+  return {
+    __typename: 'UnitListing', // Default
+    title: '',
+    description: '',
+    category: '',
+    subCategory: '',
+    location: '',
+    boost: '0',
+    boostLimit: '0',
+    media: [],
 
-        // Fractional fields:
-        timeZone: '',
-        workingHours: [],
-        weekendPrice: '',
-        booked: [],
-        customPricing: [],
-        unavailable: [],
+    // Unit fields:
+    quantity: '1',
+    price: '',
+    currency: 'fiat-USD',
+    acceptedTokens: ['token-DAI'],
 
-        // Gift Card fields:
-        retailer: '',
-        cardAmount: '',
-        issuingCountry: 'US',
-        isDigital: false,
-        isCashPurchase: false,
-        receiptAvailable: false,
+    // Fractional fields:
+    timeZone: '',
+    workingHours: [],
+    weekendPrice: '',
+    booked: [],
+    customPricing: [],
+    unavailable: [],
 
-        // Marketplace creator fields:
-        marketplacePublisher: get(props, 'creatorConfig.marketplacePublisher'),
+    // Gift Card fields:
+    retailer: '',
+    cardAmount: '',
+    issuingCountry: 'US',
+    isDigital: false,
+    isCashPurchase: false,
+    receiptAvailable: false,
 
-        ...preexistingListingData
-      }
-    }
+    // Marketplace creator fields:
+    marketplacePublisher: get(props, 'creatorConfig.marketplacePublisher'),
+
+    ...existingListing
   }
+}
 
-  setListing(listing) {
-    store.set('create-listing', listing)
-    this.setState({ listing })
-  }
+const CreateListing = props => {
+  const [listing, setListing] = useState(initialState(props))
 
-  render() {
-    if (
-      this.props.creatorConfigLoading ||
-      this.props.walletLoading ||
-      this.props.identityLoading
-    ) {
-      return (
-        <div className="app-spinner">
-          <fbt desc="App.loadingPleaseWait">
-            <h5>Loading</h5>
-            <div>Please wait</div>
-          </fbt>
-        </div>
-      )
-    }
-
-    if (!this.props.identity) {
-      return (
-        <UserActivationLink
-          location={{ pathname: '/create' }}
-          forceRedirect={true}
-        />
-      )
-    }
-
-    // Force a given listing type/category
-    // Hack: '__' is not allowed in GraphQL where we get our config from, so we change
-    // `typename` into `__typename` here.
-    const forceType =
-      this.props.creatorConfig && this.props.creatorConfig.forceType
-        ? {
-            ...this.props.creatorConfig.forceType,
-            __typename: this.props.creatorConfig.forceType.typename
-          }
-        : {}
-
-    const listingTypeMapping = {
-      UnitListing,
-      AnnouncementListing,
-      FractionalListing,
-      FractionalHourlyListing,
-      GiftCardListing
-    }
-
-    const props = {
-      listing: { ...this.state.listing, ...forceType },
-      onChange: listing => this.setListing(listing)
-    }
-
-    // Get creation component for listing type (__typename),
-    // defaulting to UnitListing
-    const ListingTypeComponent =
-      listingTypeMapping[props.listing.__typename] || UnitListing
-
+  if (
+    props.creatorConfigLoading ||
+    props.walletLoading ||
+    props.identityLoading
+  ) {
     return (
-      <div className="container create-listing">
-        <DocumentTitle
-          pageTitle={<fbt desc="createListing.title">Add A Listing</fbt>}
-        />
-        <Switch>
-          <Route
-            path="/create/details"
-            render={() => (
-              <ListingTypeComponent linkPrefix="/create/details" {...props} />
-            )}
-          />
-          <Route
-            path="/listing/:listingId/edit/:step"
-            render={({ match }) => (
-              <ListingTypeComponent
-                linkPrefix={`/listing/${match.params.listingId}/edit/details`}
-                refetch={this.props.refetch}
-                {...props}
-              />
-            )}
-          />
-          <Route
-            path="/listing/:listingId/edit"
-            render={({ match }) => (
-              <ChooseListingType
-                next={`/listing/${match.params.listingId}/edit/details`}
-                {...props}
-              />
-            )}
-          />
-          <Route
-            path="/create"
-            render={() => (
-              <ChooseListingType next="/create/details" {...props} />
-            )}
-          />
-        </Switch>
+      <div className="app-spinner">
+        <fbt desc="App.loadingPleaseWait">
+          <h5>Loading</h5>
+          <div>Please wait</div>
+        </fbt>
       </div>
     )
   }
+
+  if (!props.identity) {
+    return (
+      <UserActivationLink
+        location={{ pathname: '/create' }}
+        forceRedirect={true}
+      />
+    )
+  }
+
+  // Force a given listing type/category
+  // Hack: '__' is not allowed in GraphQL where we get our config from, so we change
+  // `typename` into `__typename` here.
+  const forceType =
+    props.creatorConfig && props.creatorConfig.forceType
+      ? {
+          ...props.creatorConfig.forceType,
+          __typename: props.creatorConfig.forceType.typename
+        }
+      : {}
+
+  const cmpProps = {
+    listing: { ...listing, ...forceType },
+    onChange: listing => {
+      setListing(listing)
+      store.set('create-listing', listing)
+    }
+  }
+
+  // Get creation component for listing type (__typename),
+  // defaulting to UnitListing
+  const listingType = listing.__typename || 'UnitListing'
+  const ListingTypeComponent = listingTypes[listingType]
+
+  return (
+    <div className="container create-listing">
+      <DocumentTitle
+        pageTitle={<fbt desc="createListing.title">Add A Listing</fbt>}
+      />
+      <Switch>
+        <Route
+          path="/create/details"
+          render={() => (
+            <ListingTypeComponent linkPrefix="/create/details" {...cmpProps} />
+          )}
+        />
+        <Route
+          path="/listing/:listingId/edit/:step"
+          render={({ match }) => (
+            <ListingTypeComponent
+              linkPrefix={`/listing/${match.params.listingId}/edit/details`}
+              refetch={props.refetch}
+              {...cmpProps}
+            />
+          )}
+        />
+        <Route
+          path="/listing/:listingId/edit"
+          render={({ match }) => (
+            <ChooseListingType
+              next={`/listing/${match.params.listingId}/edit/details`}
+              {...cmpProps}
+            />
+          )}
+        />
+        <Route
+          path="/create"
+          render={() => (
+            <ChooseListingType next="/create/listing-type" {...cmpProps} />
+          )}
+        />
+        <Route
+          path="/create/listing-type"
+          render={() => (
+            <ChooseListingType next="/create/details" {...cmpProps} />
+          )}
+        />
+      </Switch>
+    </div>
+  )
 }
 
 export default withCreatorConfig(withWallet(withIdentity(CreateListing)))
