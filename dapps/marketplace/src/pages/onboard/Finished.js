8e2b0a173ab9caf64b8f26a1e56f74c886e838f3
Shahul Hameed <shahthepro@gmail.com>
2019-06-21 23:43:05 +0530

    Refactor onboarding flow (#2504)
    * Refactor Onboarding
    
    * Prettify
    
    * Lint
    
    * Translate
    
    * Style fix
    
    * Remove unnecessary fragment
    
    * Lint
    
    * Onboard tweaks

--- dapps/marketplace/src/pages/onboard/Finished.js
+++ dapps/marketplace/src/pages/onboard/Finished.js
@@ -29,32 +29,4 @@ require('react-styl')(`
   .onboard .finished
     max-width: 475px
     margin: 0 auto
-    .profile-created
-      .info
-        text-align: center
-        border-radius: 5px
-        border: solid 1px var(--bluey-grey)
-        background-color: rgba(152, 167, 180, 0.1)
-        font-family: Lato
-        font-size: 14px
-        color: black
-        padding: 10px
-        margin-top: 1rem
-        .title
-          display: block
-          font-weight: bold
-          margin-bottom: 3px
-          & ~ a
-            margin-left: 5px
-        &.white
-          border: solid 1px #c2cbd3
-          background-color: white
-          display: flex
-          text-align: left
-          .image
-            flex: auto 0 0
-            img
-              margin-right: 1rem
-          .content
-            flex: auto 1 1
 `)
