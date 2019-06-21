9d75f8d107bcb9e0e4e4f7873c9eada655eac6a8
Tom Linton <tom@originprotocol.com>
2019-06-21 15:13:34 +1200

    Common and uncommon country separation

--- mobile/src/screens/onboarding/phone.js
+++ mobile/src/screens/onboarding/phone.js
@@ -16,16 +16,34 @@ import withConfig from 'hoc/withConfig'
 import OnboardingStyles from 'styles/onboarding'
 import _countryCodes from 'utils/countryCodes'
 
-const countryCodes = _countryCodes
+const commonCountryCodes = [
+  '1', // US/CA
+  '7', // RU
+  '44', // UK
+  '61', // AU
+  '81', // JP
+  '82', // KR
+  '86', // CN,
+  '91' // IN
+]
+
+const [commonCountries, uncommonCountries] = _countryCodes
+  // Generate the options
   .map(item => {
     return {
       label: `${item.name} (${item.prefix})`,
       value: item.prefix
     }
   })
-  .sort((a, b) => (a.label > b.label ? 1 : -1))
-  // Filter to New Zealand in development
-  .filter(x => !__DEV__ || x.value == 64)
+  // Partition into common and uncommon countries to allow for a separator in
+  // the select
+  .reduce(
+    (result, option) => {
+      result[commonCountryCodes.includes(option.value) ? 0 : 1].push(option)
+      return result
+    },
+    [[], []]
+  )
 
 class PhoneScreen extends Component {
   constructor(props) {
@@ -183,10 +201,14 @@ class PhoneScreen extends Component {
           </Text>
           <RNPickerSelect
             placeholder={{ label: 'Select a country', value: null }}
-            items={countryCodes}
-            onValueChange={async value =>
+            items={[
+              ...commonCountries,
+              { value: false, label: '---' },
+              ...uncommonCountries
+            ]}
+            onValueChange={async value => {
               await this.handleChange('countryCode', value)
-            }
+            }}
             style={pickerSelectStyles}
           />
           <TextInput
@@ -223,6 +245,7 @@ class PhoneScreen extends Component {
             title={fbt('Continue', 'PhoneScreen.continueButton')}
             disabled={
               !this.state.phoneValue.length ||
+              !this.state.countryCodeValue ||
               this.state.phoneError ||
               this.state.loading
             }
