185daf4f76ee43b3b81725e4faabd5d38da55f3e
Shahul Hameed <shahthepro@outlook.com>
2019-06-25 16:52:16 +0530

    Update tests

--- infra/bridge/test/utils.js
+++ infra/bridge/test/utils.js
@@ -4,6 +4,7 @@ const chai = require('chai')
 const expect = chai.expect
 const request = require('supertest')
 const redis = require('redis')
+const nock = require('nock')
 
 const Identity = require('@origin/identity/src/models').Identity
 const app = require('../src/app')
@@ -73,23 +74,40 @@ describe('identity exists', () => {
 
 describe('exchange rate poller', () => {
   beforeEach(() => {
-    client.del('ETH_USD_price')
-    process.env.FALLBACK_EXCHANGE_RATE = '12345.6789'
+    client.del('ETH-USD_price')
+    process.env.FALLBACK_EXCHANGE_RATE_XXX_USD = '12345.6789'
   })
 
   it('should return exchange rate from redis', async () => {
-    await client.set('ETH_USD_price', '234')
+    await client.set('ETH-USD_price', '234')
     const response = await request(app)
-      .get('/utils/exchange-rate')
+      .get('/utils/exchange-rate?market=ETH-USD')
       .expect(200)
 
     expect(response.status).to.equal(200)
     expect(response.body.price).to.equal('234')
   })
 
-  it('should return default exchange rate if not cached', async () => {
+  it('should fetch exchange rate from API if not cached', async () => {
+    nock('https://api.cryptonator.com')
+      .get('/api/ticker/YYY-USD')
+      .reply(200, { success: true, ticker: { price: '678.9' } })
+
     const response = await request(app)
-      .get('/utils/exchange-rate')
+      .get('/utils/exchange-rate?market=YYY-USD')
+      .expect(200)
+
+    expect(response.status).to.equal(200)
+    expect(response.body.price).to.equal('678.9')
+  })
+
+  it('should return default exchange rate if not cached and API is down', async () => {
+    nock('https://api.cryptonator.com')
+      .get('/api/ticker/XXX-USD')
+      .reply(200, { success: false, error: `Invalid pair` })
+
+    const response = await request(app)
+      .get('/utils/exchange-rate?market=XXX-USD')
       .expect(200)
 
     expect(response.status).to.equal(200)
