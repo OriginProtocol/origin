8f4a620980dae8e3f8c98e74d08fd871ddb29fa5
Sergio Zharinov <zharinov@users.noreply.github.com>
2019-07-01 18:51:36 +0400

    Create page 404 as React component, use it for listings (#2561)
    * Create page 404 as React component, use it for listings
    
    * Small fixes for 404 section
    
    * Get rid of redundant bootstrap grid elements
    
    * Fix linting

--- dapps/marketplace/src/pages/listing/Listing.js
+++ dapps/marketplace/src/pages/listing/Listing.js
@@ -7,11 +7,20 @@ import QueryError from 'components/QueryError'
 import DocumentTitle from 'components/DocumentTitle'
 import LoadingSpinner from 'components/LoadingSpinner'
 import Redirect from 'components/Redirect'
+import Error404 from 'components/Error404'
 
 import query from 'queries/Listing'
 import ListingDetail from './ListingDetail'
 import EditListing from './Edit'
 import Onboard from '../onboard/Onboard'
+
+const error404 = (
+  <Error404>
+    <h1 className="d-md-block">
+      <fbt desc="listing.listing-not-found">Listing not found</fbt>
+    </h1>
+  </Error404>
+)
 
 class Listing extends Component {
   state = { quantity: '1' }
@@ -33,11 +42,14 @@ class Listing extends Component {
             </fbt>
           }
         />
-        <Query query={query} variables={vars}>
+        <Query query={query} variables={vars} errorPolicy="all">
           {({ networkStatus, error, data, refetch }) => {
             if (networkStatus <= 2) {
               return <LoadingSpinner />
             } else if (error) {
+              if (data && data.marketplace && !data.marketplace.listing) {
+                return error404
+              }
               return <QueryError error={error} query={query} vars={vars} />
             } else if (!data || !data.marketplace) {
               return <div>No marketplace contract?</div>
@@ -45,11 +57,7 @@ class Listing extends Component {
 
             const listing = data.marketplace.listing
             if (!listing) {
-              return (
-                <div>
-                  <fbt desc="listing.listing-not-found">Listing not found</fbt>
-                </div>
-              )
+              return error404
             } else if (!listing.valid) {
               return (
                 <div>
