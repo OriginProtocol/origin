3d1a48f426f854baa8f5004c13af4baa3a4cde23
Tom Linton <tom@originprotocol.com>
2019-07-02 13:02:24 +1200

    Clear duplicate styles

--- mobile/src/styles/onboarding.js
+++ mobile/src/styles/onboarding.js
@@ -24,30 +24,6 @@ export default StyleSheet.create({
     textAlign: 'center',
     width: '80%'
   },
-  termsHeader: {
-    fontWeight: '600',
-    fontSize: 18,
-    marginBottom: 20,
-    textAlign: 'center'
-  },
-  termsText: {
-    paddingHorizontal: 20,
-    marginBottom: 20,
-    color: '#111d28'
-  },
-  termsHighlightContainer: {
-    borderColor: '#98a7b4',
-    backgroundColor: 'rgba(152, 167, 180, 0.1)',
-    borderWidth: 1,
-    paddingVertical: 20,
-    paddingHorizontal: 20,
-    marginVertical: 20,
-    width: '90%',
-    borderRadius: 5
-  },
-  termsHighlightText: {
-    color: '#6f8294'
-  },
   backArrow: {
     position: 'absolute',
     left: -10,
