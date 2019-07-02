8f78b3f249c3c8f5da1c55af3100a8a9c7c9ed2c
Shahul Hameed <shahthepro@outlook.com>
2019-07-02 08:46:34 +0530

    DApp3 Bug Fixes (#2586)
    * Add `identityLoaded` prop
    
    * Fix "Earn OGN" link in Profile Dropdown
    
    * Fix "Get More" link
    
    * Disable "exit" animation for attestation modals
    
    * Close "Sign To Publish" Modal on cancel
    
    * Prettify
    
    * Full-bleed categories container
    
    * Close right dropdown before showing Rewards modal
    
    * prettify
    
    * Clear search value after navigating
    
    * Hide "DownloadApp" component on Origin Wallet
    
    * Add slide in/out animation for mobile drawers
    
    * Prettify
    
    * Disallow dismissing of metamask modal
    
    * Fix dropdown on Desktop
    
    * Revert bridge
    
    * Revert animations
    
    * Revert styles
    
    * Revert attestationIssuer

--- dapps/marketplace/src/components/DownloadApp.js
+++ dapps/marketplace/src/components/DownloadApp.js
@@ -1,33 +1,41 @@
 import React from 'react'
 import { fbt } from 'fbt-runtime'
 
-const DownloadApp = () => (
-  <div className="onboard-help origin-wallet">
-    <div className="app-image mb-3">
-      <img src="images/mobile/devices-layered.png" />
-    </div>
-    <h5>
-      <fbt desc="onboard.Wallet.downloadApp">
-        Download the Origin Marketplace App
-      </fbt>
-    </h5>
-    <div className="description">
-      <fbt desc="onboard.Wallet.unlockExperience">
-        Unlock the full experience
-      </fbt>
-    </div>
-    <div className="actions">
-      <a href="https://originprotocol.com/mobile">
-        <img src="images/app-store-button@2x.png" />
-      </a>
-      <a href="https://originprotocol.com/mobile">
-        <img src="images/android-button@2x.png" />
-      </a>
-    </div>
-  </div>
-)
+import withWallet from 'hoc/withWallet'
 
-export default DownloadApp
+const DownloadApp = ({ walletType }) => {
+  if (walletType === 'Origin Wallet') {
+    return null
+  }
+
+  return (
+    <div className="onboard-help origin-wallet">
+      <div className="app-image mb-3">
+        <img src="images/mobile/devices-layered.png" />
+      </div>
+      <h5>
+        <fbt desc="onboard.Wallet.downloadApp">
+          Download the Origin Marketplace App
+        </fbt>
+      </h5>
+      <div className="description">
+        <fbt desc="onboard.Wallet.unlockExperience">
+          Unlock the full experience
+        </fbt>
+      </div>
+      <div className="actions">
+        <a href="https://originprotocol.com/mobile">
+          <img src="images/app-store-button@2x.png" />
+        </a>
+        <a href="https://originprotocol.com/mobile">
+          <img src="images/android-button@2x.png" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default withWallet(DownloadApp)
 
 require('react-styl')(`
   .onboard-help.origin-wallet
