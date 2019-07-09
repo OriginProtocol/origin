642d138751ee0a6dc998fa2a41596cc1d94d284a
Nick Poulden <nick@poulden.com>
2019-07-09 09:17:21 -0600

    Style tweaks

--- dapps/marketplace/src/pages/nav/Mobile.js
+++ dapps/marketplace/src/pages/nav/Mobile.js
@@ -184,6 +184,8 @@ require('react-styl')(`
         padding: 0.75rem 1.5rem 0.75rem 4rem
         overflow: hidden
         text-overflow: ellipsis
+        &:active,&.active
+          background-color: var(--pale-grey)
         &::before
           position: absolute
           left: 1.25rem
