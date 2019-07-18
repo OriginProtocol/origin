9b5d300bd8164dada2fd2894e108d63f6b6a9433
Tom Linton <tom@originprotocol.com>
2019-07-18 16:11:43 -0700

    wip

--- infra/token-transfer-server/src/models/grant.js
+++ infra/token-transfer-server/src/models/grant.js
@@ -3,11 +3,6 @@
 const BigNumber = require('bignumber.js')
 const moment = require('moment')
 
-// Avoid valueOf coercion
-BigNumber.prototype.valueOf = function () {
-  throw Error('valueOf called!')
-}
-
 module.exports = (sequelize, DataTypes) => {
   const Grant = sequelize.define(
     'Grant',
@@ -40,44 +35,32 @@ module.exports = (sequelize, DataTypes) => {
   Grant.prototype.vestingSchedule = function() {
     const now = this.now || moment()
 
-    const vestingSchedule = []
     if (now < this.cliff) {
-      return vestingSchedule
+      return []
     }
 
     const vestingEventCount = moment(this.end).diff(this.start, this.interval)
-    const cliffVestingCount = moment(this.cliff).diff(this.start, this.interval)
     const vestedPerEvent = BigNumber(this.amount).div(vestingEventCount)
+    const cliffVestingCount = moment(this.cliff).diff(this.start, this.interval)
+    const cliffVestAmount = vestedPerEvent.times(cliffVestingCount)
+    const remainingVestingCount = vestingEventCount - cliffVestingCount
+    const remainingVestingAmounts = Array(remainingVestingCount).fill(vestedPerEvent)
 
-    // Add the cliff vest
-    vestingSchedule.push({
-      datetime: moment(this.cliff),
-      amount: vestedPerEvent.times(cliffVestingCount)
-    })
-
-    // Generate an array of all remaining vesting events
-    const remainingVestingEventCount = vestingEventCount - cliffVestingCount
-    const remainingVestingEvents = Array.from({ length: remainingVestingEventCount })
-      .map((v, i) => {
-        return {
-          datetime: moment(this.cliff).add(i + 1, this.interval),
-          amount: vestedPerEvent
-        }
-      })
-
-    // Add all remaining vesting events
-    return vestingSchedule.concat(remainingVestingEvents)
+    return [cliffVestAmount, ...remainingVestingAmounts]
   }
 
   // Returns current number of vested tokens for this grant
   Grant.prototype.calculateVested = function() {
     const now = this.now || moment()
 
-    const vested = this.vestingSchedule().reduce((total, vestingEvent) => {
-      return total.plus(now >= vestingEvent.datetime ? vestingEvent.amount : BigNumber(0))
-    }, BigNumber(0))
+    // Number of vesting events that have occurred since the cliff determines
+    // the index of the array for calculating events that have already vested
+    const threshold = moment(now).diff(this.cliff, this.interval)
+    // Buld array of already vested amounts
+    const vested = this.vestingSchedule().slice(0, threshold + 1)
 
-    return vested
+    // Sum
+    return vested.reduce((total, amount) => total.plus(amount), BigNumber(0))
   }
 
   return Grant
--- infra/token-transfer-server/test/lib_vesting.test.js
+++ infra/token-transfer-server/test/lib_vesting.test.js
@@ -73,7 +73,7 @@ describe('vestingGrants', () => {
       // Remove the first element of the array, which is the cliff vest
       vestingEvents.shift()
       // All subsequent vesting events should vest the correct proportion
-      expect(vestingEvents.every(e => e.amount.isEqualTo(100))).to.equal(true)
+      expect(vestingEvents.every(e => e.isEqualTo(100))).to.equal(true)
     })
   })
 
@@ -98,6 +98,82 @@ describe('vestingGrants', () => {
       // 2016 is a leap year, so there is 2 * 365 days for standard years,
       // 1 * 366 for the leap year, and + 1 for the cliff vesting event
       expect(vestingEvents.length).to.equal(2 * 365 + 366 + 1)
+    })
+
+    it('should not vest before cliff', () => {
+      grant.now = moment(grant.cliff).subtract(1, 's')
+      const vestedAmount = grant.calculateVested()
+      expect(vestedAmount).to.be.bignumber.equal(0)
+    })
+
+    it('should vest 365/1461 at the cliff', () => {
+      grant.now = grant.cliff
+      const vestedAmount = grant.calculateVested()
+      expect(vestedAmount).to.be.bignumber.equal(
+        BigNumber(365)
+          .div(1461)
+          .times(grant.amount)
+      )
+    })
+
+    it('should vest 365/1461 after the cliff', () => {
+      grant.now = moment(grant.cliff).add(1, 's')
+      const vestedAmount = grant.calculateVested()
+      expect(vestedAmount).to.be.bignumber.equal(
+        BigNumber(365)
+          .div(1461)
+          .times(grant.amount)
+      )
+    })
+
+    it('should have the correct amount vested total', () => {
+      grant.now = grant.end
+      const vestedAmount = grant.calculateVested()
+      expect(vestedAmount).to.be.bignumber.equal(4800)
+    })
+
+    it('should vest the correct amount each day', () => {
+      grant.now = grant.end
+      const vestingEvents = grant.vestingSchedule()
+      // Remove the first element of the array, which is the cliff vest
+      vestingEvents.shift()
+      // All subsequent vesting events should vest the correct proportion
+      expect(
+        vestingEvents.every(e => {
+          return e.amount === BigNumber(grant.amount).div(1461)
+        })
+      ).to.equal(true)
+    })
+  })
+
+  /*
+  describe('4 year grant with 1 year cliff and minutes vesting', () => {
+    let grant
+
+    const days = 2 * 365 + 366
+    const minutes = days * 24 * 60
+    const vestingEvents = minutes + 1
+    const cliffVestingEvents = 365 * 24 * 60
+
+    beforeEach(async () => {
+      await setupDatabase()
+      grant = new Grant({
+        userId: 1,
+        start: '2014-01-01 00:00:00',
+        end: '2018-01-01 00:00:00',
+        cliff: '2015-01-01 00:00:00',
+        amount: 4800,
+        interval: 'minutes'
+      })
+      await grant.save()
+    })
+
+    it('should have the correct amount of vesting events', () => {
+      const vestingEvents = grant.vestingSchedule()
+      // 2016 is a leap year, so there is 2 * 365 days for standard years,
+      // 1 * 366 for the leap year, times 24 hours a day, 60 minutes an hour and
+      // 60 seconds a minute, + 1 for the cliff vesting event
+      expect(vestingEvents.length).to.equal(minutes + 1)
     })
 
     it('should not vest before cliff', () => {
@@ -135,4 +211,5 @@ describe('vestingGrants', () => {
       })).to.equal(true)
     })
   })
+  */
 })
