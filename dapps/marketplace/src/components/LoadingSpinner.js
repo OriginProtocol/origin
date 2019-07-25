c1decbdb6febf121fd2476aa305c4b5020b7161c
Ryan Trann <ryantrann@Ryans-Mac-mini.local>
2019-07-24 21:09:34 -0700

    cleaned up LoadSpinner component

--- dapps/marketplace/src/components/LoadingSpinner.js
+++ dapps/marketplace/src/components/LoadingSpinner.js
@@ -1,20 +1,5 @@
 import React from 'react'
 
-// const LoadingSpinner = () => <div className="loading-spinner" />
-
-// export default LoadingSpinner
-
-// require('react-styl')(`
-//   .loading-spinner
-//     background: url(images/spinner-animation-dark.svg) no-repeat center
-//     background-size: cover
-//     width: 3rem
-//     height: 3rem
-//     position: fixed
-//     top: calc(50% - 1.5rem)
-//     left: calc(50% - 1.5rem)
-// `)
-
 const LoadingSpinner = () => {
   return (
     <div className="loading-spinner-container">
