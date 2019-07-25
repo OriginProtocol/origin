b97e029c9e3d321bfe8865cf79e0aacf6e6ad142
Ryan Trann <ryantrann@Ryans-Mac-mini.local>
2019-07-24 21:01:07 -0700

    updated the LoadSpinner component to have a white bg and box shadow

--- dapps/marketplace/src/components/LoadingSpinner.js
+++ dapps/marketplace/src/components/LoadingSpinner.js
@@ -1,16 +1,50 @@
 import React from 'react'
 
-const LoadingSpinner = () => <div className="loading-spinner" />
+// const LoadingSpinner = () => <div className="loading-spinner" />
+
+// export default LoadingSpinner
+
+// require('react-styl')(`
+//   .loading-spinner 
+//     background: url(images/spinner-animation-dark.svg) no-repeat center
+//     background-size: cover
+//     width: 3rem
+//     height: 3rem
+//     position: fixed
+//     top: calc(50% - 1.5rem)
+//     left: calc(50% - 1.5rem)
+// `)
+
+
+
+const LoadingSpinner = () => {
+	return (
+		<div className="loading-spinner-container">
+			<div className="loading-spinner" />
+		</div>
+	)
+}
 
 export default LoadingSpinner
 
 require('react-styl')(`
-  .loading-spinner
-    background: url(images/spinner-animation-dark.svg) no-repeat center
+  .loading-spinner-container
+    background-color: white
     background-size: cover
+    width: 4.5em
+    height: 4.5em
     position: fixed
     top: calc(50% - 1.5rem)
     left: calc(50% - 1.5rem)
-    width: 3rem
-    height: 3rem
+    border-radius: 8px
+    box-shadow: 0px 8px 12px 0px lightGray
+
+  .loading-spinner
+    background: url(images/spinner-animation-dark.svg) no-repeat center
+    background-size: cover
+    width: 3em
+    height: 3em
+    position: relative
+    top: 0.75em
+    left: 0.75em
 `)
