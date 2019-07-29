13bf837576f15dea4688f81fbfa4117538d1c892
Shahul Hameed <shahthepro@outlook.com>
2019-07-29 18:14:02 +0530

    Change to functional components

--- dapps/marketplace/src/pages/growth/Promotions.js
+++ dapps/marketplace/src/pages/growth/Promotions.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { fbt } from 'fbt-runtime'
 import Link from 'components/Link'
 import { withRouter } from 'react-router-dom'
@@ -36,232 +36,269 @@ const actionTypeToNetwork = actionType => {
 const getActionHash = action =>
   web3.utils.sha3(`${action.type}/${action.content.post.text.default}`)
 
-class Promotions extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      runVerifyMutation: false
-    }
+const getActionFromHash = ({
+  notCompletedPromotionActions,
+  completedPromotionActions,
+  contentId
+}) => {
+  if (!contentId) {
+    return null
   }
 
-  getCurrentAction() {
-    const {
-      notCompletedPromotionActions,
-      completedPromotionActions
-    } = this.props
-    const contentId = get(this.props, 'match.params.contentId')
+  const selectedAction = [
+    ...notCompletedPromotionActions,
+    ...completedPromotionActions
+  ].find(action => getActionHash(action) === contentId)
 
-    const selectedAction = [
-      ...notCompletedPromotionActions,
-      ...completedPromotionActions
-    ].find(action => getActionHash(action) === contentId)
+  return selectedAction
+}
 
-    return selectedAction
-  }
-
-  render() {
-    const contentId = get(this.props, 'match.params.contentId')
-
-    return (
-      <>
-        {this.renderVerifyMutation()}
-        <div
-          className={`growth-promote-origin${
-            this.props.isMobile ? ' mobile' : ''
-          }`}
-        >
-          {this.renderHeader()}
-          {contentId ? this.renderChannels() : this.renderContents()}
-        </div>
-      </>
-    )
-  }
-
-  renderContents() {
-    return (
-      <>
-        {[
-          ...this.props.notCompletedPromotionActions,
-          ...this.props.completedPromotionActions
-        ].map((action, index) => (
+const PromotionContents = ({
+  notCompletedPromotionActions,
+  completedPromotionActions,
+  onShare
+}) => {
+  return (
+    <>
+      {[...notCompletedPromotionActions, ...completedPromotionActions].map(
+        (action, index) => (
           <ShareableContent
             key={index}
             onShare={() => {
-              // Note: Taking SHA3 hash just to push it to router
-              // This hash has nothing to do with Growth Event contentHash
-              const contentHash = getActionHash(action)
-              this.props.history.push(`/campaigns/promotions/${contentHash}`)
+              if (onShare) {
+                onShare(action)
+              }
             }}
             action={action}
           />
-        ))}
-      </>
-    )
+        )
+      )}
+    </>
+  )
+}
+
+const PromotionChannels = ({
+  isMobile,
+  decimalDivision,
+  locale,
+  action,
+  history,
+  onRunMutation
+}) => {
+  if (!action) {
+    return history.push(`/campaigns/promotions`)
   }
 
-  renderChannels() {
-    const { decimalDivision, isMobile, locale } = this.props
+  // TODO: As of now, there is only one growth rule per content AND social network.
+  // This has to be updated once support for multiple channels for the same content is available
 
-    const selectedAction = this.getCurrentAction()
-
-    if (!selectedAction) {
-      return this.props.history.push(`/campaigns/promotions`)
-    }
-
-    // TODO: As of now, there is only one growth rule per content AND social network.
-    // This has to be updated once support for multiple channels for the same content is available
-
-    return (
-      <>
-        {selectedAction.status === 'Completed' ? (
-          <ActionList
-            title={fbt('Completed', 'growth.promoteOrigin.completed')}
-            decimalDivision={decimalDivision}
-            isMobile={isMobile}
-            actions={[selectedAction]}
-          />
-        ) : (
-          <ActionList
-            decimalDivision={decimalDivision}
-            isMobile={isMobile}
-            actions={[selectedAction]}
-            locale={locale}
-            onActionClick={() => {
-              this.setState({
-                runVerifyMutation: true
-              })
-            }}
-          />
-        )}
-      </>
-    )
-  }
-
-  renderVerifyMutation() {
-    const { runVerifyMutation } = this.state
-
-    if (!runVerifyMutation) {
-      return null
-    }
-
-    const selectedAction = this.getCurrentAction()
-
-    const { locale } = this.props
-
-    return (
-      <Mutation
-        mutation={VerifyPromotionMutation}
-        onCompleted={({ verifyPromotion }) => {
-          if (verifyPromotion.success) {
-            if (this.props.showNotification) {
-              const message = getToastMessage(
-                selectedAction,
-                this.props.decimalDivision
-              )
-              this.props.showNotification(message, 'green')
+  return (
+    <>
+      {action.status === 'Completed' ? (
+        <ActionList
+          title={fbt('Completed', 'growth.promoteOrigin.completed')}
+          decimalDivision={decimalDivision}
+          isMobile={isMobile}
+          actions={[action]}
+        />
+      ) : (
+        <ActionList
+          decimalDivision={decimalDivision}
+          isMobile={isMobile}
+          actions={[action]}
+          locale={locale}
+          onActionClick={() => {
+            if (onRunMutation) {
+              onRunMutation(action)
             }
+          }}
+        />
+      )}
+    </>
+  )
+}
 
-            if (this.props.growthCampaignsRefetch) {
-              this.props.growthCampaignsRefetch()
-            }
-          }
-          this.setState({ runVerifyMutation: false })
-        }}
-        onError={errorData => {
-          console.error(`Failed to verify shared content`, errorData)
-          this.setState({ runVerifyMutation: false })
-        }}
-      >
-        {verifyPromotion => (
-          <AutoMutate
-            mutation={() => {
-              verifyPromotion({
-                variables: {
-                  type: 'SHARE',
-                  identity: this.props.wallet,
-                  identityProxy: this.props.walletProxy,
-                  socialNetwork: actionTypeToNetwork(selectedAction.type),
-                  content: getContentToShare(selectedAction, locale)
-                }
-              })
-            }}
-          />
-        )}
-      </Mutation>
-    )
-  }
+const RunVerifyPromotion = ({
+  action,
+  locale,
+  onCompleted,
+  onError,
+  wallet,
+  walletProxy
+}) => {
+  return (
+    <Mutation
+      mutation={VerifyPromotionMutation}
+      onCompleted={({ verifyPromotion }) => {
+        const complete = verifyPromotion.success
+        if (complete && onCompleted) {
+          onCompleted(action)
+        } else if (!complete && onError) {
+          console.error('Verification timed out: ', verifyPromotion.reason)
+          onError(verifyPromotion.reason)
+        }
+      }}
+      onError={errorData => {
+        console.error(`Failed to verify shared content`, errorData)
+        if (onError) {
+          onError(errorData)
+        }
+      }}
+    >
+      {verifyPromotion => (
+        <AutoMutate
+          mutation={() => {
+            verifyPromotion({
+              variables: {
+                type: 'SHARE',
+                identity: wallet,
+                identityProxy: walletProxy,
+                socialNetwork: actionTypeToNetwork(action.type),
+                content: getContentToShare(action, locale)
+              }
+            })
+          }}
+        />
+      )}
+    </Mutation>
+  )
+}
 
-  renderHeader() {
-    const { isMobile } = this.props
+const PromotionsHeader = ({ isMobile, hasSelectedContent, history }) => {
+  const stageTitle = (
+    <>
+      {!hasSelectedContent ? (
+        <fbt desc="GrowthPromotions.promoteOrigin">Promote Origin</fbt>
+      ) : null}
+      {hasSelectedContent ? (
+        <fbt desc="GrowthPromotions.selectChannels">Select Social Channels</fbt>
+      ) : null}
+    </>
+  )
 
-    const hasSelectedContent = get(this.props, 'match.params.contentId')
-      ? true
-      : false
+  const stageDesc = (
+    <>
+      {!hasSelectedContent ? (
+        <fbt desc="GrowthPromotions.earnBySharing">
+          Select an article or video about Origin and share it on your social
+          channels.
+        </fbt>
+      ) : null}
+      {hasSelectedContent ? (
+        <fbt desc="GrowthPromotions.selectChannelToShare">
+          Where would you like to share this? Select more channels for more
+          rewards!
+        </fbt>
+      ) : null}
+    </>
+  )
 
-    const stageTitle = (
-      <>
-        {!hasSelectedContent ? (
-          <fbt desc="GrowthPromotions.promoteOrigin">Promote Origin</fbt>
-        ) : null}
-        {hasSelectedContent ? (
-          <fbt desc="GrowthPromotions.selectChannels">
-            Select Social Channels
-          </fbt>
-        ) : null}
-      </>
-    )
-
-    const stageDesc = (
-      <>
-        {!hasSelectedContent ? (
-          <fbt desc="GrowthPromotions.earnBySharing">
-            Select an article or video about Origin and share it on your social
-            channels.
-          </fbt>
-        ) : null}
-        {hasSelectedContent ? (
-          <fbt desc="GrowthPromotions.selectChannelToShare">
-            Where would you like to share this? Select more channels for more
-            rewards!
-          </fbt>
-        ) : null}
-      </>
-    )
-
-    const header = isMobile ? (
-      <MobileModalHeader
-        showBackButton={true}
-        className="px-0"
-        onBack={() => {
-          this.props.history.goBack()
-        }}
-      >
-        {stageTitle}
-      </MobileModalHeader>
-    ) : (
-      <>
-        <Link className="back d-flex mr-auto" to="/campaigns">
-          <img src="images/caret-blue.svg" />
-          <div>
-            <fbt desc="GrowthPromotions.backToCampaign">Back to Campaign</fbt>
-          </div>
-        </Link>
-        <h1 className={`mb-2 pt-md-3 mt-3`}>{stageTitle}</h1>
-      </>
-    )
-
-    return (
-      <div>
-        {header}
-        <div
-          className={`promote-origin-subtitle${isMobile ? ' text-center' : ''}`}
-        >
-          {stageDesc}
+  const header = isMobile ? (
+    <MobileModalHeader
+      showBackButton={true}
+      className="px-0"
+      onBack={() => {
+        history.goBack()
+      }}
+    >
+      {stageTitle}
+    </MobileModalHeader>
+  ) : (
+    <>
+      <Link className="back d-flex mr-auto" to="/campaigns">
+        <img src="images/caret-blue.svg" />
+        <div>
+          <fbt desc="GrowthPromotions.backToCampaign">Back to Campaign</fbt>
         </div>
+      </Link>
+      <h1 className={`mb-2 pt-md-3 mt-3`}>{stageTitle}</h1>
+    </>
+  )
+
+  return (
+    <div>
+      {header}
+      <div
+        className={`promote-origin-subtitle${isMobile ? ' text-center' : ''}`}
+      >
+        {stageDesc}
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+const Promotions = ({
+  notCompletedPromotionActions,
+  completedPromotionActions,
+  locale,
+  history,
+  isMobile,
+  decimalDivision,
+  wallet,
+  walletProxy,
+  growthCampaignsRefetch,
+  showNotification,
+  ...props
+}) => {
+  const [runMutation, setRunMutation] = useState(false)
+
+  const contentId = get(props, 'match.params.contentId')
+  const hasSelectedContent = contentId ? true : false
+  const action = getActionFromHash({
+    notCompletedPromotionActions,
+    completedPromotionActions,
+    contentId
+  })
+
+  return (
+    <>
+      {runMutation && (
+        <RunVerifyPromotion
+          locale={locale}
+          action={action}
+          onCompleted={() => {
+            if (showNotification) {
+              const message = getToastMessage(action, decimalDivision)
+              showNotification(message, 'green')
+            }
+            if (growthCampaignsRefetch) {
+              growthCampaignsRefetch()
+            }
+            setRunMutation(false)
+          }}
+          onError={() => setRunMutation(false)}
+          wallet={wallet}
+          walletProxy={walletProxy}
+        />
+      )}
+      <PromotionsHeader
+        isMobile={isMobile}
+        hasSelectedContent={hasSelectedContent}
+        history={history}
+      />
+      {hasSelectedContent ? (
+        <PromotionChannels
+          isMobile={isMobile}
+          decimalDivision={decimalDivision}
+          locale={locale}
+          action={action}
+          history={history}
+          onRunMutation={() => setRunMutation(true)}
+        />
+      ) : (
+        <PromotionContents
+          completedPromotionActions={completedPromotionActions}
+          notCompletedPromotionActions={notCompletedPromotionActions}
+          onShare={action => {
+            // Note: Taking SHA3 hash just to push it to router
+            // This hash has nothing to do with Growth Event contentHash
+            const contentHash = getActionHash(action)
+            history.push(`/campaigns/promotions/${contentHash}`)
+          }}
+        />
+      )}
+    </>
+  )
 }
 
 export default withWallet(withRouter(Promotions))
