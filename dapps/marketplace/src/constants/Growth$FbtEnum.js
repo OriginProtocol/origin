c46b58ec7e91fed625f155ac1f5b47ee00145712
Franck <franck@originprotocol.com>
2019-07-30 21:56:11 -0700

    [SocialSharing] Rules for staging testing (#2807)
    * August rules for testing on staging
    
    * prettier
    
    * prettier again

--- dapps/marketplace/src/constants/Growth$FbtEnum.js
+++ dapps/marketplace/src/constants/Growth$FbtEnum.js
@@ -148,6 +148,14 @@ const Growth$FbtEnum = {
   'growth.purchase.listing-1-000-2999.title':
     'Piano lessons (All Ages & Levels)',
 
+  'growth.twitterShare.content1.title': 'Origin Launches a Fresh New App',
+  'growth.twitterShare.content1.details':
+    'A complete redesign of our mobile app delivers a new and improved experience to our global users',
+  'growth.twitterShare.content1.link': 'Read the article',
+  'growth.twitterShare.content2.title': 'New Ways to Earn OGN in August',
+  'growth.twitterShare.content2.details':
+    'Join Origin Rewards and become a token holder alongside thousands of users all around the world',
+  'growth.twitterShare.content2.link': 'Read the article',
   'growth.purchase.empty.details': ''
 }
 
