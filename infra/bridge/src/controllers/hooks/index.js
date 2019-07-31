207832c6effb57272f5cab1fcf57ca1818ad0810
Shahul Hameed <shahthepro@outlook.com>
2019-08-01 01:29:40 +0530

    Prettify

--- infra/bridge/src/controllers/hooks/index.js
+++ infra/bridge/src/controllers/hooks/index.js
@@ -158,7 +158,12 @@ router.post('/twitter', (req, res) => {
         const key = `twitter/follow/${event.source.id}`
         redisBatch.set(key, JSON.stringify(event), 'EX', 60 * 30)
         // TODO: This is just for the demo
-        redisBatch.set(`twitter/follow/${event.source.screen_name}`, JSON.stringify(event), 'EX', 60 * 30)
+        redisBatch.set(
+          `twitter/follow/${event.source.screen_name}`,
+          JSON.stringify(event),
+          'EX',
+          60 * 30
+        )
 
         logger.info(
           `Pushing twitter follow event for ${event.source.screen_name} at ${key}...`
@@ -186,7 +191,12 @@ router.post('/twitter', (req, res) => {
         const key = `twitter/share/${event.user.id}`
         redisBatch.set(key, JSON.stringify(event), 'EX', 60 * 30)
         // TODO: This is just for the demo
-        redisBatch.set(`twitter/share/${event.user.screen_name}`, JSON.stringify(event), 'EX', 60 * 30)
+        redisBatch.set(
+          `twitter/share/${event.user.screen_name}`,
+          JSON.stringify(event),
+          'EX',
+          60 * 30
+        )
         logger.info(
           `Pushing twitter mention event for ${event.user.screen_name} at ${key}...`
         )
