3fd086a0a9e7b6344842c672f824bb13e7758516
Shahul Hameed <shahthepro@outlook.com>
2019-08-05 14:38:44 +0530

    Split with GrowthCampaign HOC

---
+++ dapps/marketplace/src/hoc/withEnrolmentStatus.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Query } from 'react-apollo'
+import QueryError from 'components/QueryError'
+import get from 'lodash/get'
+
+import enrollmentStatusQuery from 'queries/EnrollmentStatus'
+import withWallet from './withWallet'
+
+function withEnrolmentStatus(
+  WrappedComponent,
+  { fetchPolicy = 'network-only', suppressErrors } = {}
+) {
+  const WithEnrolmentStatus = props => {
+    return (
+      <Query
+        query={enrollmentStatusQuery}
+        variables={{ walletAddress: props.wallet }}
+        skip={!props.wallet}
+        fetchPolicy={fetchPolicy}
+      >
+        {({ data, error, loading, networkStatus }) => {
+          if (error && !suppressErrors) {
+            return <QueryError error={error} query={enrollmentStatusQuery} />
+          }
+
+          const walletLoading = !networkStatus || loading || networkStatus === 1
+
+          const enrollmentStatus = get(data, 'enrollmentStatus')
+
+          return (
+            <WrappedComponent
+              {...props}
+              growthEnrollmentStatus={enrollmentStatus}
+              growthEnrollmentStatusLoading={walletLoading}
+            />
+          )
+        }}
+      </Query>
+    )
+  }
+  return withWallet(WithEnrolmentStatus)
+}
+
+export default withEnrolmentStatus
