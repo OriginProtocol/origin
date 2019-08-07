1f1150d3468c3d54e891cdbf313f780582d4270f
Ryan Trann <ryanjtrann@gmail.com>
2019-08-07 16:29:26 -0700

    fixed bridge exchange-rate test failures

--- infra/bridge/test/exchange-rate.js
+++ infra/bridge/test/exchange-rate.js
@@ -30,16 +30,17 @@ describe('exchange rate poller', () => {
       .get('/api/v3/exchange_rates')
       .reply(200, {
         rates: {
-          btc: { name: 'Bitcoin', unit: 'BTC', value: 1.0, type: 'crypto' }
+          btc: { name: 'Bitcoin', unit: 'BTC', value: 1.0, type: 'crypto' },
+          usd: { name: 'US Dollar', unit: '$', value: 11874.946, type: 'fiat' }
         }
       })
 
     const response = await request(app)
-      .get('/utils/exchange-rate?market=YYY-USD')
+      .get('/utils/exchange-rate?market=BTC-USD')
       .expect(200)
 
     expect(response.status).to.equal(200)
-    expect(response.body.rates.btc.value).to.equal(1.0)
+    expect(response.body.price).to.equal('11874.946')
   })
 
   it('should return default exchange rate if not cached and API is down', async () => {
