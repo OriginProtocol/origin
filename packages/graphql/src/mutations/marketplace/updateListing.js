b23efa20f4d4a55008740d5447676d76677fe189
Nick Poulden <nick@poulden.com>
2019-08-12 15:46:24 -0600

    Promote listings (#2882)
    * Promote listing
    
    * Promote
    
    * Promote
    
    * Promote
    
    * Promote listing
    
    * Promotion fixes
    
    * Lint
    
    * Lint
    
    * Single / Multi unit listing promote UI
    
    * Listing refetch
    
    * Allow OGN when using proxy
    
    * Tests
    
    * Promote puppeteer tests
    
    * Update deployed image tag for origin-dapp [ci skip]
    
    * Translations. Promote toggle on settings page.
    
    * Translate
    
    * Multi unit promote tests

--- packages/graphql/src/mutations/marketplace/updateListing.js
+++ packages/graphql/src/mutations/marketplace/updateListing.js
@@ -1,9 +1,12 @@
 import { post } from '@origin/ipfs'
+import IdentityProxy from '@origin/contracts/build/contracts/IdentityProxy_solc'
 import txHelper, { checkMetaMask } from '../_txHelper'
 import contracts from '../../contracts'
 import cost from '../_gasCost'
 import parseId from '../../utils/parseId'
 import { listingInputToIPFS } from './createListing'
+
+import { proxyOwner } from '../../utils/proxy'
 
 async function updateListing(_, args) {
   const { data, unitData, fractionalData, autoApprove } = args
@@ -15,6 +18,9 @@ async function updateListing(_, args) {
   const ipfsHash = await post(contracts.ipfsRPC, ipfsData)
 
   let tx
+  let gas = cost.updateListing
+  let mutation = 'updateListing'
+
   const additionalDeposit = contracts.web3.utils.toWei(
     args.additionalDeposit,
     'ether'
@@ -30,19 +36,43 @@ async function updateListing(_, args) {
   }
 
   if (autoApprove && additionalDeposit > 0) {
-    const fnSig = contracts.web3.eth.abi.encodeFunctionSignature(
-      'updateListingWithSender(address,uint256,bytes32,uint256)'
-    )
-    const params = contracts.web3.eth.abi.encodeParameters(
-      ['uint256', 'bytes32', 'uint256'],
-      [listingId, ipfsHash, additionalDeposit]
-    )
-    tx = contracts.ognExec.methods.approveAndCallWithSender(
-      contracts.marketplace._address,
-      additionalDeposit,
-      fnSig,
-      params
-    )
+    let owner = from,
+      isProxy
+    if (contracts.config.proxyAccountsEnabled) {
+      owner = await proxyOwner(from)
+      isProxy = owner ? true : false
+    }
+    if (isProxy) {
+      const Proxy = new contracts.web3Exec.eth.Contract(IdentityProxy.abi, from)
+      const txData = await contracts.marketplaceExec.methods
+        .updateListing(listingId, ipfsHash, additionalDeposit)
+        .encodeABI()
+
+      tx = Proxy.methods.transferTokenMarketplaceExecute(
+        owner,
+        contracts.marketplace._address,
+        txData,
+        contracts.ognExec._address,
+        additionalDeposit
+      )
+      mutation = 'transferTokenMarketplaceExecute'
+      gas += 1500000
+    } else {
+      const fnSig = contracts.web3.eth.abi.encodeFunctionSignature(
+        'updateListingWithSender(address,uint256,bytes32,uint256)'
+      )
+      const params = contracts.web3.eth.abi.encodeParameters(
+        ['uint256', 'bytes32', 'uint256'],
+        [listingId, ipfsHash, additionalDeposit]
+      )
+      tx = contracts.ognExec.methods.approveAndCallWithSender(
+        contracts.marketplace._address,
+        additionalDeposit,
+        fnSig,
+        params
+      )
+      gas += 100000
+    }
   } else {
     tx = contracts.marketplaceExec.methods.updateListing(
       listingId,
@@ -54,8 +84,10 @@ async function updateListing(_, args) {
   return txHelper({
     tx,
     from,
-    mutation: 'updateListing',
-    gas: cost.updateListing
+    mutation,
+    gas,
+    onReceipt: () => context.eventSource.resetMemos(),
+    onConfirmation: () => context.eventSource.resetMemos()
   })
 }
 
