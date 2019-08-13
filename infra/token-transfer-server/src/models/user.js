14f6b0acbd1357840f1db3a82b1e2ae69f5b3798
Tom Linton <tom@originprotocol.com>
2019-08-13 22:15:48 +1200

    Unique email

--- infra/token-transfer-server/src/models/user.js
+++ infra/token-transfer-server/src/models/user.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
       id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
       phone: DataTypes.STRING,
       // Email address of the user.
-      email: DataTypes.STRING,
+      email: { type: DataTypes.STRING, unique: true },
       // OTP secret key, stored encrypted.
       otpKey: DataTypes.STRING,
       otpVerified: DataTypes.BOOLEAN,
