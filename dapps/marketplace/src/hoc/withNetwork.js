6ef4118e6e3bf2e39c36032a48a5a6017fa90165
Nick Poulden <nick@poulden.com>
2019-08-13 16:14:23 -0600

    Move hoc components to Apollo hooks API

--- dapps/marketplace/src/hoc/withNetwork.js
+++ dapps/marketplace/src/hoc/withNetwork.js
@@ -1,23 +1,18 @@
 import React from 'react'
-import { Query } from 'react-apollo'
+import { useQuery } from '@apollo/react-hooks'
 import get from 'lodash/get'
 
 import query from 'queries/Network'
 
 function withNetwork(WrappedComponent) {
   const WithNetwork = props => {
+    const { data } = useQuery(query)
     return (
-      <Query query={query}>
-        {({ data }) => {
-          return (
-            <WrappedComponent
-              networkId={get(data, 'web3.networkId')}
-              networkName={get(data, 'web3.networkName')}
-              {...props}
-            />
-          )
-        }}
-      </Query>
+      <WrappedComponent
+        networkId={get(data, 'web3.networkId')}
+        networkName={get(data, 'web3.networkName')}
+        {...props}
+      />
     )
   }
   return WithNetwork
