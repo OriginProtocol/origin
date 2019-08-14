90dd10b14e73a73f1b2025b7a1b8ca6437249c68
Shahul Hameed <shahthepro@outlook.com>
2019-08-14 17:35:02 +0530

    Add tests

--- infra/bridge/test/telegram.js
+++ infra/bridge/test/telegram.js
@@ -4,15 +4,22 @@ const chai = require('chai')
 const expect = chai.expect
 const request = require('supertest')
 
+const Web3 = require('web3')
+const redis = require('redis')
+
 const Attestation = require('../src/models/index').Attestation
 const AttestationTypes = Attestation.AttestationTypes
 const app = require('../src/app')
 
 const ethAddress = '0x112234455c3a32fd11230c42e7bccd4a84e02010'
+const client = redis.createClient()
 
 describe('telegram attestation', () => {
   beforeEach(async () => {
     process.env.TELEGRAM_BOT_TOKEN = '12345.6789'
+
+    // Clear out redis-mock
+    await new Promise(resolve => client.flushall(resolve))
 
     await Attestation.destroy({
       where: {},
@@ -20,13 +27,38 @@ describe('telegram attestation', () => {
     })
   })
 
+  it('should generrate a code and a seed', async () => {
+    const response = await request(app)
+      .get('/api/attestations/telegram/generate-code?identity=' + ethAddress)
+      .expect(200)
+
+    expect(response.body).not.undefined.and.not.null
+
+    const seed = await new Promise(resolve => client.get(`telegram/attestation/seed/${ethAddress}`, (err, data) => resolve(data)))
+
+    expect(seed).not.undefined.and.not.null
+  })
+
   it('should generate attestation on valid data', async () => {
+    const code = 'weddingcatalogtopplecatalogabovefebruary'
+    const eventKey = Web3.utils.sha3(code)
+
+    await new Promise(resolve => client.set(`telegram/attestation/seed/${ethAddress}`, '123456', 'EX', 60, () => resolve()))
+    await new Promise(resolve => client.set(`telegram/attestation/event/${eventKey}`, JSON.stringify({
+      message: {
+        from: {
+          id: '12345'
+        }
+      },
+      payload: 'weddingcatalogtopplecatalogabovefebruary'
+    }), 'EX', 60, () => resolve()))
+
     const response = await request(app)
       .post('/api/attestations/telegram/verify')
       .send({
         identity: ethAddress,
-        hash:
-          '7cc77e64216db0c62395f57e88e4153edfb801a2cd9279c1126a2a5a8d3b588c',
+        code:
+          'weddingcatalogtopplecatalogabovefebruary',
         id: '12345'
       })
       .expect(200)
@@ -58,18 +90,43 @@ describe('telegram attestation', () => {
     expect(results[0].profileUrl).to.null
   })
 
-  it('should fail on invalid hash', async () => {
+  it('should fail on invalid code', async () => {
+    const code = 'weddingcatalogtopplecatalogabovefebruary'
+    const eventKey = Web3.utils.sha3(code)
+
+    await new Promise(resolve => client.set(`telegram/attestation/seed/${ethAddress}`, '123456', 'EX', 60, () => resolve()))
+    await new Promise(resolve => client.set(`telegram/attestation/event/${eventKey}`, JSON.stringify({
+      message: {
+        from: {
+          id: '12345'
+        }
+      },
+      payload: 'weddingcatalogtopplecatalogabovefebruary234'
+    }), 'EX', 60, () => resolve()))
+
     const response = await request(app)
       .post('/api/attestations/telegram/verify')
       .send({
         identity: ethAddress,
-        hash:
-          '7cc77e64216db0c62395f57e88e4153edfb801a2cd9279c1126a2a5a8d3b588d',
+        code,
         id: '12345'
       })
-      .expect(400)
+      .expect(500)
 
-    expect(response.body.errors.length).to.equal(1)
-    expect(response.body.errors[0]).to.equal('Failed to create an attestation')
+    expect(response.body.errors[0]).to.equal(`Code verification failed`)
+  })
+
+  it('should fail on missing seed and event', async () => {
+    const response = await request(app)
+      .post('/api/attestations/telegram/verify')
+      .send({
+        identity: ethAddress,
+        code:
+          'weddingcatalogtopplecatalogabovefebruary',
+        id: '12345'
+      })
+      .expect(500)
+
+    expect(response.body.errors[0]).to.equal(`You haven\'t interacted with the verification bot yet.`)
   })
 })
