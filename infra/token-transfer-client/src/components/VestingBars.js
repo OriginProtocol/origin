861ca494489020ebb74473b3a9fce616a4157ca6
Tom Linton <tom@originprotocol.com>
2019-08-16 13:13:09 +1200

    Tooltips for grants

--- infra/token-transfer-client/src/components/VestingBars.js
+++ infra/token-transfer-client/src/components/VestingBars.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment'
 
 const VestingBars = props => {
+  const [displayPopover, setDisplayPopover] = useState({})
+
   if (!props.grants || props.grants.length === 0) {
     return null
   }
@@ -33,7 +35,12 @@ const VestingBars = props => {
 
   // Extract x points (months) across the duration to display between first
   // start date and last end date to display
-  const maxPoints = 9
+  let maxPoints
+  if (window.innerWidth < 1000) {
+    maxPoints = 5
+  } else {
+    maxPoints = 9
+  }
   const displayMonths = intermediateMonths.filter((e, i, arr) => {
     return (
       i !== 0 &&
@@ -42,10 +49,23 @@ const VestingBars = props => {
     )
   })
 
+  const handleTogglePopover = (event, grantId) => {
+    // Calculate a left offset to make the popover display at the point of the
+    // mouse click. This is calculating the offset of 3 parent relative elements.
+    const leftOffset =
+      document.getElementById('main').offsetLeft +
+      document.getElementById('vestingBars').offsetParent.offsetLeft +
+      document.getElementById('vestingBars').offsetLeft
+    setDisplayPopover({
+      ...displayPopover,
+      [grantId]: displayPopover[grantId] ? false : event.clientX - leftOffset
+    })
+  }
+
   return (
     <div className="vesting-bars-wrapper">
       <h2>Vesting Progress</h2>
-      <div style={{ position: 'relative' }}>
+      <div id="vestingBars" style={{ position: 'relative' }}>
         {grants.map(grant => {
           // Calculate the percentage of the grant that is complete with a
           // upper bound of 100
@@ -64,27 +84,57 @@ const VestingBars = props => {
               className="progress"
               key={grant.id}
               style={{ width: `${width}%`, marginLeft: `${left}%` }}
+              onClick={event => handleTogglePopover(event, grant.id)}
             >
               <div
                 className="progress-bar bg-success"
                 role="progressbar"
                 style={{ width: `${complete}%` }}
               />
+              {displayPopover[grant.id] && (
+                <div
+                  className="popover"
+                  style={{ left: `${displayPopover[grant.id]}px`, top: '10px' }}
+                >
+                  <div
+                    className="cover"
+                    onClick={event => handleTogglePopover(event, grant.id)}
+                  />
+                  <div>
+                    <strong>Start</strong> {grant.start.format('YYYY-MM-DD')}
+                  </div>
+                  <div>
+                    <strong>Cliff</strong> {grant.cliff.format('YYYY-MM-DD')}
+                  </div>
+                  <div>
+                    <strong>End</strong> {grant.end.format('YYYY-MM-DD')}
+                  </div>
+                  <div>
+                    <strong>Grant</strong> {grant.amount}
+                  </div>
+                </div>
+              )}
             </div>
           )
         })}
+
         {displayMonths.map(month => {
           const left = ((moment(month) - firstStartDate) / totalDuration) * 100
           const style = {
             position: 'absolute',
             left: `${left}%`,
             top: 0,
-            height: '150%'
+            height: '150%',
+            pointerEvents: 'none' // Stop absolute positioning from stealing clicks
           }
           return (
             <div key={month} style={style}>
               <div
-                style={{ borderLeft: '1px solid #dbe6eb', height: '100%' }}
+                style={{
+                  borderLeft: '1px solid #dbe6eb',
+                  height: '100%',
+                  width: 0
+                }}
               ></div>
               <div style={{ marginLeft: '-50%' }}>
                 <small>{moment(month).format('MMM YYYY')}</small>
@@ -100,8 +150,29 @@ const VestingBars = props => {
 export default VestingBars
 
 require('react-styl')(`
-  .progress
-    margin-top: 20px
   .vesting-bars-wrapper
     margin-bottom: 150px
+
+  .progress
+    margin-top: 20px
+    cursor: pointer
+
+  .cover
+    position: fixed
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+
+  .popover
+    position: absolute
+    z-index: 2
+    padding: 20px
+    border-radius: 5px;
+    box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.29);
+    div
+      margin-bottom: 2px
+    strong
+      width: 50px
+      display: inline-block
 `)
