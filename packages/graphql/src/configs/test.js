9fe0eb255522c7bc14b1934f1603eaf984de9e61
Mike Shultz <shultzm@gmail.com>
2019-08-16 09:08:34 -0600

    fixed test graphql port

--- packages/graphql/src/configs/test.js
+++ packages/graphql/src/configs/test.js
@@ -15,7 +15,7 @@ const config = {
   ipfsRPC: `http://${HOST}:5002`,
   bridge: 'https://bridge.dev.originprotocol.com',
   performanceMode: localStorageHas('performanceMode', 'true'),
-  graphql: `http://${HOST}:4007`,
+  graphql: `http://${HOST}:4002`,
   relayer: `http://${HOST}:5100`,
   automine: 500,
   attestationIssuer: '0x5be37555816d258f5e316e0f84D59335DB2400B2',
