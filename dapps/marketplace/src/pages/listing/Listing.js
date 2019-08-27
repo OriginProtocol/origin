445e0db9c78b4350da01574c4f5a2c7e8f629a59
Nick Poulden <nick@poulden.com>
2019-08-27 09:24:12 -0600

    Invalid listing (#3095)

--- dapps/marketplace/src/pages/listing/Listing.js
+++ dapps/marketplace/src/pages/listing/Listing.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Query } from 'react-apollo'
+import React, { useState } from 'react'
+import { useQuery } from '@apollo/react-hooks'
 import { Switch, Route } from 'react-router-dom'
 import { fbt } from 'fbt-runtime'
 
@@ -22,86 +22,84 @@ const error404 = (
   </Error404>
 )
 
-class Listing extends Component {
-  state = { quantity: '1' }
+const Listing = props => {
+  const [quantity, setQuantity] = useState('1')
+  const [redirect, setRedirect] = useState()
 
-  render() {
-    const listingId = this.props.match.params.listingID
-    const vars = { listingId }
+  const listingId = props.match.params.listingID
+  const variables = { listingId }
 
-    if (this.state.redirect) {
-      return <Redirect to={this.state.redirect} push />
+  const { networkStatus, error, data, refetch } = useQuery(query, {
+    variables,
+    errorPolicy: 'all'
+  })
+
+  if (redirect) {
+    return <Redirect to={redirect} push />
+  }
+
+  if (networkStatus <= 2) {
+    return <LoadingSpinner />
+  } else if (error) {
+    if (String(error).match(/no such listing/i)) {
+      return error404
     }
+    return <QueryError error={error} query={query} vars={variables} />
+  } else if (!data || !data.marketplace) {
+    return <div className="container">No marketplace contract?</div>
+  }
 
+  const listing = data.marketplace.listing
+  if (!listing) {
+    return error404
+  } else if (!listing.valid) {
     return (
-      <>
-        <DocumentTitle
-          pageTitle={
-            <fbt desc="listing.title">
-              Listing <fbt:param name="id">{listingId}</fbt:param>
-            </fbt>
-          }
-        />
-        <Query query={query} variables={vars} errorPolicy="all">
-          {({ networkStatus, error, data, refetch }) => {
-            if (networkStatus <= 2) {
-              return <LoadingSpinner />
-            } else if (error) {
-              if (String(error).match(/no such listing/i)) {
-                return error404
-              }
-              return <QueryError error={error} query={query} vars={vars} />
-            } else if (!data || !data.marketplace) {
-              return <div>No marketplace contract?</div>
-            }
-
-            const listing = data.marketplace.listing
-            if (!listing) {
-              return error404
-            } else if (!listing.valid) {
-              return (
-                <div>
-                  <fbt desc="listing.listing-invalid">Listing invalid</fbt>
-                </div>
-              )
-            }
-
-            const wrappedRefetch = async redirect => {
-              await refetch()
-              this.setState({ redirect })
-            }
-
-            return (
-              <Switch>
-                <Route
-                  path="/listing/:listingID/onboard"
-                  render={() => (
-                    <Onboard listing={listing} quantity={this.state.quantity} />
-                  )}
-                />
-                <Route
-                  path="/listing/:listingID/edit"
-                  render={() => (
-                    <EditListing listing={listing} refetch={wrappedRefetch} />
-                  )}
-                />
-                <Route
-                  render={() => (
-                    <ListingDetail
-                      listing={listing}
-                      refetch={wrappedRefetch}
-                      quantity={this.state.quantity}
-                      updateQuantity={quantity => this.setState({ quantity })}
-                    />
-                  )}
-                />
-              </Switch>
-            )
-          }}
-        </Query>
-      </>
+      <Error404>
+        <h1 className="d-md-block">
+          <fbt desc="listing.listingInvalid">Listing Invalid</fbt>
+        </h1>
+      </Error404>
     )
   }
+
+  const wrappedRefetch = async redirect => {
+    await refetch()
+    setRedirect(redirect)
+  }
+
+  return (
+    <>
+      <DocumentTitle
+        pageTitle={
+          <fbt desc="listing.title">
+            Listing <fbt:param name="id">{listingId}</fbt:param>
+          </fbt>
+        }
+      />
+      <Switch>
+        <Route
+          path="/listing/:listingID/onboard"
+          render={() => <Onboard listing={listing} quantity={quantity} />}
+        />
+        <Route
+          path="/listing/:listingID/edit"
+          render={() => (
+            <EditListing listing={listing} refetch={wrappedRefetch} />
+          )}
+        />
+        <Route
+          render={() => (
+            <ListingDetail
+              listing={listing}
+              refetch={wrappedRefetch}
+              quantity={quantity}
+              updateQuantity={quantity => setQuantity(quantity)}
+            />
+          )}
+        />
+      </Switch>
+    </>
+  )
 }
 
 export default Listing
