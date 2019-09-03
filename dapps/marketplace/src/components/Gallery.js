ee87b700a45eb68c1b8b254e64032dd5d2eb5be9
Nick Poulden <nick@poulden.com>
2019-09-03 10:28:16 -0600

    Gallery improvements

--- dapps/marketplace/src/components/Gallery.js
+++ dapps/marketplace/src/components/Gallery.js
@@ -1,20 +1,92 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-const Gallery = ({ pics = [] }) => {
-  const [active, setActive] = useState(0)
-  const [zoom, setZoom] = useState(false)
+const Zoom = ({ pics = [], onClose, initialActive }) => {
+  const [active, setActive] = useState(initialActive)
+  const current = pics[active]
+
+  // Prevent body scrolling when lightbox is open
   useEffect(() => {
-    if (zoom) {
-      document.body.className += ' pl-modal-open'
-    }
-    // Specify how to clean up after this effect:
-    return function cleanup() {
+    document.body.className += ' pl-modal-open'
+    return () => {
       document.body.className = document.body.className.replace(
         ' pl-modal-open',
         ''
       )
     }
-  }, [zoom])
+  }, [])
+
+  useEffect(() => {
+    function onKeyDown(e) {
+      if (e.keyCode === 39) {
+        // Right arrow
+        setActive(active === pics.length - 1 ? 0 : active + 1)
+      } else if (e.keyCode === 37) {
+        // Left arrow
+        setActive(active === 0 ? pics.length - 1 : active - 1)
+      } else if (e.keyCode === 27) {
+        // Esc
+        onClose(active)
+      }
+    }
+    window.addEventListener('keydown', onKeyDown)
+    return () => window.removeEventListener('keydown', onKeyDown)
+  }, [active])
+
+  return (
+    <div className="zoom" onClick={() => onClose()}>
+      {pics.length > 1 && (
+        <a
+          href="#prev"
+          className="prev"
+          onClick={e => {
+            e.stopPropagation()
+            e.preventDefault()
+            setActive(active === 0 ? pics.length - 1 : active - 1)
+          }}
+        />
+      )}
+      <div className="zoom-pic">
+        <img src={current.urlExpanded} />
+      </div>
+      {pics.length > 1 && (
+        <a
+          href="#next"
+          className="next"
+          onClick={e => {
+            e.stopPropagation()
+            e.preventDefault()
+            setActive(active === pics.length - 1 ? 0 : active + 1)
+          }}
+        />
+      )}
+      <a
+        href="#close"
+        className="close"
+        onClick={e => {
+          e.stopPropagation()
+          e.preventDefault()
+          onClose(active)
+        }}
+      />
+    </div>
+  )
+}
+
+const Gallery = ({ pics = [] }) => {
+  const [active, setActive] = useState(0)
+  const [zoom, setZoom] = useState(false)
+  const thumbnails = useRef()
+  useEffect(() => {
+    const img = thumbnails.current.querySelector(`img:nth-child(${active + 1})`)
+    thumbnails.current.scrollTo({
+      left:
+        img.offsetLeft -
+        thumbnails.current.clientWidth / 2 +
+        img.clientWidth / 2,
+      behavior: 'smooth'
+    })
+  }, [thumbnails, active])
+
   const current = pics[active]
   if (!current) return null
 
@@ -23,56 +95,51 @@ const Gallery = ({ pics = [] }) => {
   return (
     <div className="gallery">
       {zoom && (
-        <div className="zoom" onClick={() => setZoom(false)}>
-          {pics.length > 1 && (
-            <a
-              href="#prev"
-              className="prev"
-              onClick={e => {
-                e.stopPropagation()
-                e.preventDefault()
-                setActive(active === 0 ? pics.length - 1 : active - 1)
-              }}
-            />
-          )}
-          <div className="zoom-pic">
-            <img src={current.urlExpanded} />
-          </div>
-          {pics.length > 1 && (
-            <a
-              href="#next"
-              className="next"
-              onClick={e => {
-                e.stopPropagation()
-                e.preventDefault()
-                setActive(active === pics.length - 1 ? 0 : active + 1)
-              }}
-            />
-          )}
-          <a
-            href="#close"
-            className="close"
-            onClick={e => {
-              e.stopPropagation()
-              e.preventDefault()
-              setZoom(false)
-            }}
-          />
-        </div>
+        <Zoom
+          pics={pics}
+          initialActive={active}
+          onClose={lastActive => {
+            setZoom(false)
+            setActive(lastActive)
+          }}
+        />
       )}
       <div
         className="main-pic"
         style={{ backgroundImage }}
         onClick={() => setZoom(true)}
-      />
+      >
+        {pics.length > 1 && (
+          <a
+            href="#prev"
+            className="prev"
+            onClick={e => {
+              e.stopPropagation()
+              e.preventDefault()
+              setActive(active === 0 ? pics.length - 1 : active - 1)
+            }}
+          />
+        )}
+        {pics.length > 1 && (
+          <a
+            href="#next"
+            className="next"
+            onClick={e => {
+              e.stopPropagation()
+              e.preventDefault()
+              setActive(active === pics.length - 1 ? 0 : active + 1)
+            }}
+          />
+        )}
+      </div>
       {pics.length === 1 ? null : (
-        <div className="thumbnails">
+        <div className="thumbnails" ref={thumbnails}>
           <div className="inner">
             {pics.map((m, idx) => (
-              <div
+              <img
                 key={idx}
+                src={m.urlExpanded}
                 onClick={() => setActive(idx)}
-                style={{ backgroundImage: `url(${m.urlExpanded})` }}
                 className={active === idx ? 'active' : ''}
               />
             ))}
@@ -137,30 +204,48 @@ require('react-styl')(`
       flex: 1
       height: 100%
       cursor: pointer
+      position: relative
+      .prev,.next
+        display: block
+        position: absolute
+        top: 50%
+        left: 0
+        transform: translateY(-50%)
+        width: 80px
+        height: 80px
+        background: url(images/left-arrow-large.svg) no-repeat center
+        background-size: 30px
+        opacity: 0.8
+        &:hover
+          opacity: 1
+        &.next
+          background-image: url(images/right-arrow-large.svg)
+          left: auto
+          right: 0
     .thumbnails
-      height: 80px
+      height: 60px
+      overflow-x: scroll
+      overflow-y: hidden
+      display: flex
+      justify-content: center
       .inner
         height: 100%
-        overflow-x: scroll
-        overflow-y: hidden
-        white-space: nowrap
-        > div
-          width: 100px
+        display: flex
+        min-width: 0
+        > img
           height: 100%
           border-radius: 5px
-          display: inline-block
-          background-position: center
-          background-repeat: no-repeat
-          background-size: cover
-          background-origin: content-box
           cursor: pointer
-          opacity: 0.75
+          opacity: 0.6
+          transition: opacity 0.1s
           &:not(:last-child)
             margin-right: 0.75rem
           &:hover
-            opacity: 1
+            opacity: 0.8
+            transition: opacity 0.1s
           &.active
             opacity: 1
+            transition: opacity 0.1s
 
   @media (max-width: 767.98px)
     .gallery
