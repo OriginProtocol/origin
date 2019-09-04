701896823a02e43ef9040a8549f35ec6e20b7c12
Tom Linton <tom@originprotocol.com>
2019-09-05 10:39:36 +1200

    Format

--- packages/react-native-samsung-bks/android/src/main/java/com/originprotocol/samsungbks/RNSamsungBKSModule.java
+++ packages/react-native-samsung-bks/android/src/main/java/com/originprotocol/samsungbks/RNSamsungBKSModule.java
@@ -1,17 +1,15 @@
 package com.originprotocol.samsungbks;
 
-
-import java.util.List;
-import java.util.ArrayList;
-import java.lang.reflect.Field;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeMap;
-
 import com.samsung.android.sdk.coldwallet.*;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RNSamsungBKSModule extends ReactContextBaseJavaModule {
 
@@ -29,92 +27,136 @@ public class RNSamsungBKSModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void isSupported(Promise promise) {
-      promise.resolve(ScwService.getInstance() != null);
+    promise.resolve(ScwService.getInstance() != null);
   }
 
   @ReactMethod
   public void getKeystoreApiLevel(Promise promise) {
-      int keystoreApiLevel = ScwService.getInstance().getKeystoreApiLevel();
-      promise.resolve(keystoreApiLevel);
+    int keystoreApiLevel = ScwService.getInstance().getKeystoreApiLevel();
+    promise.resolve(keystoreApiLevel);
   }
 
   @ReactMethod
   public void getSeedHash(Promise promise) {
-      String seedHash = ScwService.getInstance().getSeedHash();
-      promise.resolve(seedHash);
+    String seedHash = ScwService.getInstance().getSeedHash();
+    promise.resolve(seedHash);
   }
 
   @ReactMethod
   public void checkForMandatoryAppUpdate(final Promise promise) {
-      ScwService.ScwCheckForMandatoryAppUpdateCallback callback =
-          new ScwService.ScwCheckForMandatoryAppUpdateCallback() {
-	      @Override
-	      public void onMandatoryAppUpdateNeeded(boolean needed) {
-	          if (needed) {
-		      promise.resolve(ScwDeepLink.GALAXY_STORE);
-		  } else {
-		      promise.resolve(false);
-		  }
-	       }
-	   };
+    ScwService.ScwCheckForMandatoryAppUpdateCallback callback =
+        new ScwService.ScwCheckForMandatoryAppUpdateCallback() {
+          @Override
+          public void onMandatoryAppUpdateNeeded(boolean needed) {
+            if (needed) {
+              promise.resolve(ScwDeepLink.GALAXY_STORE);
+            } else {
+              promise.resolve(false);
+            }
+          }
+        };
 
     ScwService.getInstance().checkForMandatoryAppUpdate(callback);
   }
 
   @ReactMethod
   public void getDeepLinks(Promise promise) {
-      Field[] interfaceFields = ScwDeepLink.class.getFields();
+    Field[] interfaceFields = ScwDeepLink.class.getFields();
 
-      WritableMap links = new WritableNativeMap();
-      for (Field f: interfaceFields) {
-	  try {
-	      links.putString(f.getName(), f.get(ScwDeepLink.class).toString());
-	  } catch (Exception error) {
-	      // Skip
-	  }
+    WritableMap links = new WritableNativeMap();
+    for (Field f : interfaceFields) {
+      try {
+        links.putString(f.getName(), f.get(ScwDeepLink.class).toString());
+      } catch (Exception error) {
+        // Skip
       }
+    }
 
-      promise.resolve(links);
+    promise.resolve(links);
   }
 
   @ReactMethod
   public void getAddressList(String hdPath, final Promise promise) {
-    ScwService.ScwGetAddressListCallback callback = new ScwService.ScwGetAddressListCallback() {
-	@Override
-	public void onSuccess(List<String> addressList) {
-	    promise.resolve(addressList.get(0));
-	}
+    ScwService.ScwGetAddressListCallback callback =
+        new ScwService.ScwGetAddressListCallback() {
+          @Override
+          public void onSuccess(List<String> addressList) {
+            promise.resolve(addressList.get(0));
+          }
 
-	@Override
-	public void onFailure(int errorCode) {
-	    Field[] interfaceFields = ScwErrorCode.class.getFields();
-	    Field errorField = null;
-	    for (Field f: interfaceFields) {
-		try {
-		    if (f.getInt(ScwErrorCode.class) == errorCode) {
-			errorField = f;
-		    }
-		} catch (Exception error) {
-		    // Skip
-		}
-	    }
+          @Override
+          public void onFailure(int errorCode) {
+            Field[] interfaceFields = ScwErrorCode.class.getFields();
+            Field errorField = null;
+            for (Field f : interfaceFields) {
+              try {
+                if (f.getInt(ScwErrorCode.class) == errorCode) {
+                  errorField = f;
+                }
+              } catch (Exception error) {
+                // Skip
+              }
+            }
 
-	    if (errorField != null) {
-		try {
-		    promise.reject(errorField.get(ScwErrorCode.class).toString(), errorField.getName());
-		} catch (Exception error) {
-		    System.out.println(error);
-		    promise.reject("Samsung Blockchain Keystore error " + errorCode);
-		}
-	    } else {
-		promise.reject("Samsung Blockchain Keystore error " + errorCode);
-	    }
-	}
-      };
+            if (errorField != null) {
+              try {
+                promise.reject(errorField.get(ScwErrorCode.class).toString(), errorField.getName());
+              } catch (Exception error) {
+                System.out.println(error);
+                promise.reject("Samsung Blockchain Keystore error " + errorCode);
+              }
+            } else {
+              promise.reject("Samsung Blockchain Keystore error " + errorCode);
+            }
+          }
+        };
 
-      ArrayList<String> hdPathList = new ArrayList<>();
-      hdPathList.add(hdPath);
+    ArrayList<String> hdPathList = new ArrayList<>();
+    hdPathList.add(hdPath);
 
-      ScwService.getInstance().getAddressList(callback, hdPathList);
+    ScwService.getInstance().getAddressList(callback, hdPathList);
+  }
+
+  @ReactMethod
+  public void signEthTransaction(
+      String hdPath,
+      String toAddress,
+      String ethAmount,
+      string ethGasPrice,
+      String ethGasLimit,
+      String data,
+      final Promise promise) {
+    ScwService.ScwSignEthTransactionCallback callback =
+        new ScwService.ScwSignEthTransactionCallback() {
+          @Override
+          public void onSuccess(byte[] signedEthTransaction) {}
+
+          @Override
+          public void onFailure(int errorCode) {}
+        };
+
+    byte[] encodedUnsignedEthTx =
+        createRawTransaction(toAddress, ethAmount, ethGasPrice, ethGasLimit, data);
+
+    // private byte[] createRawTransaction () {
+    // }
+
+    ScwService.getInstance().signEthTransaction(callback, encodedUnsignedEthTx, hdPath);
+  }
+
+  @ReatMethod
+  public void signEthPersonalMessage(String hdPath, Promise promise) {
+    ScwService.ScwSignEthPersonalMessageCallback callback =
+        new ScwService.ScwSignEthPersonalMessageCallback() {
+          @Override
+          public void onSuccess(byte[] signedPersonalMessage) {}
+
+          @Override
+          public void onFailure(int errorCode) {}
+        };
+
+    byte[] unSignedMsg = "To sign up, please sign this message.".getBytes();
+
+    ScwService.getInstance().signEthPersonalMessage(callback, unSignedMsg, hdPath);
   }
 }
--- packages/react-native-samsung-bks/android/src/main/java/com/originprotocol/samsungbks/RNSamsungBKSPackage.java
+++ packages/react-native-samsung-bks/android/src/main/java/com/originprotocol/samsungbks/RNSamsungBKSPackage.java
@@ -1,28 +1,27 @@
 package com.originprotocol.samsungbks;
 
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
-
 public class RNSamsungBKSPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNSamsungBKSModule(reactContext));
-    }
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    return Arrays.<NativeModule>asList(new RNSamsungBKSModule(reactContext));
+  }
 
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
+  // Deprecated from RN 0.47
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
 
-    @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
-    }
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
 }
