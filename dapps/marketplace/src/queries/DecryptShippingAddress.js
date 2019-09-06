ebaf54b46643c520fd15b27f9e92d3cc54451e56
Shahul Hameed <shahthepro@outlook.com>
2019-09-06 12:33:35 +0530

    Fix test

---
+++ dapps/marketplace/src/queries/DecryptShippingAddress.js
@@ -0,0 +1,18 @@
+import gql from 'graphql-tag'
+
+const DecryptShippingAddress = gql`
+  query DecryptShippingAddress($encrypted: String!) {
+    messaging(id: "defaultAccount") {
+      decryptShippingAddress(encrypted: $encrypted) {
+        name
+        address1
+        address2
+        city
+        stateProvinceRegion
+        postalCode
+        country
+      }
+    }
+  }
+`
+export default DecryptShippingAddress
