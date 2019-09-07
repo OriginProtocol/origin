349d769fd7645d19521c842fdb8f1ccc4eaf139f
Mike Shultz <shultzm@gmail.com>
2019-09-07 15:47:02 -0600

    remove web3-provider from browsers

--- packages/graphql/src/contracts.js
+++ packages/graphql/src/contracts.js
@@ -236,7 +236,7 @@ export function setNetwork(net, customConfig) {
       qps,
       ethGasStation: ['mainnet', 'rinkeby'].includes(net)
     })
-  } else if (!isWebView) {
+  } else if (!isBrowser && !isWebView) {
     // TODO: Allow for browser?
     createEngine(web3, {
       qps,
