e8e3c5fdf8a4d53c64302ecb56c996f8d711c65e
Tom Linton <tom@originprotocol.com>
2019-09-09 16:41:33 +1200

    Move aar from react-native-samsung-bks to this package

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -9,10 +9,7 @@ const path = require('path')
 
 const externalPackages = {
   '@origin/graphql': path.resolve(__dirname + '/../packages/graphql/'),
-  '@origin/contracts': path.resolve(__dirname + '/../packages/contracts/'),
-  'react-native-samsung-bks': path.resolve(
-    __dirname + '/../packages/react-native-samsung-bks'
-  )
+  '@origin/contracts': path.resolve(__dirname + '/../packages/contracts/')
 }
 
 const extraNodeModules = {
