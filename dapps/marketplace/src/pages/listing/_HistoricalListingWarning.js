2c0f322ef44814a56bb61027d5b6dfb4d3bbe572
Shahul Hameed <shahthepro@outlook.com>
2019-09-13 09:21:15 +0530

    Add Historical Price

--- dapps/marketplace/src/pages/listing/_HistoricalListingWarning.js
+++ dapps/marketplace/src/pages/listing/_HistoricalListingWarning.js
@@ -2,22 +2,33 @@ import React from 'react'
 import Link from 'components/Link'
 import { currentListingIdFromHistoricalId } from 'utils/listing'
 
+import Price from 'components/Price'
+
 import { fbt } from 'fbt-runtime'
 
 const HistoricalListingWarning = ({ listing }) => (
-  <div className="historical-warning-container">
-    <div className="historical-warning">
-      <fbt desc="listingDetail.view-current-listing">
-        A newer version of this listing has been published
-      </fbt>
+  <>
+    <div className="listing-buy">
+      <div className="price">
+        <Price listing={listing} descriptor />
+      </div>
     </div>
-    <Link
-      to={`/listing/${currentListingIdFromHistoricalId(listing)}`}
-      className="btn btn-primary btn-block btn-rounded"
-    >
-      <fbt desc="listingDetail.view-current-listing">View Current Listing</fbt>
-    </Link>
-  </div>
+    <div className="historical-warning-container">
+      <div className="historical-warning">
+        <fbt desc="listingDetail.view-current-listing">
+          A newer version of this listing has been published
+        </fbt>
+      </div>
+      <Link
+        to={`/listing/${currentListingIdFromHistoricalId(listing)}`}
+        className="btn btn-primary btn-block btn-rounded"
+      >
+        <fbt desc="listingDetail.view-current-listing">
+          View Current Listing
+        </fbt>
+      </Link>
+    </div>
+  </>
 )
 
 export default HistoricalListingWarning
@@ -25,6 +36,8 @@ export default HistoricalListingWarning
 require('react-styl')(`
   .historical-warning-container
     margin-bottom: 2rem
+    padding: 0 0 2rem 0
+    border-bottom: 1px solid #dde6ea
     .historical-warning
       border-radius: 0.625rem
       border: solid 1px #ffc000
