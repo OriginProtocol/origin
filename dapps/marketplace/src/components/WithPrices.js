53f7574ce6df33aacdd349991b82dcc9ad6dd4ba
Shahul Hameed <shahthepro@outlook.com>
2019-09-15 12:28:56 +0530

    Prettify

--- dapps/marketplace/src/components/WithPrices.js
+++ dapps/marketplace/src/components/WithPrices.js
@@ -23,13 +23,17 @@ const WithPrices = ({
     needsAllowance,
     needsBalance
 
-  const isLoadingData = Object.keys(props).some(key => key.endsWith('Loading') && props[key])
+  const isLoadingData = Object.keys(props).some(
+    key => key.endsWith('Loading') && props[key]
+  )
 
   // loading: true if wallet or anything else is loading
-  if (isLoadingData) return children({ prices: {}, tokenStatus: { loading: true } })
+  if (isLoadingData)
+    return children({ prices: {}, tokenStatus: { loading: true } })
 
   // If there is no wallet after it has loaded, return loading: false
-  if (!props.wallet) return children({ prices: {}, tokenStatus: { loading: false } })
+  if (!props.wallet)
+    return children({ prices: {}, tokenStatus: { loading: false } })
 
   if (!currency) return children({ prices: {}, tokenStatus: { loading: true } })
   const foundCurrency = currencies.find(c => c.id === currency.id)
