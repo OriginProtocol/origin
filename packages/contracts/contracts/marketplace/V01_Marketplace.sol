048c955d723ef872b49dcd4753625a7de059ba06
Mike Shultz <shultzm@gmail.com>
2019-09-20 15:09:23 -0600

    brings conract header inline with what went to etherscan.  closes #3322

--- packages/contracts/contracts/marketplace/V01_Marketplace.sol
+++ packages/contracts/contracts/marketplace/V01_Marketplace.sol
@@ -1,13 +1,34 @@
 pragma solidity ^0.4.24;
 
-import "../../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
-
-/**
- * @title A Marketplace contract for managing listings, offers, payments, escrow and arbitration
- * @author Nick Poulden <nick@poulden.com>
+/*
+ * Origin Protocol
+ * https://originprotocol.com
  *
- * Listings may be priced in Eth or ERC20.
+ * Released under the MIT license
+ * https://github.com/OriginProtocol
+ *
+ * Copyright 2019 Origin Protocol, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
+
+import "../../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 
 contract ERC20 {
@@ -16,6 +37,11 @@ contract ERC20 {
 }
 
 
+**
+ * @title A Marketplace contract for managing listings, offers, payments, escrow and arbitration
+ *
+ * Listings may be priced in ETH or ERC20.
+ */
 contract V01_Marketplace is Ownable {
 
     /**
