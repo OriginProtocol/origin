ea13fbac292e8b5f0f512f691e25268612ec7ac7
Nick Poulden <nick@poulden.com>
2019-09-25 13:33:28 -0600

    Nick/small tweaks (#3491)
    * Center and add padding to Loading listings indicator on mobile
    
    * User profile use hooks
    
    * SendMessage paste fix
    
    * Messages lifecycle events. Closes #1819
    
    * Dispute offer fixes
    
    * Refactor reviews component

--- dapps/marketplace/src/hoc/withCounterpartyEvents.js
+++ dapps/marketplace/src/hoc/withCounterpartyEvents.js
@@ -7,7 +7,7 @@ import CounterpartyEventsQuery from 'queries/CounterpartyEventsQuery'
 function withCounterpartyEvents(WrappedComponent) {
   const WithCounterpartyEvents = props => {
     const { data, loading } = useQuery(CounterpartyEventsQuery, {
-      variables: { user: props.wallet, counterparty: props.id },
+      variables: { user: props.walletProxy, counterparty: props.id },
       skip: !props.wallet || !props.id
     })
     return (
