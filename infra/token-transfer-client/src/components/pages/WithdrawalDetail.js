58908d1c96f9afbf1da234ab2e394f241ad1abad
Tom Linton <tom@originprotocol.com>
2019-09-27 14:38:05 +1200

    Trigger rerenders so hasExpired is correct

--- infra/token-transfer-client/src/components/pages/WithdrawalDetail.js
+++ infra/token-transfer-client/src/components/pages/WithdrawalDetail.js
@@ -23,6 +23,14 @@ class WithdrawalDetail extends Component {
 
   componentDidMount() {
     this.props.fetchTransfers()
+    // Force component update every three seconds because time passing won't
+    // trigger an update as it is not in state. This is required to correctly
+    // update when expired.
+    this.interval = setInterval(() => this.forceUpdate(), 3000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
   }
 
   componentDidUpdate(prevProps) {
