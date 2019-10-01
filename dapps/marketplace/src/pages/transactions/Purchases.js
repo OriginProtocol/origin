b0f7165bd18725e54df4d10435cf8040ebad8f13
VIviane Dias <viviane.dias@colab.re>
2019-09-30 21:48:37 -0300

    Remove Fragment and replace div with <a> tag

--- dapps/marketplace/src/pages/transactions/Purchases.js
+++ dapps/marketplace/src/pages/transactions/Purchases.js
@@ -126,44 +126,40 @@ const NoPurchases = () => (
 )
 
 const Purchase = ({ listing, offer }) => (
-  <Fragment>
-    <Link to={`/purchases/${offer.id}`}>
-      <div className="purchase">
-        <div className="pic">
-          <Pic listing={listing} />
-          {offer.quantity === undefined || offer.quantity <= 1 ? null : (
-            <div className="quantity">{offer.quantity}</div>
-          )}
-        </div>
-        <div className="details">
-          <div className="top">
-            <h2 className="title mb-1">
-              {listing.title || <i>Untitled Listing</i>}
-            </h2>
-            <div className="right">
-              <span className="time-estimate">
-                {distanceToNow(offer.createdEvent.timestamp, true) + ' ago'}
-              </span>
-              <OfferStatus offer={offer} />
-            </div>
-          </div>
-          <div className="date">
-            {listing.createdEvent &&
-              fbt('Offer made on', 'Purchases.offerMadeOn') +
-                ` ${dayjs
-                  .unix(offer.createdEvent.timestamp)
-                  .format('MMMM D, YYYY')}`}
-          </div>
-          <div className="price">
-            <div className="d-flex">
-              <Price price={offer.totalPrice} />
-            </div>
-          </div>
-          <Stages mini offer={offer} />
+  <Link className="purchase" to={`/purchases/${offer.id}`}>
+    <div className="pic">
+      <Pic listing={listing} />
+      {offer.quantity === undefined || offer.quantity <= 1 ? null : (
+        <div className="quantity">{offer.quantity}</div>
+      )}
+    </div>
+    <div className="details">
+      <div className="top">
+        <h2 className="title mb-1">
+          {listing.title || <i>Untitled Listing</i>}
+        </h2>
+        <div className="right">
+          <span className="time-estimate">
+            {distanceToNow(offer.createdEvent.timestamp, true) + ' ago'}
+          </span>
+          <OfferStatus offer={offer} />
         </div>
       </div>
-    </Link>
-  </Fragment>
+      <div className="date">
+        {listing.createdEvent &&
+          fbt('Offer made on', 'Purchases.offerMadeOn') +
+            ` ${dayjs
+              .unix(offer.createdEvent.timestamp)
+              .format('MMMM D, YYYY')}`}
+      </div>
+      <div className="price">
+        <div className="d-flex">
+          <Price price={offer.totalPrice} />
+        </div>
+      </div>
+      <Stages mini offer={offer} />
+    </div>
+  </Link>
 )
 
 export default withWallet(Listings)
