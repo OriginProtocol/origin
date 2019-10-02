0d9b46b8848edeeee05cd5dc48ff34d34cd96ce8
Mike Shultz <shultzm@gmail.com>
2019-10-02 10:15:30 -0600

    integrate @nick's feedback

--- packages/graphql/src/utils/memorize.js
+++ packages/graphql/src/utils/memorize.js
@@ -17,8 +17,8 @@ const DontCache = [
  */
 export default function memorize(func, resolver) {
   if (
-    typeof func != 'function' ||
-    (resolver != null && typeof resolver != 'function')
+    typeof func !== 'function' ||
+    (resolver !== null && typeof resolver !== 'function')
   ) {
     throw new TypeError('Not a function')
   }
