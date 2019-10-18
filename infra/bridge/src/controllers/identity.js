b93194754253b32027bcc0266fe81cacf1356ff2
Franck Chastagnol <franck@originprotocol.com>
2019-10-17 21:27:56 -0700

    Add identity routes to bridge

---
+++ infra/bridge/src/controllers/identity.js
@@ -0,0 +1,185 @@
+'use strict'
+
+const express = require('express')
+const get = require('lodash/get')
+
+const router = express.Router()
+const { GrowthEvent } = require('@origin/growth-event/src/resources/event')
+const {
+  loadIdentityAttestationsMetadata,
+  loadIdentityAddresses,
+  recordGrowthProfileEvent,
+  recordGrowthAttestationEvents
+} = require('@origin/identity/src/utils')
+const Identity = require('@origin/identity/src/models').Identity
+const validator = require('@origin/validator')
+
+const { pinIdentityToIpfs, postToEmailWebhook } = require('../utils/identity')
+const {
+  identityReadVerify,
+  identityWriteVerify
+} = require('../utils/validation')
+
+const logger = require('../logger')
+
+/**
+ * Loads the identity associated with an eth address.
+ * Returns:
+ *  - 200 if identity was found in the database.
+ *  - 204 if no identity found.
+ *  - 500 if identity row found but with empty data in the database.
+ *
+ *  @args {string} req.ethAddress Eth address of the user.
+ *  @returns {{identity: Object, ipfsHash: string}}
+ */
+router.get('/', identityReadVerify, async (req, res) => {
+  const ethAddress = req.query.ethAddress.toLowerCase()
+  logger.debug(`identity/read called for addr ${ethAddress}`)
+
+  // TODO: should we support lookups for both proxy and owner ???
+  // Probably... ?
+  // Lookup in proxy table ?
+
+  // Lookup the identity associated with the eth address.
+  const identity = await Identity.findOne({ where: { ethAddress } })
+
+  /* FOR TESTING
+  const identity = {
+    data: {
+      "schemaId":"https://schema.originprotocol.com/identity_1.0.0.json",
+      "profile": {
+        "firstName": "Francky" + Math.ceil(Math.random()*100),
+        "lastName": "Baloboa",
+        "description": "I'm a test account! I think?",
+        "avatarUrl": "ipfs://QmWnTmoY6Pi5u3gxE9QSSFzw1MoCLgcd1Wg5mxTxzsL57c",
+        "schemaId": "https://schema.originprotocol.com/profile_2.0.0.json",
+        "ethAddress": ethAddress
+      },
+      "attestations": []
+    }
+  }
+  */
+
+  if (!identity || !identity.data) {
+    logger.debug(`No identity found for eth address ${ethAddress}.`)
+    return res.status(204).end()
+  }
+
+  // Check validity of the data before returning it.
+  const errors = []
+  for (const field of ['identity', 'ipfsHash', 'ipfsHashHistory']) {
+    if (!get(identity.data, field)) {
+      errors.push(`Identity ${ethAddress} is missing field data.${field}`)
+    }
+  }
+  if (errors.length) {
+    logger.error(errors)
+    return res.status(500).send({ errors })
+  }
+
+  return res.status(200).send({
+    identity: identity.data.identity,
+    ipfsHash: identity.data.ipfsHash
+  })
+})
+
+/**
+ * Writes an identity to the database.
+ *
+ * @args {string} req.query.ethAddress: Eth address of the user.
+ * @args {Object} req.body.identity: Identity JSON blob.
+ * @args {string} req.body.ipfsHash: IPFS hash of the identity JSON blob.
+ */
+router.post('/', identityWriteVerify, async (req, res) => {
+  const ethAddress = req.query.ethAddress.toLowerCase()
+  const data = req.body || {}
+  logger.debug(`identity/write called for addr ${ethAddress}`)
+
+  if (!data.identity) {
+    return res.status(400).send({ errors: ['Identity data missing'] })
+  }
+  if (!data.ipfsHash) {
+    return res.status(400).send({ errors: ['IPFS hash missing'] })
+  }
+
+  // Parse the identity data to make sure it is valid.
+  try {
+    validator(
+      'https://schema.originprotocol.com/identity_1.0.0.json',
+      data.identity
+    )
+    validator(
+      'https://schema.originprotocol.com/profile_2.0.0.json',
+      data.identity.profile
+    )
+    data.identity.attestations.forEach(a => {
+      validator('https://schema.originprotocol.com/attestation_1.0.0.json', a)
+    })
+  } catch (err) {
+    logger.error(`Failed parsing identity data for ${ethAddress}`, err)
+    return res
+      .status(400)
+      .send({ errors: [`Failed parsing identity data: ${err}`] })
+  }
+
+  // Load attestation data from the DB.
+  const addresses = loadIdentityAddresses(ethAddress)
+  const metadata = loadIdentityAttestationsMetadata(
+    addresses,
+    data.identity.attestations
+  )
+
+  // Create an object representing the updated identity.
+  const identity = {
+    ethAddress,
+    firstName: get(data.identity, 'profile.firstName'),
+    lastName: get(data.identity, 'profile.lastName'),
+    avatarUrl: get(data.identity, 'profile.avatarUrl'),
+    data: {
+      identity: data.identity,
+      ipfsHash: data.ipfsHash,
+      ipfsHashHistory: []
+    },
+    ...metadata
+  }
+
+  // Lookup any existing identity associated with the eth address
+  // to get the ipfs hash history.
+  const identityRow = await Identity.findOne({ where: { ethAddress } })
+  if (identityRow) {
+    logger.debug(`Found existing identity DB row for ${ethAddress}`)
+    // Append the old IPFS hash to the history.
+    const ipfsHashHistory = get(identityRow, 'data.ipfsHashHistory', [])
+    const prevIpfsHash = get(identityRow, 'data.ipfsHash')
+    if (prevIpfsHash) {
+      ipfsHashHistory.push({
+        ipfsHash: prevIpfsHash,
+        timestamp: identityRow.updatedAt.getTime()
+      })
+    }
+    identity.data.ipfsHashHistory = ipfsHashHistory
+  }
+  await Identity.upsert(identity)
+
+  // Record the growth events.
+  const now = new Date()
+  await recordGrowthProfileEvent(ethAddress, identity, now, GrowthEvent)
+  await recordGrowthAttestationEvents(
+    ethAddress,
+    data.identity.attestations,
+    now,
+    GrowthEvent
+  )
+
+  // Pin the Identity data to the IPFS cluster.
+  // TODO: check the data format expected by the GCP cloud function
+  await pinIdentityToIpfs(data.identity)
+
+  // Call the webhook to record the user's email in the insight tool.
+  // TODO: check data format.
+  await postToEmailWebhook(identity)
+
+  return res.status(200).send({ id: req.query.ethAddress })
+})
+
+module.exports = router
---
+++ infra/bridge/src/utils/identity.js
@@ -0,0 +1,91 @@
+'use strict'
+
+const http = require('http')
+const https = require('https')
+const urllib = require('url')
+const { PubSub } = require('@google-cloud/pubsub')
+
+const logger = require('../logger')
+
+async function pinIdentityToIpfs(identity) {
+  if (process.env.NODE_ENV !== 'production') {
+    return
+  }
+
+  const projectId = process.env.GCLOUD_PROJECT_ID
+  const topic = process.env.GCLOUD_PUBSUB_TOPIC
+  const pubsub = new PubSub({
+    projectId,
+    keyFilename: process.env.GCLOUD_SERVICE_ACCOUNT_JSON
+  })
+
+  return await pubsub
+    .topic(topic)
+    .publish(Buffer.from(JSON.stringify(identity)))
+}
+
+/**
+ * Sends a blob of data to a webhook.
+ */
+async function _postToWebhook(
+  urlString,
+  data,
+  contentType = 'application/json'
+) {
+  const url = new urllib.URL(urlString)
+  const postOptions = {
+    host: url.hostname,
+    port: url.port,
+    path: url.pathname,
+    method: 'POST',
+    headers: {
+      'Content-Type': contentType,
+      'Content-Length': Buffer.byteLength(data)
+    }
+  }
+  return new Promise((resolve, reject) => {
+    logger.debug(`Calling webhook ${urlString}`)
+    const client = url.protocol === 'https:' ? https : http
+    const req = client.request(postOptions, res => {
+      logger.debug(`Webhook response status code=${res.statusCode}`)
+      if (res.statusCode === 200 || res.statusCode === 204) {
+        resolve()
+      } else {
+        reject(new Error(`statusCode ${res.statusCode}`))
+      }
+    })
+    req.on('error', err => {
+      reject(err)
+    })
+    req.write(data)
+    req.end()
+  })
+}
+
+/**
+ * Triggers on Identity event to add the user's email to
+ * our global Origin mailing list.
+ */
+async function postToEmailWebhook(identity) {
+  if (process.env.NODE_ENV !== 'production') {
+    return
+  }
+  const url = process.env.EMAIL_WEBHOOK
+  if (!identity.email) {
+    logger.warn('No email present in identity, skipping email webhook.')
+    return
+  }
+
+  const emailData = `eth_address=${encodeURIComponent(
+    identity.ethAddress
+  )}&email=${encodeURIComponent(
+    identity.email
+  )}&first_name=${encodeURIComponent(
+    identity.firstName || ''
+  )}&last_name=${encodeURIComponent(
+    identity.lastName || ''
+  )}&phone=${encodeURIComponent(identity.phone || '')}&dapp_user=1`
+  await _postToWebhook(url, emailData, 'application/x-www-form-urlencoded')
+}
+
+module.exports = { pinIdentityToIpfs, postToEmailWebhook }
