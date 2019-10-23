f2baf55a57a8a28e3cc9dbdd264e246661b6dc16
Tom Linton <tom@originprotocol.com>
2019-10-24 12:53:15 +1300

    T3 Lockup Implementation (#3772)
    * Start lockup implementation
    
    * Restructure and implement balance changes to handle lockups
    
    * Logic for earned tokens
    
    * Tests passing
    
    * Test updates
    
    * Refactor logic
    
    * Fix data types
    
    * Restructure logic
    
    * Below 0 handling and tests
    
    * Prettier
    
    * QRcode package
    
    * Yarn.lock conflict

---
+++ infra/token-transfer-server/src/models/lockup.js
@@ -0,0 +1,24 @@
+'use strict'
+
+module.exports = (sequelize, DataTypes) => {
+  const Lockup = sequelize.define(
+    'Lockup',
+    {
+      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      userId: { type: DataTypes.INTEGER, allowNull: false },
+      start: DataTypes.DATE,
+      end: DataTypes.DATE,
+      bonus_rate: DataTypes.INTEGER,
+      amount: DataTypes.INTEGER
+    },
+    {
+      tableName: 't3_lockup'
+    }
+  )
+
+  Lockup.associate = models => {
+    Lockup.belongsTo(models.User)
+  }
+
+  return Lockup
+}
