86c51b1dfd991ffd43ed81d25f10cf3dc83ed8ee
Mike Shultz <shultzm@gmail.com>
2019-10-31 17:42:05 -0600

    added .circleci/docker_test_builds.sh to make builds conditional

---
+++ .circleci/docker_test_builds.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+################################################################################
+## Runs builds only if a specific file/directory has been updated in the latest
+## commit.
+##
+## Usage
+## -----
+## do_build package-name path/to/file/to/check
+################################################################################
+
+echo "Testing builds..."
+
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd)"
+REPO_COMMIT=$(git rev-parse HEAD)
+
+retval=""
+fails=0
+do_build() {
+    COMMIT=$(git log -1 --format=format:%H --full-diff $2)
+    if [[ "$REPO_COMMIT" = "$COMMIT" ]]; then
+        echo "Building $1..."
+        docker build --build-arg ENVKEY=$ENVKEY -t $1 -f $DIR/devops/dockerfiles/$1 .
+        RC=$?
+        if [[ $RC -eq 0 ]]; then
+            retval="[ OK  ] $1"
+        else
+            retval="[ERROR] $1"
+            fails=$((fails++))
+        fi
+        echo "$1 build complete."
+    else
+        retval="[ SKIP ] $1"
+    fi
+}
+
+results=""
+
+do_build origin-cron infra/cron/package.json
+results="$results
+$retval"
+do_build origin-discovery infra/discovery/package.json
+results="$results
+$retval"
+do_build event-listener infra/discovery/package.json
+results="$results
+$retval"
+do_build origin-graphql packages/graphql/package.json
+results="$results
+$retval"
+do_build origin-growth infra/growth/package.json
+results="$results
+$retval"
+do_build origin-ipfs-proxy packages/ipfs/package.json
+results="$results
+$retval"
+do_build origin-messaging infra/messaging/package.json
+results="$results
+$retval"
+do_build origin-notifications infra/notifications/package.json
+results="$results
+$retval"
+do_build origin-relayer infra/relayer/package.json
+results="$results
+$retval"
+do_build origin-dapp dapps/marketplace/package.json
+results="$results
+$retval"
+
+echo "Finished build tests:
+$results"
+
+exit $fails
