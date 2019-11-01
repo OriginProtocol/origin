968df2e8a3a2076b967e708f0aad4639cab80684
Mike Shultz <shultzm@gmail.com>
2019-11-01 12:32:58 -0600

    add check for ENVKEY env var

--- .circleci/docker_test_builds.sh
+++ .circleci/docker_test_builds.sh
@@ -13,6 +13,11 @@ echo "Testing builds..."
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd)"
 REPO_COMMIT=$(git rev-parse master)
 
+if [[ $ENVKEY = "" ]]; then
+    echo "ENVKEY must be set!"
+    exit 1
+fi
+
 retval=""
 fails=0
 do_build() {
