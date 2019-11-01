95be71dad9a263352357742a99315cd82d704aae
Mike Shultz <shultzm@gmail.com>
2019-11-01 11:43:42 -0600

    bash script cleanup

--- .circleci/docker_test_builds.sh
+++ .circleci/docker_test_builds.sh
@@ -13,15 +13,11 @@ echo "Testing builds..."
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd)"
 REPO_COMMIT=$(git rev-parse master)
 
-echo "git version: $(git --version)"
-
 retval=""
 fails=0
 do_build() {
     CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
-    echo "Current branch: $CURRENT_BRANCH"
     DIFF=$(git diff --name-only $CURRENT_BRANCH..origin/master | grep $2 | wc -l)
-    echo "Changed $2? $DIFF"
     if [[ "$DIFF" != "0" ]]; then
         echo "Building $1..."
         docker build --build-arg ENVKEY=$ENVKEY -t $1 -f $DIR/devops/dockerfiles/$1 .
