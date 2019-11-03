268c91a8728b3bb1b1875b14686cda96352a162e
Tom Linton <tom@originprotocol.com>
2019-11-03 18:58:40 +1300

    Prettier

--- infra/token-transfer-server/migrations/20191103185100-add-lockup-data-column.js
+++ infra/token-transfer-server/migrations/20191103185100-add-lockup-data-column.js
@@ -5,16 +5,10 @@ const tableName = 't3_lockup'
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.addColumn(
-        tableName,
-        'data',
-        Sequelize.JSONB
-      )
+      queryInterface.addColumn(tableName, 'data', Sequelize.JSONB)
     ])
   },
   down: queryInterface => {
-    return Promise.all([
-      queryInterface.removeColumn(tableName, 'data')
-    ])
+    return Promise.all([queryInterface.removeColumn(tableName, 'data')])
   }
 }
