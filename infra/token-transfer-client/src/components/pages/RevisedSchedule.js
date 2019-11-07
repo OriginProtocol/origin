859bcc01a711579528f921747f9cac83cc781613
Tom Linton <tom@originprotocol.com>
2019-11-07 15:24:15 +1300

    Prettier

--- infra/token-transfer-client/src/components/pages/RevisedSchedule.js
+++ infra/token-transfer-client/src/components/pages/RevisedSchedule.js
@@ -15,12 +15,12 @@ const RevisedSchedule = () => {
       <div className="action-card wide">
         <h1>Revised Token Unlock Schedule</h1>
         <p>
-          Multiple top-tier exchanges that we are partnering with to list
-          Origin Tokens (OGN) have required us to modify our token release
-          schedule. These amendments have been approved by our largest
-          investors, including Pantera Capital, Foundation Capital, Blocktower
-          Capital, PreAngel Fund, Hashed, Danhua Capital, and dozens of other
-          funds and individuals.
+          Multiple top-tier exchanges that we are partnering with to list Origin
+          Tokens (OGN) have required us to modify our token release schedule.
+          These amendments have been approved by our largest investors,
+          including Pantera Capital, Foundation Capital, Blocktower Capital,
+          PreAngel Fund, Hashed, Danhua Capital, and dozens of other funds and
+          individuals.
         </p>
         <p>
           Original Schedule: 3 month waiting period then 100% unlock
@@ -32,10 +32,10 @@ const RevisedSchedule = () => {
         <img className="img-fluid my-4" src={UnlockSchedule} />
         <h2 className="mb-3">Why is this necessary?</h2>
         <p>
-          Origin, our top investors, and our exchange partners all believe
-          that having a more gradual release is to the benefit of all our
-          investors and token holders. The new release schedule has been
-          designed to foster long-term community growth and participation.
+          Origin, our top investors, and our exchange partners all believe that
+          having a more gradual release is to the benefit of all our investors
+          and token holders. The new release schedule has been designed to
+          foster long-term community growth and participation.
         </p>
         <button
           className="btn btn-secondary btn-lg mt-5"
