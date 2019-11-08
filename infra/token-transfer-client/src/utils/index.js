643832aaa1ef2b144d1a3eb16f19396dc77d784c
Tom Linton <tom@originprotocol.com>
2019-11-08 16:39:05 +1300

    Prettier

--- infra/token-transfer-client/src/utils/index.js
+++ infra/token-transfer-client/src/utils/index.js
@@ -1,7 +1,6 @@
 import { employeeUnlockDate, investorUnlockDate } from '@/constants'
 
 export const getUnlockDate = user => {
-  if (!user)
-    return
+  if (!user) return
   return user.employee ? employeeUnlockDate : investorUnlockDate
 }
