ce3e199ed8c1a7477db218dcbd65b48fe1f7dcb3
Micah Alcorn <mail@micahalcorn.com>
2019-11-15 14:30:43 -0800

    Elaborate on lockup consequences

--- infra/token-transfer-client/src/components/BonusModal.js
+++ infra/token-transfer-client/src/components/BonusModal.js
@@ -175,8 +175,7 @@ class BonusModal extends Component {
           ) : (
             <>
               <div className="p-5 mx-4 text-muted text-center">
-                Please enter a number of tokens (minimum
-                10 OGN) to lock up for one year. Bonus tokens will be calculated based on that amount.
+                Please enter the number of tokens (minimum 100 OGN) to lock up for one year. Bonus tokens will be calculated based on this amount. Locked up and bonus tokens will be unavailable for withdrawal during the one-year lockup.
               </div>
               <hr />
             </>
