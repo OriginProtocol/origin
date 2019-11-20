c3c386cd6ab3780e0fb3ec8662bf1d4cc5d6fa6d
Tom Linton <tom@originprotocol.com>
2019-11-20 14:51:44 +1300

    WIP

--- infra/token-transfer-server/src/tasks/transfer.js
+++ infra/token-transfer-server/src/tasks/transfer.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
 const moment = require('moment')
 
+const Token = require('@origin/token/src/token')
+
+const { discordWebhookUrl } = require('../config')
+const { postToWebhook } = require('../lib/webhook')
 const { Transfer, Sequelize } = require('../models')
 const { executeTransfer } = require('../lib/transfer')
 const logger = require('../logger')
@@ -8,26 +12,39 @@ const enums = require('../enums')
 
 const LARGE_TRANSFER_THRESHOLD = 100000
 const LARGE_TRANSFER_DELAY = 60
+const WATCHDOG_PATH = './.execution'
+const NETWORK_ID = 1
 
 const initWatchdog = () => {
   // Check there is no existing watchdog.
-  if (fs.existsSync(this.config.watchdog)) {
+  if (fs.existsSync(WATCHDOG_PATH)) {
     throw new Error(
-      `Watchdog detected at ${this.config.watchdog}. Processing aborted.`
+      `Watchdog detected at ${WATCHDOG_PATH}. Processing aborted.`
     )
   }
 
   // Create a watchdog for this run.
-  fs.writeFileSync(this.config.watchdog, `Pid ${process.pid}`)
+  fs.writeFileSync(WATCHDOG_PATH, `Pid ${process.pid}`)
 }
 
 const clearWatchdog = () => {
   // Clean watchdog.
-  fs.unlinkSync(this.config.watchdog)
+  fs.unlinkSync(WATCHDOG_PATH)
 }
 
 export const executeTransfers = async () => {
   initWatchdog()
+
+  const waitingTransfers = await Transfer.findAll({
+    where: {
+      status: enums.TransferStatuses.WaitingConfirmation
+    }
+  })
+  if (waitingTransfers.length > 0) {
+    throw new Error(
+      `Found unconfirmed transfer(s). Fix before running this script again.`
+    )
+  }
 
   const cutoffTime = moment.utc().subtract(LARGE_TRANSFER_DELAY, 'hours')
   const transfers = await Transfer.findAll({
@@ -52,7 +69,7 @@ export const executeTransfers = async () => {
     logger.info(`Processing transfer ${transfer.id}`)
     await transfer.update({ status: enums.TransferStatuses.Processing })
     const result = await executeTransfer(transfer, {
-      networkId: this.config.networkId
+      networkId: NETWORK_ID
     })
     logger.info(
       `Processed transfer ${transfer.id}. Status: ${result.txStatus} TxHash: ${result.txHash}`
@@ -60,4 +77,20 @@ export const executeTransfers = async () => {
   }
 
   clearWatchdog()
+}
+
+export const checkWalletBalance = async () => {
+  const token = new Token(NETWORK_ID)
+  const balance = token.toTokenUnit(token.balance())
+
+  if (balance < 10) {
+    const webhookData = {
+      embeds: [
+        {
+          title: `Wallet balance is \`${balance}\`, should be \`${}\` OGN`
+        }
+      ]
+    }
+    await postToWebhook(discordWebhookUrl, JSON.stringify(webhookData))
+  }
 }
