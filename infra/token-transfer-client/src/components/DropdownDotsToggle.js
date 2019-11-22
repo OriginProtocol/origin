95ce4af644b3965b2b58c60566eb2a936e6b2776
Tom Linton <tom@originprotocol.com>
2019-11-22 14:31:08 +1300

    Upgrade T3 packages and fix for renovate merged package upgrade

--- infra/token-transfer-client/src/components/DropdownDotsToggle.js
+++ infra/token-transfer-client/src/components/DropdownDotsToggle.js
@@ -1,27 +1,17 @@
 import React from 'react'
 
-class DropdownDotsToggle extends React.Component {
-  constructor(props, context) {
-    super(props, context)
-
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick(e) {
+const DropdownDotsToggle = React.forwardRef(({ onClick }, ref) => (
+  <a href="" ref={ref} onClick={e => {
     e.preventDefault()
+    onClick(e)
+  }}
+className="dropdown-toggle-buttons">
+    <span className="ml-2" style={{ fontWeight: 900, color: '#bdcbd5' }}>
+      &#8942;
+    </span>
+  </a>
+))
 
-    this.props.onClick(e)
-  }
-
-  render() {
-    return (
-      <a href="" onClick={this.handleClick}>
-        <span className="ml-2" style={{ fontWeight: 900, color: '#bdcbd5' }}>
-          &#8942;
-        </span>
-      </a>
-    )
-  }
-}
+DropdownDotsToggle.displayName = 'DropdownDotsToggle'
 
 export default DropdownDotsToggle
