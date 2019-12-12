baea54e531e76e3a541c59944c349974b5c2e549
Mike Shultz <shultzm@gmail.com>
2019-12-11 19:20:06 -0700

    fix config fetchin in react hook

--- dapps/marketplace/src/hoc/withPartnerCampaignConfig.js
+++ dapps/marketplace/src/hoc/withPartnerCampaignConfig.js
@@ -2,7 +2,17 @@ import React, { useState, useEffect } from 'react'
 
 import { PARNTER_REFERRAL_CONFIG_URL } from 'constants/config'
 
-let cachedCampaigns
+let cachedCampaigns = {}
+
+async function fetchConfig() {
+  const resp = await fetch(PARNTER_REFERRAL_CONFIG_URL)
+  if (resp.status !== 200) {
+    console.error('Failed to fetch campaign config')
+    return
+  }
+  cachedCampaigns = await resp.json()
+  return cachedCampaigns
+}
 
 function withPartnerCampaignConfig(WrappedComponent) {
   const WithPartnerCampaignConfig = props => {
@@ -10,17 +20,11 @@ function withPartnerCampaignConfig(WrappedComponent) {
 
     useEffect(() => {
       let timeout
-      if (cachedCampaigns) {
+      if (cachedCampaigns && Object.keys(cachedCampaigns).length) {
         return
       } else {
         setTimeout(async () => {
-          const resp = await fetch(PARNTER_REFERRAL_CONFIG_URL)
-          if (resp.status !== 200) {
-            console.error('Failed to fetch campaign config')
-            return
-          }
-          const jason = await resp.json()
-          setPartnerConfig(jason)
+          setPartnerConfig(await fetchConfig())
         }, 3000)
       }
 
