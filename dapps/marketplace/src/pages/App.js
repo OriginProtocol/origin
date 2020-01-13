b3ec2d3152b2970dddec149b57ee49ff32256af9
Shahul Hameed <shahthepro@outlook.com>
2020-01-13 09:51:43 +0530

    Fix callback

--- dapps/marketplace/src/pages/App.js
+++ dapps/marketplace/src/pages/App.js
@@ -124,7 +124,7 @@ class App extends Component {
         ) : null}
         {!hideNavbar && (
           <Nav
-            onShowHelp={() => window.zE.activate}
+            onShowHelp={() => window.zE.activate()}
             onShowFooter={() => this.setState({ footer: true })}
             navbarBlueMode={isOnWelcomeAndNotOboard}
           />
@@ -181,7 +181,7 @@ class App extends Component {
         <Footer
           open={this.state.footer}
           onClose={() => this.setState({ footer: false })}
-          onShowHelp={() => window.zE.activate}
+          onShowHelp={() => window.zE.activate()}
           locale={this.props.locale}
           onLocale={this.props.onLocale}
           creatorConfig={creatorConfig}
