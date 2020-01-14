6ac3a8e30e63d6f093060b0592ad61aa806f7d77
Domen Grabec <grabec@gmail.com>
2020-01-14 17:04:36 +0100

    prettify

--- packages/utils/src/growth.js
+++ packages/utils/src/growth.js
@@ -21,35 +21,36 @@ async function makeReferralConnection(code, walletAddress) {
   const referrer = await _getReferrer(code)
 
   if (referrer === walletAddress.toLowerCase()) {
-  	throw new Error(`Referrer ${referrer} can't use own referral code`)
+    throw new Error(`Referrer ${referrer} can't use own referral code`)
   }
 
   const returnObj = {
-  	referrer
+    referrer
   }
 
-  if (
-    referralLink
-  ) {
-  	if (referralLink.referrerEthAddress.toLowerCase() !== referrer.toLowerCase()) {
-  		/* The referrer associated with the invite code does not match previously stored referrer.
-	     * A corner case scenario this might happen is as follow:
-	     *  - referee receives multiple invites.
-	     *  - referee clicks on an invite and enrolls into growth campaing
-	     *  - referee clicks on another invite link and enrolls again into
-	     *  growth campaign.
-	     *
-	     * When this happens we ignore the subsequent invites and attribute all
-	     * referees actions to the initial referrer.
-	     *
-	     */
-	  	throw new Error(`Referee ${walletAddress} already referred by ${referralLink.referrerEthAddress}`)
-  	} else {
-  		// the referrer -> referee link already created
-  		return returnObj
-  	}
+  if (referralLink) {
+    if (
+      referralLink.referrerEthAddress.toLowerCase() !== referrer.toLowerCase()
+    ) {
+      /* The referrer associated with the invite code does not match previously stored referrer.
+       * A corner case scenario this might happen is as follow:
+       *  - referee receives multiple invites.
+       *  - referee clicks on an invite and enrolls into growth campaing
+       *  - referee clicks on another invite link and enrolls again into
+       *  growth campaign.
+       *
+       * When this happens we ignore the subsequent invites and attribute all
+       * referees actions to the initial referrer.
+       *
+       */
+      throw new Error(
+        `Referee ${walletAddress} already referred by ${referralLink.referrerEthAddress}`
+      )
+    } else {
+      // the referrer -> referee link already created
+      return returnObj
+    }
   }
-
 
   await GrowthReferral.create({
     referrerEthAddress: referrer,
