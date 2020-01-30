e69100bbaa2a64947151a472b3d8bf6fb455fb9b
Shahul Hameed <shahthepro@outlook.com>
2020-01-30 09:30:05 +0530

    Update return type

--- packages/graphql/src/utils/unitTokenValue.js
+++ packages/graphql/src/utils/unitTokenValue.js
@@ -1,4 +1,5 @@
 import currencies from './currencies'
+import contracts from '../contracts'
 
 /**
  * Converts a fixed value to big int string
@@ -6,9 +7,11 @@ import currencies from './currencies'
  * @param {String} amount The token value with decimals
  * @param {String|Number} token The token ID or the number of decimals
  *
- * @returns {String} `amount` represented in smallest unit of `token`
+ * @returns {BN} `amount` represented in smallest unit of `token`
  */
 function getUnitTokenValue(amount, tokenOrDecimals) {
+  const BN = contracts.web3.utils.BN
+
   let tokenDecimals = tokenOrDecimals
 
   if (typeof tokenOrDecimals === 'string') {
@@ -31,9 +34,9 @@ function getUnitTokenValue(amount, tokenOrDecimals) {
   const frac = (_frac || '').substr(0, tokenDecimals)
 
   if (frac.length === tokenDecimals) {
-    return `${whole}${frac}`
+    return new BN(`${whole}${frac}`)
   } else if (frac.length < tokenDecimals) {
-    return `${whole}${frac.padEnd(tokenDecimals, '0')}`
+    return new BN(`${whole}${frac.padEnd(tokenDecimals, '0')}`)
   }
 
   // frac.length > tokenDecimals
