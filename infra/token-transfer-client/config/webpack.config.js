bdee54b7d65d464a5f6157694dd5ef36aea2002a
Tom Linton <tom@originprotocol.com>
2020-02-04 10:41:21 +1300

    Fix config name (#4320)

--- infra/token-transfer-client/config/webpack.config.js
+++ infra/token-transfer-client/config/webpack.config.js
@@ -572,7 +572,7 @@ module.exports = function(webpackEnv) {
           exclude: [/\.map$/, /asset-manifest\.json$/],
           importWorkboxFrom: 'cdn',
           navigateFallback: publicUrl + '/index.html',
-          navigateFallbackBlacklist: [
+          navigateFallbackDenylist: [
             // Exclude URLs starting with /_, as they're likely an API call
             new RegExp('^/_'),
             // Exclude URLs containing a dot, as they're likely a resource in
