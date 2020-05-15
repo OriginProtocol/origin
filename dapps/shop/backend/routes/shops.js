aede3439f560f02d022a6eb29b20c62a5b65ee5b
Mike Shultz <shultzm@gmail.com>
2020-05-15 17:07:20 -0600

    CloudDNS support for dshop super admin (#4461)
    * adds clouddns util lib
    
    * changes import path for cloudflare module
    
    * ugprades ipfs packages
    
    * integrates cloud DNS with shop setup, adds configuration for GCP service creds, fixes URL for /networks
    
    * revert ipfs back to 40.0
    
    * addresses PR feedback
    
    * revert change to super admin Settings page, and fix zone.getRecords result handling
    
    * ===

--- dapps/shop/backend/routes/shops.js
+++ dapps/shop/backend/routes/shops.js
@@ -4,7 +4,8 @@ const { authSellerAndShop, authRole, authSuperUser } = require('./_auth')
 const { createSeller } = require('../utils/sellers')
 const { getConfig, setConfig } = require('../utils/encryptedConfig')
 const { createShop } = require('../utils/shop')
-const setCloudflareRecords = require('../utils/cloudflare')
+const setCloudflareRecords = require('../utils/dns/cloudflare')
+const setCloudDNSRecords = require('../utils/dns/clouddns')
 const get = require('lodash/get')
 const set = require('lodash/set')
 const fs = require('fs')
@@ -304,23 +305,34 @@ module.exports = function(app) {
       hash = String(allFiles[allFiles.length - 1].cid)
     }
 
-    const subdomain = req.body.hostname
-    const zone = networkConfig.domain
     let domain
-
-    if (networkConfig.cloudflareApiKey) {
+    if (networkConfig.cloudflareApiKey || networkConfig.gcpCredentials) {
+      const subdomain = req.body.hostname
+      const zone = networkConfig.domain
       domain = `https://${subdomain}.${zone}`
-      await setCloudflareRecords({
-        email: networkConfig.cloudflareEmail,
-        key: networkConfig.cloudflareApiKey,
+
+      const opts = {
         ipfsGateway: 'ipfs-prod.ogn.app',
         zone,
         subdomain,
         hash
-      })
+      }
+
+      if (networkConfig.cloudflareApiKey) {
+        await setCloudflareRecords({
+          ...opts,
+          email: networkConfig.cloudflareEmail,
+          key: networkConfig.cloudflareApiKey,
+        })
+      } else if (networkConfig.gcpCredentials) {
+        await setCloudDNSRecords({
+          ...opts,
+          credentials: networkConfig.gcpCredentials
+        })
+      }
     }
 
-    res.json({ success: true, hash, domain, gateway: network.ipfs })
+    return res.json({ success: true, hash, domain, gateway: network.ipfs })
   })
 
   app.post(
